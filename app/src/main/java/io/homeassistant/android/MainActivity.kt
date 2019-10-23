d1425433e424cf778376c06b0de57eb5a0225547
CedrickFlocon <7161169+CedrickFlocon@users.noreply.github.com>
2019-10-23 14:41:40 +0200

    Update all dependencies

--- app/src/main/java/io/homeassistant/android/MainActivity.kt
+++ app/src/main/java/io/homeassistant/android/MainActivity.kt
@@ -1,17 +1,15 @@
 package io.homeassistant.android
 
-import android.util.Log
-import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
-import android.widget.Toast
-import com.google.android.gms.tasks.OnCompleteListener
-import com.google.firebase.iid.FirebaseInstanceId
-import com.google.firebase.messaging.FirebaseMessaging
+import android.util.Log
+import android.view.WindowManager
 import android.webkit.WebView
 import android.webkit.WebViewClient
-import android.support.v4.content.ContextCompat
-import android.view.WindowManager
-
+import android.widget.Toast
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.content.ContextCompat
+import com.google.android.gms.tasks.OnCompleteListener
+import com.google.firebase.iid.FirebaseInstanceId
 
 
 class MainActivity : AppCompatActivity() {
