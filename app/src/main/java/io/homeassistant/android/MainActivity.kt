6f1bc98fde1fa7b0f7b51e9056ab945c34de0c86
CedrickFlocon <7161169+CedrickFlocon@users.noreply.github.com>
2019-10-23 14:41:45 +0200

    Cleanup the main activity

--- app/src/main/java/io/homeassistant/android/MainActivity.kt
+++ app/src/main/java/io/homeassistant/android/MainActivity.kt
@@ -2,12 +2,10 @@ package io.homeassistant.android
 
 import android.os.Bundle
 import android.util.Log
-import android.view.WindowManager
 import android.webkit.WebView
 import android.webkit.WebViewClient
 import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
-import androidx.core.content.ContextCompat
 import com.google.android.gms.tasks.OnCompleteListener
 import com.google.firebase.iid.FirebaseInstanceId
 
@@ -18,35 +16,28 @@ class MainActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
-        val window = getWindow()
+        findViewById<WebView>(R.id.webview).apply {
+            settings.javaScriptEnabled = true
+            settings.domStorageEnabled = true
+            webViewClient = WebViewClient()
+            loadUrl("https://demo.home-assistant.io")
+        }
 
-        window.clearFlags(WindowManager.LayoutParams.FLAG_TRANSLUCENT_STATUS)
+        FirebaseInstanceId.getInstance()
+            .instanceId.addOnCompleteListener(OnCompleteListener { task ->
+            if (!task.isSuccessful) {
+                Log.w(TAG, "getInstanceId failed", task.exception)
+                return@OnCompleteListener
+            }
 
-        window.addFlags(WindowManager.LayoutParams.FLAG_DRAWS_SYSTEM_BAR_BACKGROUNDS)
+            // Get new Instance ID token
+            val token = task.result?.token
 
-        window.setStatusBarColor(ContextCompat.getColor(applicationContext, R.color.colorPrimary))
-
-        val myWebView: WebView = findViewById(R.id.webview)
-        myWebView.settings.javaScriptEnabled = true
-        myWebView.settings.domStorageEnabled = true
-        myWebView.webViewClient = WebViewClient()
-        myWebView.loadUrl("https://demo.home-assistant.io")
-
-        FirebaseInstanceId.getInstance().instanceId
-            .addOnCompleteListener(OnCompleteListener { task ->
-                if (!task.isSuccessful) {
-                    Log.w(TAG, "getInstanceId failed", task.exception)
-                    return@OnCompleteListener
-                }
-
-                // Get new Instance ID token
-                val token = task.result?.token
-
-                // Log and toast
-                val msg = getString(R.string.msg_token_fmt, token)
-                Log.d(TAG, msg)
-                Toast.makeText(baseContext, msg, Toast.LENGTH_SHORT).show()
-            })
+            // Log and toast
+            val msg = getString(R.string.msg_token_fmt, token)
+            Log.d(TAG, msg)
+            Toast.makeText(baseContext, msg, Toast.LENGTH_SHORT).show()
+        })
     }
 
     companion object {
