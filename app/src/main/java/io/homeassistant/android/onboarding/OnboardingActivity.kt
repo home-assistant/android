47bc4b0b56e9da9d41780058cd81e1fd3d008d1c
CedrickFlocon <7161169+CedrickFlocon@users.noreply.github.com>
2019-10-26 11:26:33 +0200

    Create onboarding activity

---
+++ app/src/main/java/io/homeassistant/android/onboarding/OnboardingActivity.kt
@@ -0,0 +1,15 @@
+package io.homeassistant.android.onboarding
+
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import io.homeassistant.android.R
+
+
+class OnboardingActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_onboarding)
+    }
+
+}
--- app/src/main/java/io/homeassistant/android/webview/WebviewActivity.kt
+++ app/src/main/java/io/homeassistant/android/webview/WebviewActivity.kt
@@ -1,20 +1,21 @@
-package io.homeassistant.android
+package io.homeassistant.android.webview
 
 import android.os.Bundle
 import android.webkit.WebView
 import android.webkit.WebViewClient
 import androidx.appcompat.app.AppCompatActivity
+import io.homeassistant.android.R
 
 
-class MainActivity : AppCompatActivity() {
+class WebviewActivity : AppCompatActivity() {
 
     companion object {
-        private const val TAG = "MainActivity"
+        private const val TAG = "WebviewActivity"
     }
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        setContentView(R.layout.activity_webview)
 
         findViewById<WebView>(R.id.webview).apply {
             settings.javaScriptEnabled = true
