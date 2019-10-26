52b0868ed06168668341f8526eb97542ccd408d5
CedrickFlocon <7161169+CedrickFlocon@users.noreply.github.com>
2019-10-26 12:21:00 +0200

    Discovery fragment

--- app/src/main/java/io/homeassistant/android/onboarding/OnboardingActivity.kt
+++ app/src/main/java/io/homeassistant/android/onboarding/OnboardingActivity.kt
@@ -5,11 +5,26 @@ import androidx.appcompat.app.AppCompatActivity
 import io.homeassistant.android.R
 
 
-class OnboardingActivity : AppCompatActivity() {
+class OnboardingActivity : AppCompatActivity(), DiscoveryListener {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_onboarding)
+
+        if (savedInstanceState == null) {
+            supportFragmentManager
+                .beginTransaction()
+                .add(R.id.content, DiscoveryFragment.newInstance())
+                .commit()
+        }
+    }
+
+    override fun onSelectManualSetup() {
+        throw NotImplementedError()
+    }
+
+    override fun onHomeAssistantDiscover() {
+        throw NotImplementedError()
     }
 
 }
