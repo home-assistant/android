1727e59ee897f794aaa624dea6de0134cf9285f3
Cedrick Flocon <7161169+CedrickFlocon@users.noreply.github.com>
2019-11-05 15:06:57 +0100

    [FIREBASE] App Distribution (#14)
    * Wrong path to Service Account
    
    * Run Unit test on pr and on branch

--- .travis/before_install.sh
+++ .travis/before_install.sh
@@ -6,8 +6,8 @@ then
     tar xvf .travis/secrets.tar
     mv google-services.json app/google-services.json
     mv upload_keystore.keystore app/release_keystore.keystore
-    mv home-assistant-mobile-apps-5fd6b9dd0fdb.json app/firebaseAppDistributionServiceCredentialsFile.json
     mv home-assistant-mobile-apps-0b13292f44c4.json app/playStorePublishServiceCredentialsFile.json
+    mv home-assistant-mobile-apps-5fd6b9dd0fdb.json firebaseAppDistributionServiceCredentialsFile.json
 else
     mv .travis/mock-google-services.json app/google-services.json
 fi
