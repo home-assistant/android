ad419f42510feb37d39cdf36a35e05e52dacec69
CedrickFlocon <7161169+CedrickFlocon@users.noreply.github.com>
2019-11-08 23:08:29 +0100

    Implement authorization repository

---
+++ data/src/main/java/io/homeassistant/companion/android/data/LocalStorage.kt
@@ -0,0 +1,14 @@
+package io.homeassistant.companion.android.data
+
+
+interface LocalStorage {
+
+    suspend fun putString(key: String, value: String?)
+
+    suspend fun getString(key: String): String?
+
+    suspend fun putLong(key: String, value: Long?)
+
+    suspend fun getLong(key: String): Long?
+
+}
