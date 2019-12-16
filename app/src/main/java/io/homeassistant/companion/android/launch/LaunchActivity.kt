969548ad8cfc90f696a79f22ced7cf943bb769f5
Bram Kragten <mail@bramkragten.nl>
2019-12-16 20:42:04 +0100

    Add back Lokalise SDK (#161)
    * Add back Lokalise SDK
    
    Copy paste bug was fixed in beta 4.
    
    * Lint

--- app/src/main/java/io/homeassistant/companion/android/launch/LaunchActivity.kt
+++ app/src/main/java/io/homeassistant/companion/android/launch/LaunchActivity.kt
@@ -1,8 +1,12 @@
 package io.homeassistant.companion.android.launch
 
+import android.content.Context
 import android.content.Intent
 import android.os.Bundle
+import android.view.MenuInflater
 import androidx.appcompat.app.AppCompatActivity
+import com.lokalise.sdk.LokaliseContextWrapper
+import com.lokalise.sdk.menu_inflater.LokaliseMenuInflater
 import io.homeassistant.companion.android.DaggerPresenterComponent
 import io.homeassistant.companion.android.PresenterModule
 import io.homeassistant.companion.android.background.LocationBroadcastReceiver
@@ -46,5 +50,13 @@ class LaunchActivity : AppCompatActivity(), LaunchView {
     override fun onDestroy() {
         presenter.onFinish()
         super.onDestroy()
+    }
+
+    override fun attachBaseContext(newBase: Context) {
+        super.attachBaseContext(LokaliseContextWrapper.wrap(newBase))
+    }
+
+    override fun getMenuInflater(): MenuInflater {
+        return LokaliseMenuInflater(this)
     }
 }
--- app/src/main/java/io/homeassistant/companion/android/onboarding/OnboardingActivity.kt
+++ app/src/main/java/io/homeassistant/companion/android/onboarding/OnboardingActivity.kt
@@ -3,7 +3,10 @@ package io.homeassistant.companion.android.onboarding
 import android.content.Context
 import android.content.Intent
 import android.os.Bundle
+import android.view.MenuInflater
 import androidx.appcompat.app.AppCompatActivity
+import com.lokalise.sdk.LokaliseContextWrapper
+import com.lokalise.sdk.menu_inflater.LokaliseMenuInflater
 import io.homeassistant.companion.android.R
 import io.homeassistant.companion.android.onboarding.authentication.AuthenticationFragment
 import io.homeassistant.companion.android.onboarding.authentication.AuthenticationListener
@@ -77,6 +80,14 @@ class OnboardingActivity : AppCompatActivity(), DiscoveryListener, ManualSetupLi
 
     override fun onIntegrationRegistrationSkipped() {
         startWebView()
+    }
+
+    override fun attachBaseContext(newBase: Context) {
+        super.attachBaseContext(LokaliseContextWrapper.wrap(newBase))
+    }
+
+    override fun getMenuInflater(): MenuInflater {
+        return LokaliseMenuInflater(this)
     }
 
     private fun startWebView() {
