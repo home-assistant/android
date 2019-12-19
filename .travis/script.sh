a9e6dfa2148eddd3bc5dd5eda3cb17d64a0958fa
Cedrick Flocon <7161169+CedrickFlocon@users.noreply.github.com>
2019-12-19 19:11:10 +0100

    Use git log to get the change log (#178)

--- .travis/script.sh
+++ .travis/script.sh
@@ -2,7 +2,7 @@
 
 set -ev
 
-echo $TRAVIS_COMMIT_MESSAGE > CHANGES.md
+echo `git log --format=%B -n 1 $TRAVIS_COMMIT` > CHANGES.md
 export VERSION_CODE=`git rev-list --count HEAD`
 export VERSION_NAME=`git describe --tags $(git rev-list --tags --max-count=1)`
 
