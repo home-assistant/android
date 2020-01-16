72a15a373a3e0b3310c716c1ceedf1fa6bbf3a83
neopilou <45233613+neopilou@users.noreply.github.com>
2020-01-16 20:34:08 +0100

    Update ViewActivity.kt
    add fullscreen option

--- app/src/main/java/io/homeassistant/companion/android/webview/WebViewActivity.kt
+++ app/src/main/java/io/homeassistant/companion/android/webview/WebViewActivity.kt
@@ -226,6 +226,25 @@ class WebViewActivity : AppCompatActivity(), io.homeassistant.companion.android.
         presenter.onViewReady()
     }
 
+    override fun onWindowFocusChanged(hasFocus: Boolean) {
+        super.onWindowFocusChanged(hasFocus)
+
+        if (hasFocus)
+            if (presenter.isFS("fullscreen"))
+                hideSystemUI()
+            else
+                showSystemUI()
+    }
+
+    private fun hideSystemUI() {
+        window.decorView.systemUiVisibility = (android.view.View.SYSTEM_UI_FLAG_LAYOUT_STABLE
+                or android.view.View.SYSTEM_UI_FLAG_LAYOUT_HIDE_NAVIGATION
+                or android.view.View.SYSTEM_UI_FLAG_LAYOUT_FULLSCREEN
+                or android.view.View.SYSTEM_UI_FLAG_HIDE_NAVIGATION
+                or android.view.View.SYSTEM_UI_FLAG_FULLSCREEN
+                or android.view.View.SYSTEM_UI_FLAG_IMMERSIVE_STICKY)
+    }
+    
     override fun attachBaseContext(newBase: Context) {
         super.attachBaseContext(LokaliseContextWrapper.wrap(newBase))
     }
