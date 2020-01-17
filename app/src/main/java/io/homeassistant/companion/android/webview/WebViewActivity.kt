ddfae68fb3f1388d4684479ff6744a40a9f77048
neopilou <45233613+neopilou@users.noreply.github.com>
2020-01-17 21:58:25 +0100

    Add FullScreen capability

--- app/src/main/java/io/homeassistant/companion/android/webview/WebViewActivity.kt
+++ app/src/main/java/io/homeassistant/companion/android/webview/WebViewActivity.kt
@@ -225,9 +225,9 @@ class WebViewActivity : AppCompatActivity(), io.homeassistant.companion.android.
         }
         
         window.decorView.setOnSystemUiVisibilityChangeListener { visibility ->
-           if (visibility and View.SYSTEM_UI_FLAG_FULLSCREEN == 0)
-                    hideSystemUI()
-       }
+            if (visibility and View.SYSTEM_UI_FLAG_FULLSCREEN == 0)
+                hideSystemUI()
+        }
 
         presenter.onViewReady()
     }
