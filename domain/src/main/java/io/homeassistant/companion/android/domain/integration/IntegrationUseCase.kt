fcf855bdc6f197c7711cce708eb949bf47b64ba3
neopilou <45233613+neopilou@users.noreply.github.com>
2020-01-18 10:47:24 +0100

    Add get/set fun for fullscreen

--- domain/src/main/java/io/homeassistant/companion/android/domain/integration/IntegrationUseCase.kt
+++ domain/src/main/java/io/homeassistant/companion/android/domain/integration/IntegrationUseCase.kt
@@ -25,6 +25,8 @@ interface IntegrationUseCase {
 
     suspend fun setBackgroundTrackingEnabled(enabled: Boolean)
     suspend fun isBackgroundTrackingEnabled(): Boolean
+    
+    suspend fun getThemeColor(): String
 
     suspend fun setFullScreenEnabled(enabled: Boolean)
     suspend fun isFullScreenEnabled(): Boolean
