9e81559a0681026404a8a84af44ef278dcc776e1
Basset, Justin <Bassett.JustinT@gmail.com>
2020-01-17 21:19:41 -0500

    Request we be exempt from Doze.

--- app/src/main/java/io/homeassistant/companion/android/onboarding/integration/MobileAppIntegrationFragment.kt
+++ app/src/main/java/io/homeassistant/companion/android/onboarding/integration/MobileAppIntegrationFragment.kt
@@ -1,6 +1,12 @@
 package io.homeassistant.companion.android.onboarding.integration
 
+import android.annotation.SuppressLint
+import android.content.Intent
+import android.net.Uri
+import android.os.Build
 import android.os.Bundle
+import android.os.PowerManager
+import android.provider.Settings
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
@@ -19,6 +25,8 @@ class MobileAppIntegrationFragment : Fragment(), MobileAppIntegrationView {
     companion object {
         private const val LOADING_VIEW = 0
         private const val ERROR_VIEW = 1
+
+        private const val IGNORE_OPT_REQUEST = 0
 
         fun newInstance(): MobileAppIntegrationFragment {
             return MobileAppIntegrationFragment()
@@ -61,7 +69,8 @@ class MobileAppIntegrationFragment : Fragment(), MobileAppIntegrationView {
             PermissionManager.requestLocationPermissions(this)
         } else {
             // If we have permission already we can just continue with
-            (activity as MobileAppIntegrationListener).onIntegrationRegistrationComplete()
+            presenter.onGrantedLocationPermission(context!!, activity!!)
+            requestBackgroundAccess()
         }
     }
 
@@ -87,8 +96,32 @@ class MobileAppIntegrationFragment : Fragment(), MobileAppIntegrationView {
 
         if (PermissionManager.validateLocationPermissions(requestCode, permissions, grantResults)) {
             presenter.onGrantedLocationPermission(context!!, activity!!)
+            requestBackgroundAccess()
+        } else {
+            (activity as MobileAppIntegrationListener).onIntegrationRegistrationComplete()
         }
+    }
 
-        (activity as MobileAppIntegrationListener).onIntegrationRegistrationComplete()
+    @SuppressLint("BatteryLife")
+    private fun requestBackgroundAccess() {
+        val packageName = activity?.packageName ?: ""
+        val intent: Intent
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.M &&
+            context?.getSystemService(PowerManager::class.java)?.isIgnoringBatteryOptimizations(packageName) == false
+        ) {
+            intent = Intent(
+                Settings.ACTION_REQUEST_IGNORE_BATTERY_OPTIMIZATIONS,
+                Uri.parse("package:${activity?.packageName}")
+            )
+            startActivityForResult(intent, IGNORE_OPT_REQUEST)
+        } else {
+            (activity as MobileAppIntegrationListener).onIntegrationRegistrationComplete()
+        }
+    }
+
+    override fun onActivityResult(requestCode: Int, resultCode: Int, data: Intent?) {
+        if (requestCode == IGNORE_OPT_REQUEST) {
+            (activity as MobileAppIntegrationListener).onIntegrationRegistrationComplete()
+        }
     }
 }
