0fdb1348d1938232929ba8b91bd572f1456fc441
neopilou <45233613+neopilou@users.noreply.github.com>
2020-01-23 10:59:15 +0100

    Add get/set Fullscreen
    Delete trailing spaces

--- data/src/main/java/io/homeassistant/companion/android/data/integration/IntegrationRepositoryImpl.kt
+++ data/src/main/java/io/homeassistant/companion/android/data/integration/IntegrationRepositoryImpl.kt
@@ -183,7 +183,7 @@ class IntegrationRepositoryImpl @Inject constructor(
     override suspend fun isBackgroundTrackingEnabled(): Boolean {
         return localStorage.getBoolean(PREF_BACKGROUND_ENABLED)
     }
-    
+
     override suspend fun setFullScreenEnabled(enabled: Boolean) {
         localStorage.putBoolean(PREF_FULLSCREEN_ENABLED, enabled)
     }
