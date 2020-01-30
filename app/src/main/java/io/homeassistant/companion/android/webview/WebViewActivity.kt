779b4ceff99f3fb43ae63b2dccabac754db58c95
Neonicus <45233613+neopilou@users.noreply.github.com>
2020-01-30 17:19:00 +0100

    Fix Fullscreen for notch (#326)
    * Fix Fullscreen for notch
    
    Test if notch or not, if notch just hide nav bar
    
    * Fix Fullscreen on screen with notch
    
    * Fix Fullscreen for screen with notch
    
    <item name="android:windowLayoutInDisplayCutoutMode" tools:targetApi="27">shortEdges</item> 
    Is for renders content into the cutout area in both portrait and landscape modes therefore useless since in case of a notch we just hide the navigation bar
    
    * Fix Fullscreen for screen with notch
    
    * Fix Fullscreen for screen with notch
    
    * Fix Fullscreen fo screen with notch
    
    * Fix Fullscreen for screen with notch

--- app/src/main/java/io/homeassistant/companion/android/webview/WebViewActivity.kt
+++ app/src/main/java/io/homeassistant/companion/android/webview/WebViewActivity.kt
@@ -247,12 +247,16 @@ class WebViewActivity : AppCompatActivity(), io.homeassistant.companion.android.
     }
 
     private fun hideSystemUI() {
-        window.decorView.systemUiVisibility = (View.SYSTEM_UI_FLAG_LAYOUT_STABLE
-                or View.SYSTEM_UI_FLAG_LAYOUT_HIDE_NAVIGATION
-                or View.SYSTEM_UI_FLAG_LAYOUT_FULLSCREEN
-                or View.SYSTEM_UI_FLAG_HIDE_NAVIGATION
-                or View.SYSTEM_UI_FLAG_FULLSCREEN
-                or View.SYSTEM_UI_FLAG_IMMERSIVE_STICKY)
+        if (isCutout())
+            window.decorView.systemUiVisibility = (View.SYSTEM_UI_FLAG_HIDE_NAVIGATION
+                    or View.SYSTEM_UI_FLAG_IMMERSIVE_STICKY)
+        else
+            window.decorView.systemUiVisibility = (View.SYSTEM_UI_FLAG_LAYOUT_STABLE
+                    or View.SYSTEM_UI_FLAG_LAYOUT_HIDE_NAVIGATION
+                    or View.SYSTEM_UI_FLAG_LAYOUT_FULLSCREEN
+                    or View.SYSTEM_UI_FLAG_HIDE_NAVIGATION
+                    or View.SYSTEM_UI_FLAG_FULLSCREEN
+                    or View.SYSTEM_UI_FLAG_IMMERSIVE_STICKY)
     }
 
     private fun showSystemUI() {
@@ -335,5 +339,12 @@ class WebViewActivity : AppCompatActivity(), io.homeassistant.companion.android.
         } else {
             super.onRequestPermissionsResult(requestCode, permissions, grantResults)
         }
+    }
+
+    private fun isCutout(): Boolean {
+        var cutout = false
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.P && window.decorView.rootWindowInsets.displayCutout != null)
+            cutout = true
+        return cutout
     }
 }
