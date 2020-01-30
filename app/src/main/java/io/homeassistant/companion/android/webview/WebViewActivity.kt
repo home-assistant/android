d45bbec9bf9310bdbe3e32488084085c5d6bf0d8
Neonicus <45233613+neopilou@users.noreply.github.com>
2020-01-30 17:08:01 +0100

    Fix Fullscreen for screen with notch

--- app/src/main/java/io/homeassistant/companion/android/webview/WebViewActivity.kt
+++ app/src/main/java/io/homeassistant/companion/android/webview/WebViewActivity.kt
@@ -1,7 +1,6 @@
 package io.homeassistant.companion.android.webview
 
 import android.annotation.SuppressLint
-import android.annotation.TargetApi
 import android.content.Context
 import android.content.Intent
 import android.net.http.SslError
