75beecf12c3ea1e54519099ac3d8a5b965d913f4
Robbie Trencheny <me@robbiet.us>
2020-02-12 18:27:13 -0800

    Whoops

--- app/src/main/java/io/homeassistant/companion/android/notifications/MessagingService.kt
+++ app/src/main/java/io/homeassistant/companion/android/notifications/MessagingService.kt
@@ -26,6 +26,11 @@ import io.homeassistant.companion.android.domain.integration.IntegrationUseCase
 import io.homeassistant.companion.android.webview.WebViewActivity
 import java.net.URL
 import javax.inject.Inject
+import kotlinx.coroutines.CoroutineScope
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.Job
+import kotlinx.coroutines.launch
+import kotlinx.coroutines.withContext
 
 class MessagingService : FirebaseMessagingService() {
     companion object {
