4a9c6f0283ac74a7a9b03d7c6343427e690ec533
Jeroen Seegers <jeroenseegers@users.noreply.github.com>
2020-03-29 14:16:05 +0200

    Add a seperate battery_state sensor (#530)
    * Remove Battery State related information from Battery Level
    
    * Extract determining battery icon to own method
    
    * Update battery icon method with new parameters
    
    * Add second battery sensor, Battery State
    
    * Calculate battery percentage in own method

--- app/src/main/java/io/homeassistant/companion/android/sensors/BatterySensorManager.kt
+++ app/src/main/java/io/homeassistant/companion/android/sensors/BatterySensorManager.kt
@@ -17,6 +17,7 @@ class BatterySensorManager : SensorManager {
     override fun getSensorRegistrations(context: Context): List<SensorRegistration<Any>> {
         return context.registerReceiver(null, IntentFilter(Intent.ACTION_BATTERY_CHANGED))?.let {
             val retVal = ArrayList<SensorRegistration<Any>>()
+
             getBatteryLevelSensor(it)?.let { sensor ->
                 retVal.add(
                     SensorRegistration(
@@ -28,14 +29,29 @@ class BatterySensorManager : SensorManager {
                 )
             }
 
+            getBatteryStateSensor(it)?.let { sensor ->
+                retVal.add(
+                    SensorRegistration(
+                        sensor,
+                        "Battery State",
+                        "battery"
+                    )
+                )
+            }
+
             return@let retVal
         } ?: listOf()
     }
 
     override fun getSensors(context: Context): List<Sensor<Any>> {
         val retVal = ArrayList<Sensor<Any>>()
+
         context.registerReceiver(null, IntentFilter(Intent.ACTION_BATTERY_CHANGED))?.let {
             getBatteryLevelSensor(it)?.let { sensor ->
+                retVal.add(sensor)
+            }
+
+            getBatteryStateSensor(it)?.let { sensor ->
                 retVal.add(sensor)
             }
         }
@@ -43,45 +59,90 @@ class BatterySensorManager : SensorManager {
         return retVal
     }
 
-    private fun getBatteryLevelSensor(intent: Intent): Sensor<Any>? {
-        val level = intent.getIntExtra(BatteryManager.EXTRA_LEVEL, -1)
-        val scale = intent.getIntExtra(BatteryManager.EXTRA_SCALE, -1)
-        val status: Int = intent.getIntExtra(BatteryManager.EXTRA_STATUS, -1)
+    private fun getBatteryPercentage(level: Int, scale: Int): Int {
+        return (level.toFloat() / scale.toFloat() * 100.0f).toInt()
+    }
 
-        if (level == -1 || scale == -1) {
-            Log.e(TAG, "Issue getting battery level!")
-            return null
-        }
-
-        val isCharging: Boolean = status == BatteryManager.BATTERY_STATUS_CHARGING ||
-                status == BatteryManager.BATTERY_STATUS_FULL
-
-        val chargerType = when (intent.getIntExtra(BatteryManager.EXTRA_PLUGGED, -1)) {
-            BatteryManager.BATTERY_PLUGGED_AC -> "AC"
-            BatteryManager.BATTERY_PLUGGED_USB -> "USB"
-            BatteryManager.BATTERY_PLUGGED_WIRELESS -> "Wireless"
-            else -> "N/A"
-        }
-
-        val percent = (level.toFloat() / scale.toFloat() * 100.0f).toInt()
+    private fun getBatteryIcon(percentage: Int, isCharging: Boolean = false, chargerType: String? = null, chargingStatus: String? = null): String {
         var batteryIcon = "mdi:battery"
+
+        if (chargingStatus == "unknown") {
+            batteryIcon += "-unknown"
+
+            return batteryIcon
+        }
+
         if (isCharging)
             batteryIcon += "-charging"
-        if (chargerType == "Wireless")
+
+        if (chargerType == "wireless")
             batteryIcon += "-wireless"
 
-        val batteryStep = percent / 10
+        val batteryStep: Int = percentage / 10
         batteryIcon += when (batteryStep) {
             0 -> "-outline"
             10 -> ""
             else -> "-${batteryStep}0"
         }
 
+        return batteryIcon
+    }
+
+    private fun getBatteryLevelSensor(intent: Intent): Sensor<Any>? {
+        val level: Int = intent.getIntExtra(BatteryManager.EXTRA_LEVEL, -1)
+        val scale: Int = intent.getIntExtra(BatteryManager.EXTRA_SCALE, -1)
+
+        if (level == -1 || scale == -1) {
+            Log.e(TAG, "Issue getting battery level!")
+            return null
+        }
+
+        val percentage: Int = getBatteryPercentage(level, scale)
+
         return Sensor(
             "battery_level",
-            percent,
+            percentage,
             "sensor",
-            batteryIcon,
+            getBatteryIcon(percentage),
+            mapOf()
+        )
+    }
+
+    private fun getBatteryStateSensor(intent: Intent): Sensor<Any>? {
+        val level: Int = intent.getIntExtra(BatteryManager.EXTRA_LEVEL, -1)
+        val scale: Int = intent.getIntExtra(BatteryManager.EXTRA_SCALE, -1)
+        val status: Int = intent.getIntExtra(BatteryManager.EXTRA_STATUS, -1)
+
+        if (level == -1 || scale == -1 || status == -1) {
+            Log.e(TAG, "Issue getting battery state!")
+            return null
+        }
+
+        val isCharging: Boolean = status == BatteryManager.BATTERY_STATUS_CHARGING ||
+                status == BatteryManager.BATTERY_STATUS_FULL
+
+        val chargerType: String = when (intent.getIntExtra(BatteryManager.EXTRA_PLUGGED, -1)) {
+            BatteryManager.BATTERY_PLUGGED_AC -> "ac"
+            BatteryManager.BATTERY_PLUGGED_USB -> "usb"
+            BatteryManager.BATTERY_PLUGGED_WIRELESS -> "wireless"
+            else -> "unknown"
+        }
+
+        val chargingStatus: String = when (status) {
+            BatteryManager.BATTERY_STATUS_FULL -> "full"
+            BatteryManager.BATTERY_STATUS_CHARGING -> "charging"
+            BatteryManager.BATTERY_STATUS_DISCHARGING -> "discharging"
+            BatteryManager.BATTERY_STATUS_NOT_CHARGING -> "not_charging"
+            else -> "unknown"
+        }
+
+        val percentage: Int = getBatteryPercentage(level, scale)
+
+        return Sensor(
+            "battery_state",
+            chargingStatus,
+            "sensor",
+            getBatteryIcon(percentage, isCharging, chargerType, chargingStatus),
             mapOf(
                 "is_charging" to isCharging,
                 "charger_type" to chargerType
