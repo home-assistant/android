3b2cd4766e475c76e466f543169f1ae4d4ddd30a
Justin Bassett <bassett.justint@gmail.com>
2020-05-04 10:09:53 -0400

    Fix crashes that last shortcuts caused. (#576)
    * Catch the exceptions because panels call doesn't exists yet.

--- app/src/main/java/io/homeassistant/companion/android/settings/shortcuts/ShortcutsPresenterImpl.kt
+++ app/src/main/java/io/homeassistant/companion/android/settings/shortcuts/ShortcutsPresenterImpl.kt
@@ -1,5 +1,6 @@
 package io.homeassistant.companion.android.settings.shortcuts
 
+import android.util.Log
 import io.homeassistant.companion.android.domain.integration.IntegrationUseCase
 import io.homeassistant.companion.android.domain.integration.Panel
 import javax.inject.Inject
@@ -15,7 +16,13 @@ class ShortcutsPresenterImpl @Inject constructor(
 
     override fun getPanels(): Array<Panel> {
         return runBlocking {
-            integrationUseCase.getPanels()
+            var panels = arrayOf<Panel>()
+            try {
+                panels = integrationUseCase.getPanels()
+            } catch (e: Exception) {
+                Log.e(TAG, "Issue getting panels.", e)
+            }
+            panels
         }
     }
 }
