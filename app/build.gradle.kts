6c77280694c3bdb23c084f5d75ab42ade2a6b3b0
Tim <60823285+Timmmeeeh@users.noreply.github.com>
2020-06-10 21:16:11 +0200

    Feature/switch to kotlin dsl (#595)
    * Switch to using Kotlin in of groovy for the gradle configurations.
    
    * Gradle file cleanup.
    
    * Switch to using Kotlin in of groovy for the gradle configurations.
    
    * Gradle file cleanup.
    
    * Retrofit (and maybe others libs) requires java compile options 1.8, still backwards compatible.
    
    * Prep for release 1.9.0-XXX
    
    * Fix ktLint errors.

---
+++ app/build.gradle.kts
@@ -0,0 +1,139 @@
+plugins {
+    id("com.android.application")
+    id("kotlin-android")
+    id("kotlin-kapt")
+    id("kotlin-android-extensions")
+    id("com.google.firebase.appdistribution")
+    id("com.github.triplet.play") version "2.7.5"
+    id("io.fabric")
+}
+
+apply(plugin = "com.google.firebase.appdistribution")
+
+buildscript {
+    repositories {
+        google()
+        maven(url = Config.Repository.gradle)
+    }
+    dependencies {
+        classpath(Config.Plugin.playPublisher)
+        classpath(Config.Plugin.androidJunit5)
+    }
+}
+
+android {
+    compileSdkVersion(Config.Android.compileSdk)
+
+    defaultConfig {
+        applicationId = "io.homeassistant.companion.android"
+        minSdkVersion(Config.Android.minSdk)
+        targetSdkVersion(Config.Android.targetSdk)
+
+        val envCode = System.getenv("VERSION_CODE")
+        val vCode = envCode?.toIntOrNull() ?: 1
+        versionCode = vCode
+        versionName = "${Config.version}-$vCode"
+    }
+
+    viewBinding {
+        isEnabled = true
+    }
+
+    compileOptions {
+        sourceCompatibility = JavaVersion.VERSION_1_8
+        targetCompatibility = JavaVersion.VERSION_1_8
+    }
+
+    firebaseAppDistribution {
+        serviceCredentialsFile = "firebaseAppDistributionServiceCredentialsFile.json"
+        releaseNotesFile = "app/src/main/play/release-notes/en-US/default.txt"
+        groups = "continuous-deployment"
+    }
+
+    signingConfigs {
+        create("release") {
+            storeFile = file("release_keystore.keystore")
+            storePassword = System.getenv("KEYSTORE_PASSWORD") ?: ""
+            keyAlias = System.getenv("KEYSTORE_ALIAS") ?: ""
+            keyPassword = System.getenv("KEYSTORE_ALIAS_PASSWORD") ?: ""
+            isV1SigningEnabled = true
+            isV2SigningEnabled = true
+        }
+    }
+
+    buildTypes {
+        named("debug").configure {
+            applicationIdSuffix = ".debug"
+        }
+        named("release").configure {
+            isDebuggable = false
+            isJniDebuggable = false
+            isZipAlignEnabled = true
+            signingConfig = signingConfigs.getByName("release")
+        }
+    }
+
+    testOptions {
+        unitTests.apply { isReturnDefaultValues = true }
+    }
+
+    tasks.withType<Test> {
+        useJUnitPlatform {
+            includeEngines("spek2")
+        }
+    }
+
+    lintOptions {
+        disable("MissingTranslation")
+    }
+}
+
+play {
+    serviceAccountCredentials = file("playStorePublishServiceCredentialsFile.json")
+    track = "beta"
+    resolutionStrategy = "ignore"
+}
+
+dependencies {
+    implementation(project(":common"))
+    implementation(project(":domain"))
+
+    implementation(Config.Dependency.Kotlin.core)
+    implementation(Config.Dependency.Kotlin.coroutines)
+    implementation(Config.Dependency.Kotlin.coroutinesAndroid)
+
+    implementation(Config.Dependency.Google.dagger)
+    kapt(Config.Dependency.Google.daggerCompiler)
+
+    implementation(Config.Dependency.AndroidX.appcompat)
+    implementation(Config.Dependency.AndroidX.lifecycle)
+    implementation(Config.Dependency.AndroidX.constraintlayout)
+    implementation(Config.Dependency.AndroidX.recyclerview)
+    implementation(Config.Dependency.AndroidX.preference)
+    implementation(Config.Dependency.Google.material)
+
+    implementation(Config.Dependency.Misc.threeTenAbp) {
+        exclude(group = "org.threeten")
+    }
+
+    implementation(Config.Dependency.Misc.crashlytics)
+    implementation(Config.Dependency.Misc.lokalize)
+
+    implementation(Config.Dependency.Play.location)
+    implementation(Config.Dependency.Firebase.core)
+    implementation(Config.Dependency.Firebase.iid)
+    implementation(Config.Dependency.Firebase.messaging)
+
+    implementation(Config.Dependency.AndroidX.workManager)
+    implementation(Config.Dependency.AndroidX.biometric)
+
+    testImplementation(Config.Dependency.Testing.spek2Jvm)
+    testImplementation(Config.Dependency.Testing.spek2JUnit)
+    testImplementation(Config.Dependency.Testing.assertJ)
+    testImplementation(Config.Dependency.Testing.mockk)
+    testImplementation(Config.Dependency.Kotlin.coroutinesTest)
+}
+
+// This plugin must stay at the bottom
+// https://developers.google.com/android/guides/google-services-plugin
+apply(plugin = "com.google.gms.google-services")
---
+++ buildSrc/src/main/kotlin/Config.kt
@@ -0,0 +1,100 @@
+object Config {
+    const val version = "1.9.0"
+
+    object Plugin {
+        const val android = "com.android.tools.build:gradle:3.6.3"
+        const val kotlin = "org.jetbrains.kotlin:kotlin-gradle-plugin:${Dependency.Kotlin.version}"
+        const val google = "com.google.gms:google-services:4.3.3"
+        const val appDistribution = "com.google.firebase:firebase-appdistribution-gradle:1.4.0"
+        const val fabric = "io.fabric.tools:gradle:1.31.2"
+        const val ktlint = "org.jlleitschuh.gradle:ktlint-gradle:9.2.1"
+        const val playPublisher = "com.github.triplet.gradle:play-publisher:2.7.5"
+        const val androidJunit5 = "de.mannodermaus.gradle.plugins:android-junit5:1.6.0.0"
+    }
+
+    object Repository {
+        const val fabric = "https://maven.fabric.io/public"
+        const val gradle = "https://plugins.gradle.org/m2/"
+        const val lokalize = "https://maven.lokalise.co"
+    }
+
+    object Android {
+        const val compileSdk = 29
+        const val minSdk = 21
+        const val targetSdk = 29
+    }
+
+    object Dependency {
+        object Kotlin {
+            const val version = "1.3.72"
+            const val core = "org.jetbrains.kotlin:kotlin-stdlib-jdk8:${version}"
+
+            private const val coroutinesVersion = "1.3.3"
+            const val coroutines = "org.jetbrains.kotlinx:kotlinx-coroutines-core:${coroutinesVersion}"
+            const val coroutinesAndroid = "org.jetbrains.kotlinx:kotlinx-coroutines-android:${coroutinesVersion}"
+            const val coroutinesTest = "org.jetbrains.kotlinx:kotlinx-coroutines-test:${coroutinesVersion}"
+        }
+
+        object Google {
+            private const val daggerVersion = "2.27"
+            const val dagger = "com.google.dagger:dagger:${daggerVersion}"
+            const val daggerCompiler = "com.google.dagger:dagger-compiler:${daggerVersion}"
+
+            const val material = "com.google.android.material:material:1.1.0"
+        }
+
+        object AndroidX {
+            const val appcompat = "androidx.appcompat:appcompat:1.1.0"
+            const val lifecycle = "androidx.lifecycle:lifecycle-runtime-ktx:2.2.0"
+            const val recyclerview = "androidx.recyclerview:recyclerview:1.1.0"
+            const val constraintlayout = "androidx.constraintlayout:constraintlayout:1.1.3"
+            const val preference = "androidx.preference:preference-ktx:1.1.1"
+
+            const val workManager = "androidx.work:work-runtime-ktx:2.3.4"
+            const val biometric = "androidx.biometric:biometric:1.0.1"
+        }
+
+        object Play {
+            const val location = "com.google.android.gms:play-services-location:17.0.0"
+        }
+
+        object Firebase {
+            const val core = "com.google.firebase:firebase-core:17.3.0"
+            const val iid = "com.google.firebase:firebase-iid:20.1.5"
+            const val messaging = "com.google.firebase:firebase-messaging:20.1.5"
+        }
+
+        object Square {
+            private const val retrofitVersion = "2.8.1"
+            const val retrofit = "com.squareup.retrofit2:retrofit:${retrofitVersion}"
+            const val retrofitJacksonConverter = "com.squareup.retrofit2:converter-jackson:${retrofitVersion}"
+
+            private const val okhttpVersion = "4.5.0"
+            const val okhttp = "com.squareup.okhttp3:okhttp:$okhttpVersion"
+            const val okhttpInterceptor = "com.squareup.okhttp3:logging-interceptor:$okhttpVersion"
+            const val okhttpMockServer = "com.squareup.okhttp3:mockwebserver:$okhttpVersion"
+        }
+
+        object Testing {
+            private const val assertJVersion = "3.13.2"
+            const val assertJ = "org.assertj:assertj-core:${assertJVersion}"
+
+            private const val mockkVersion = "1.9.3"
+            const val mockk = "io.mockk:mockk:${mockkVersion}"
+
+            const val spek2Version = "2.0.8"
+            const val spek2Jvm = "org.spekframework.spek2:spek-dsl-jvm:${spek2Version}"
+            const val spek2JUnit = "org.spekframework.spek2:spek-runner-junit5:${spek2Version}"
+        }
+
+        object Misc {
+            const val crashlytics = "com.crashlytics.sdk.android:crashlytics:2.10.1"
+            const val lokalize = "com.lokalise.android:sdk:2.0.0-beta-5"
+            const val jackson = "com.fasterxml.jackson.module:jackson-module-kotlin:2.10.1"
+            const val threeTenBp = "org.threeten:threetenbp:1.4.0"
+            const val threeTenAbp = "com.jakewharton.threetenabp:threetenabp:1.2.1"
+            const val javaxInject = "javax.inject:javax.inject:1"
+        }
+    }
+
+}
