f6219d90ccbbb782454546cbb409c347b8e2c743
Timmmeeeh <timmeeeeh@outlook.com>
2020-06-10 19:19:50 +0200

    Also add google-services.json to wear module, use cp instead of mv.

--- .travis/before_install.sh
+++ .travis/before_install.sh
@@ -8,10 +8,13 @@ then
     tar xvf .travis/secrets.tar
     cp google-services.json app/google-services.json
     cp google-services.json wear/google-services.json
-    mv upload_keystore.keystore app/release_keystore.keystore
-    mv home-assistant-mobile-apps-0b13292f44c4.json app/playStorePublishServiceCredentialsFile.json
+    cp upload_keystore.keystore app/release_keystore.keystore
+    cp upload_keystore.keystore wear/release_keystore.keystore
+    cp home-assistant-mobile-apps-0b13292f44c4.json app/playStorePublishServiceCredentialsFile.json
+    cp home-assistant-mobile-apps-0b13292f44c4.json app/playStorePublishServiceCredentialsFile.json
     mv home-assistant-mobile-apps-5fd6b9dd0fdb.json firebaseAppDistributionServiceCredentialsFile.json
-    mv .travis/mock-google-services.json app/src/debug/google-services.json
+    cp .travis/mock-google-services.json app/src/debug/google-services.json
+    cp .travis/mock-google-services.json wear/src/debug/google-services.json
 else
     cp .travis/mock-google-services.json app/google-services.json
     cp .travis/mock-google-services.json wear/google-services.json
