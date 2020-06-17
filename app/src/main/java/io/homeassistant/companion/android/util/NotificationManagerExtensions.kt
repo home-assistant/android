4097d290d77aaa3254c0c6d21acd82968dd14555
hasenbolle <edgi@arcor.de>
2020-06-17 21:04:39 +0200

    Improved cancellation of notifications
    * If only the group and the notification inside should be canceled, then the whole group gets now canceled,
    instead of first cancel the notification and then the group. If the group is canceled, all notification inside of the group gets canceled.
    With this logic we save one call of cancel method.
    
    * getActiveNotifications is called only once now

--- app/src/main/java/io/homeassistant/companion/android/util/NotificationManagerExtensions.kt
+++ app/src/main/java/io/homeassistant/companion/android/util/NotificationManagerExtensions.kt
@@ -1,36 +1,38 @@
 package io.homeassistant.companion.android.util
 
-import android.annotation.SuppressLint
 import android.app.NotificationManager
 import android.os.Build
 
-@SuppressLint("NewApi")
 fun NotificationManager.cancel(tag: String?, id: Int, cancelGroup: Boolean) {
 
-    var groupKey: String? = null
-    var doCancelGroup = cancelGroup && Build.VERSION.SDK_INT >= Build.VERSION_CODES.N
+    if (cancelGroup && Build.VERSION.SDK_INT >= Build.VERSION_CODES.N) {
+        var currentActiveNotifications = this.activeNotifications
 
-    if (doCancelGroup) {
         // Get group key from current notification
         // to handle possible group deletion
-        groupKey =
-            this.activeNotifications.singleOrNull { s -> s.id == id && s.tag == tag && s.isGroup }?.groupKey
+        var groupKey =
+            currentActiveNotifications.singleOrNull { s -> s.id == id && s.tag == tag && s.isGroup }?.groupKey
+
+        // Notification has a group?
+        if (!groupKey.isNullOrBlank()) {
+            // Yes it has a group get notifications of the group...
+            val groupNotifications =
+                currentActiveNotifications.filter { s -> s.groupKey == groupKey }
+
+            // If the are only two left notifications, then this means only the current to be
+            // canceled notification AND the group of the notification is left
+            // If we cancel the group of the notification, the notifications inside of the group
+            // will be canceled too.
+            if (groupNotifications.size == 2) {
+                val group = groupNotifications[0].notification.group
+                val groupId = group.hashCode()
+                this.cancel(group, groupId)
+                return
+            }
+        }
     }
 
     // Clear notification
     this.cancel(tag, id)
 
-    if (doCancelGroup && !groupKey.isNullOrBlank()) {
-        // Get notifications of the group
-        val groupNotifications =
-            this.activeNotifications.filter { s -> s.groupKey == groupKey }
-
-        // Only one left. That means. Just the group itself is left.
-        // Then clear the group
-        if (groupNotifications.size == 1) {
-            val group = groupNotifications[0].notification.group
-            val groupId = group.hashCode()
-            this.cancel(group, groupId)
-        }
-    }
 }
