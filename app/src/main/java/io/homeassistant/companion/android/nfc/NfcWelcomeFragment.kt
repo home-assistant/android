5635766f66f85118d78954ed9842bb4292633e07
Justin Bassett <bassett.justint@gmail.com>
2020-08-16 14:45:36 -0400

    Remove commented code

--- app/src/main/java/io/homeassistant/companion/android/nfc/NfcWelcomeFragment.kt
+++ app/src/main/java/io/homeassistant/companion/android/nfc/NfcWelcomeFragment.kt
@@ -46,7 +46,6 @@ class NfcWelcomeFragment : Fragment() {
         }
 
         btn_nfc_write.setOnClickListener {
-//            findNavController().navigate(R.id.action_NFC_WRITE)
             viewModel.postNewUUID()
         }
     }
