47a88000e94f122f4f2ea966d72301e653a43399
Justin Bassett <bassett.justint@gmail.com>
2020-08-16 15:59:08 -0400

    Fix issue with Room DB migrations. (#778)
    * Fix issue with Room DB migrations.
    
    * Add migration for sensors.

--- app/src/main/java/io/homeassistant/companion/android/database/AppDatabase.kt
+++ app/src/main/java/io/homeassistant/companion/android/database/AppDatabase.kt
@@ -4,6 +4,8 @@ import android.content.Context
 import androidx.room.Database
 import androidx.room.Room
 import androidx.room.RoomDatabase
+import androidx.room.migration.Migration
+import androidx.sqlite.db.SupportSQLiteDatabase
 import io.homeassistant.companion.android.database.authentication.Authentication
 import io.homeassistant.companion.android.database.authentication.AuthenticationDao
 import io.homeassistant.companion.android.database.sensor.Sensor
@@ -14,7 +16,7 @@ import io.homeassistant.companion.android.database.sensor.SensorDao
         Authentication::class,
         Sensor::class
     ],
-    version = 1
+    version = 2
 )
 abstract class AppDatabase : RoomDatabase() {
     abstract fun authenticationDao(): AuthenticationDao
@@ -22,18 +24,31 @@ abstract class AppDatabase : RoomDatabase() {
 
     companion object {
         private const val DATABASE_NAME = "HomeAssistantDB"
-        @Volatile private var instance: AppDatabase? = null
+        @Volatile
+        private var instance: AppDatabase? = null
 
         fun getInstance(context: Context): AppDatabase {
             return instance ?: synchronized(this) {
                 instance ?: buildDatabase(context).also { instance = it }
             }
         }
+
         private fun buildDatabase(context: Context): AppDatabase {
             return Room
                 .databaseBuilder(context, AppDatabase::class.java, DATABASE_NAME)
                 .allowMainThreadQueries()
+                .addMigrations(MIGRATION_1_2)
                 .build()
+        }
+
+        private val MIGRATION_1_2 = object : Migration(1, 2) {
+            override fun migrate(database: SupportSQLiteDatabase) {
+                database.execSQL(
+                    "CREATE TABLE IF NOT EXISTS `sensors` (`unique_id` TEXT NOT NULL, " +
+                            "`enabled` INTEGER NOT NULL, `registered` INTEGER NOT NULL, " +
+                            "`state` TEXT NOT NULL, PRIMARY KEY(`unique_id`))"
+                )
+            }
         }
     }
 }
