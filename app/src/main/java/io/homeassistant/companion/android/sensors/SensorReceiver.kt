9cae26b771ca8cd5862a07ee9b6e629ee7dea67e
Daniel Shokouhi <dshokouhi@gmail.com>
2020-08-25 20:04:25 -0700

    Add do not disturb sensor (#813)
    * Initial commit of DND sensor
    
    * Update sensor name
    
    * Adapt to new sensor model
    
    * Review comments

--- app/src/main/java/io/homeassistant/companion/android/sensors/SensorReceiver.kt
+++ app/src/main/java/io/homeassistant/companion/android/sensors/SensorReceiver.kt
@@ -24,6 +24,7 @@ class SensorReceiver : BroadcastReceiver() {
             AudioSensorManager(),
             BatterySensorManager(),
             BluetoothSensorManager(),
+            DNDSensorManager(),
             GeocodeSensorManager(),
             LastRebootSensorManager(),
             LightSensorManager(),
