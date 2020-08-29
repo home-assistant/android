7dde8bba7ba4bb9b50ada46e265cd47bea634949
Anna Tikhomirova <vamp@vampik.ru>
2020-08-30 00:02:45 +0300

    Remove excess calls to setupLocationTracking (#843)
    * Remove excess calls to setupLocationTracking
    
    * Remove unused import

--- app/src/main/java/io/homeassistant/companion/android/webview/WebViewActivity.kt
+++ app/src/main/java/io/homeassistant/companion/android/webview/WebViewActivity.kt
@@ -59,7 +59,6 @@ import io.homeassistant.companion.android.database.AppDatabase
 import io.homeassistant.companion.android.database.authentication.Authentication
 import io.homeassistant.companion.android.nfc.NfcSetupActivity
 import io.homeassistant.companion.android.onboarding.OnboardingActivity
-import io.homeassistant.companion.android.sensors.LocationSensorManager
 import io.homeassistant.companion.android.sensors.SensorWorker
 import io.homeassistant.companion.android.settings.SettingsActivity
 import io.homeassistant.companion.android.themes.ThemesManager
@@ -131,10 +130,6 @@ class WebViewActivity : AppCompatActivity(), io.homeassistant.companion.android.
 
         // Start the sensor worker if they start the app. The only other place we start this ia Boot BroadcastReceiver
         SensorWorker.start(this)
-
-        val intent = Intent(this, LocationSensorManager::class.java)
-        intent.action = LocationSensorManager.ACTION_REQUEST_LOCATION_UPDATES
-        sendBroadcast(intent)
 
         if (BuildConfig.DEBUG) {
             WebView.setWebContentsDebuggingEnabled(true)
