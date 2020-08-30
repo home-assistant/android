a03551d6cc53e5ffcd572eca83b1be86d6abd337
Daniel Shokouhi <dshokouhi@gmail.com>
2020-08-29 19:35:47 -0700

    Review comments

--- app/src/main/java/io/homeassistant/companion/android/settings/SettingsFragment.kt
+++ app/src/main/java/io/homeassistant/companion/android/settings/SettingsFragment.kt
@@ -129,10 +129,6 @@ class SettingsFragment : PreferenceFragmentCompat(), SettingsView {
             return@setOnPreferenceClickListener true
         }
 
-        findPreference<Preference>("docs")?.let {
-            it.isCopyingEnabled = true
-        }
-
         findPreference<Preference>("version")?.let {
             it.isCopyingEnabled = true
             it.summary = BuildConfig.VERSION_NAME
