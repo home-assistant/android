68b8987704441b89efeb03675f703700e034f108
Daniel Shokouhi <dshokouhi@gmail.com>
2020-09-01 21:44:13 -0700

    Use a foreground service for the sensor worker to make updates more reliable (#854)

--- app/src/main/java/io/homeassistant/companion/android/sensors/SensorWorker.kt
+++ app/src/main/java/io/homeassistant/companion/android/sensors/SensorWorker.kt
@@ -1,14 +1,21 @@
 package io.homeassistant.companion.android.sensors
 
+import android.app.NotificationChannel
+import android.app.NotificationManager
 import android.content.Context
+import android.content.Context.NOTIFICATION_SERVICE
+import android.os.Build
 import android.util.Log
+import androidx.core.app.NotificationCompat
 import androidx.work.Constraints
 import androidx.work.CoroutineWorker
 import androidx.work.ExistingPeriodicWorkPolicy
+import androidx.work.ForegroundInfo
 import androidx.work.NetworkType
 import androidx.work.PeriodicWorkRequestBuilder
 import androidx.work.WorkManager
 import androidx.work.WorkerParameters
+import io.homeassistant.companion.android.R
 import io.homeassistant.companion.android.common.dagger.GraphComponentAccessor
 import io.homeassistant.companion.android.domain.integration.IntegrationUseCase
 import java.util.concurrent.TimeUnit
@@ -22,10 +29,15 @@ class SensorWorker(
 ) : CoroutineWorker(appContext, workerParams) {
     companion object {
         private const val TAG = "SensorWorker"
+        const val channelId = "Sensor Worker"
+        const val NOTIFICATION_ID = 42
+        var notificationText = ""
+
         fun start(context: Context) {
             val constraints = Constraints.Builder()
                 .setRequiredNetworkType(NetworkType.CONNECTED).build()
 
+            notificationText = context.getString(R.string.updating_sensors)
             val sensorWorker =
                 PeriodicWorkRequestBuilder<SensorWorker>(15, TimeUnit.MINUTES)
                     .setConstraints(constraints)
@@ -38,6 +50,8 @@ class SensorWorker(
     @Inject
     lateinit var integrationUseCase: IntegrationUseCase
 
+    private val notificationManager = appContext.getSystemService(NOTIFICATION_SERVICE) as NotificationManager
+
     init {
         DaggerSensorComponent.builder()
             .appComponent((appContext.applicationContext as GraphComponentAccessor).appComponent)
@@ -47,7 +61,30 @@ class SensorWorker(
 
     override suspend fun doWork(): Result = withContext(Dispatchers.IO) {
         Log.d(TAG, "Updating all Sensors.")
+        createNotificationChannel()
+        val notification = NotificationCompat.Builder(applicationContext, channelId)
+            .setSmallIcon(R.drawable.ic_stat_ic_notification)
+            .setContentTitle(notificationText)
+            .setPriority(NotificationCompat.PRIORITY_LOW)
+            .build()
+
+        val foregroundInfo = ForegroundInfo(NOTIFICATION_ID, notification)
+        setForeground(foregroundInfo)
+
         SensorReceiver().updateSensors(appContext, integrationUseCase)
         Result.success()
+    }
+
+    private fun createNotificationChannel() {
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.O) {
+            var notificationChannel =
+                notificationManager?.getNotificationChannel(channelId)
+            if (notificationChannel == null) {
+                notificationChannel = NotificationChannel(
+                    channelId, TAG, NotificationManager.IMPORTANCE_LOW
+                )
+                notificationManager?.createNotificationChannel(notificationChannel)
+            }
+        }
     }
 }
