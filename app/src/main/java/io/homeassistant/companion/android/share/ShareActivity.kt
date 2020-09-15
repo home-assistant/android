8f68b9dbbb309e06e43f34cb0f251e66711fa60f
Basset, Justin <Bassett.JustinT@gmail.com>
2020-09-15 00:16:32 -0400

    Add caller package and subject if present.

--- app/src/main/java/io/homeassistant/companion/android/share/ShareActivity.kt
+++ app/src/main/java/io/homeassistant/companion/android/share/ShareActivity.kt
@@ -3,12 +3,15 @@ package io.homeassistant.companion.android.share
 import android.content.Intent
 import android.os.Bundle
 import android.util.Log
+import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
+import androidx.core.app.ActivityCompat
+import io.homeassistant.companion.android.R
 import io.homeassistant.companion.android.common.dagger.GraphComponentAccessor
 import io.homeassistant.companion.android.common.data.integration.IntegrationRepository
-import java.lang.Exception
 import javax.inject.Inject
 import kotlinx.coroutines.runBlocking
+import okhttp3.HttpUrl.Companion.toHttpUrlOrNull
 
 class ShareActivity : AppCompatActivity() {
 
@@ -27,17 +30,37 @@ class ShareActivity : AppCompatActivity() {
             .build()
             .inject(this)
 
+        val data = mutableMapOf(
+            "caller" to ActivityCompat.getReferrer(this).toString()
+        )
+
         if (intent?.action == Intent.ACTION_SEND) {
+            intent.getStringExtra(Intent.EXTRA_SUBJECT)?.let {
+                data["subject"] = it
+            }
             intent.getStringExtra(Intent.EXTRA_TEXT)?.let {
-                Log.d(TAG, "Got share request for: $it")
-                runBlocking {
-                    try {
-                        integrationRepository.fireEvent("mobile_app.share", mapOf("text" to it))
-                        Log.d(TAG, "Share successful!")
-                    } catch (e: Exception) {
-                        Log.e(TAG, "Issue sharing with Home Assistant", e)
-                    }
-                }
+                if (it.toHttpUrlOrNull() == null)
+                    data["text"] = it
+                else
+                    data["url"] = it
+            }
+        }
+        runBlocking {
+            try {
+                integrationRepository.fireEvent("mobile_app.share", data)
+                Log.d(TAG, "Share successful!")
+                Toast.makeText(
+                    applicationContext,
+                    R.string.share_success,
+                    Toast.LENGTH_SHORT
+                ).show()
+            } catch (e: Exception) {
+                Log.e(TAG, "Issue sharing with Home Assistant", e)
+                Toast.makeText(
+                    applicationContext,
+                    R.string.share_failed,
+                    Toast.LENGTH_LONG
+                ).show()
             }
         }
         finish()
