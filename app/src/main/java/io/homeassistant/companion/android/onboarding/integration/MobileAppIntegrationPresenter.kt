abfe0d04f807fcdfb2fd12abfa5a92ebd1a21435
Basset, Justin <Bassett.JustinT@gmail.com>
2020-09-29 09:13:13 -0400

    First screen working as designed.

--- app/src/main/java/io/homeassistant/companion/android/onboarding/integration/MobileAppIntegrationPresenter.kt
+++ app/src/main/java/io/homeassistant/companion/android/onboarding/integration/MobileAppIntegrationPresenter.kt
@@ -1,6 +1,6 @@
 package io.homeassistant.companion.android.onboarding.integration
 
 interface MobileAppIntegrationPresenter {
-    fun onRegistrationAttempt(simple: Boolean)
+    fun onRegistrationAttempt(simple: Boolean, deviceName: String)
     fun onFinish()
 }
