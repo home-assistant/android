1209d5956f1e8ca17bfff9f0f7ab20d3164f5b04
Daniel Shokouhi <dshokouhi@gmail.com>
2020-10-02 10:06:37 -0700

    Send an event and post a notification when the migration fails

--- app/src/main/java/io/homeassistant/companion/android/database/AppDatabase.kt
+++ app/src/main/java/io/homeassistant/companion/android/database/AppDatabase.kt
@@ -1,14 +1,22 @@
 package io.homeassistant.companion.android.database
 
+import android.app.NotificationChannel
+import android.app.NotificationManager
 import android.content.ContentValues
 import android.content.Context
+import android.os.Build
 import android.util.Log
+import android.widget.Toast
+import androidx.core.app.NotificationCompat
+import androidx.core.app.NotificationManagerCompat
 import androidx.room.Database
 import androidx.room.OnConflictStrategy
 import androidx.room.Room
 import androidx.room.RoomDatabase
 import androidx.room.migration.Migration
 import androidx.sqlite.db.SupportSQLiteDatabase
+import io.homeassistant.companion.android.R
+import io.homeassistant.companion.android.common.data.integration.IntegrationRepository
 import io.homeassistant.companion.android.database.authentication.Authentication
 import io.homeassistant.companion.android.database.authentication.AuthenticationDao
 import io.homeassistant.companion.android.database.sensor.Attribute
@@ -21,6 +29,7 @@ import io.homeassistant.companion.android.database.widget.StaticWidgetDao
 import io.homeassistant.companion.android.database.widget.StaticWidgetEntity
 import io.homeassistant.companion.android.database.widget.TemplateWidgetDao
 import io.homeassistant.companion.android.database.widget.TemplateWidgetEntity
+import kotlinx.coroutines.runBlocking
 
 @Database(
     entities = [
@@ -44,17 +53,23 @@ abstract class AppDatabase : RoomDatabase() {
     companion object {
         private const val DATABASE_NAME = "HomeAssistantDB"
         internal const val TAG = "AppDatabase"
+        const val channelId = "App Database"
+        const val NOTIFICATION_ID = 45
+        lateinit var appContext: Context
+        lateinit var integrationRepository: IntegrationRepository
 
         @Volatile
         private var instance: AppDatabase? = null
 
         fun getInstance(context: Context): AppDatabase {
+            appContext = context
             return instance ?: synchronized(this) {
                 instance ?: buildDatabase(context).also { instance = it }
             }
         }
 
         private fun buildDatabase(context: Context): AppDatabase {
+            appContext = context
             return Room
                 .databaseBuilder(context, AppDatabase::class.java, DATABASE_NAME)
                 .allowMainThreadQueries()
@@ -162,6 +177,24 @@ abstract class AppDatabase : RoomDatabase() {
                         migrationSuccessful = true
                     } catch (e: Exception) {
                         Log.e(TAG, "Unable to migrate, proceeding with recreating the table", e)
+                        createNotificationChannel()
+                        val notification = NotificationCompat.Builder(appContext, channelId)
+                            .setSmallIcon(R.drawable.ic_stat_ic_notification)
+                            .setContentTitle(appContext.getString(R.string.database_migration_failed))
+                            .setPriority(NotificationCompat.PRIORITY_HIGH)
+                            .build()
+                        with(NotificationManagerCompat.from(appContext)) {
+                            notify(NOTIFICATION_ID, notification)
+                        }
+                        runBlocking {
+                            try {
+                                integrationRepository.fireEvent("mobile_app.migration_failed", mapOf())
+                                Log.d(TAG, "Event sent to Home Assistant")
+                            } catch (e: Exception) {
+                                Log.e(TAG, "Unable to send event to Home Assistant", e)
+                                Toast.makeText(appContext, R.string.database_event_failure, Toast.LENGTH_LONG).show()
+                            }
+                        }
                     }
                 }
                 cursor.close()
@@ -212,6 +245,24 @@ abstract class AppDatabase : RoomDatabase() {
                         migrationSuccessful = true
                     } catch (e: Exception) {
                         Log.e(TAG, "Unable to migrate, proceeding with recreating the table", e)
+                        createNotificationChannel()
+                        val notification = NotificationCompat.Builder(appContext, channelId)
+                            .setSmallIcon(R.drawable.ic_stat_ic_notification)
+                            .setContentTitle(appContext.getString(R.string.database_migration_failed))
+                            .setPriority(NotificationCompat.PRIORITY_HIGH)
+                            .build()
+                        with(NotificationManagerCompat.from(appContext)) {
+                            notify(NOTIFICATION_ID, notification)
+                        }
+                        runBlocking {
+                            try {
+                                integrationRepository.fireEvent("mobile_app.migration_failed", mapOf())
+                                Log.d(TAG, "Event sent to Home Assistant")
+                            } catch (e: Exception) {
+                                Log.e(TAG, "Unable to send event to Home Assistant", e)
+                                Toast.makeText(appContext, R.string.database_event_failure, Toast.LENGTH_LONG).show()
+                            }
+                        }
                     }
                 }
                 cursor.close()
@@ -228,6 +279,21 @@ abstract class AppDatabase : RoomDatabase() {
         private val MIGRATION_10_11 = object : Migration(10, 11) {
             override fun migrate(database: SupportSQLiteDatabase) {
                 database.execSQL("CREATE TABLE IF NOT EXISTS `sensor_settings` (`sensor_id` TEXT NOT NULL, `name` TEXT NOT NULL, `value` TEXT NOT NULL, `value_type` TEXT NOT NULL DEFAULT 'string', PRIMARY KEY(`sensor_id`, `name`))")
+            }
+        }
+
+        private fun createNotificationChannel() {
+            if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.O) {
+                val notificationManager = appContext.getSystemService(Context.NOTIFICATION_SERVICE) as NotificationManager
+
+                var notificationChannel =
+                    notificationManager?.getNotificationChannel(channelId)
+                if (notificationChannel == null) {
+                    notificationChannel = NotificationChannel(
+                        channelId, TAG, NotificationManager.IMPORTANCE_HIGH
+                    )
+                    notificationManager?.createNotificationChannel(notificationChannel)
+                }
             }
         }
     }
