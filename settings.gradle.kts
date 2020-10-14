d4a89cefacec2774cc61e0d95397b4f4b82b6bad
Nelson Osacky <nosacky@gradle.com>
2020-10-14 18:43:16 +0200

    Add Gradle Build Scans
    To discover insights in to which dependencies are pulling in older versions of kotlin.
    For example:
    https://scans.gradle.com/s/l3vm2auntthac/dependencies?dependencies=reflect&expandAll&focusedDependency=WzAsMyw3MzcsWzAsMCxbMV1dXQ&focusedDependencyIncomingPathsToggled=W1sxLDAsMV1d&focusedDependencyView=versions

--- settings.gradle.kts
+++ settings.gradle.kts
@@ -1,3 +1,15 @@
 include(":common", ":app")
 
 rootProject.name = "home-assistant-android"
+
+plugins {
+    id("com.gradle.enterprise").version("3.4.1")
+}
+
+gradleEnterprise {
+    buildScan {
+        termsOfServiceUrl = "https://gradle.com/terms-of-service"
+        termsOfServiceAgree = "yes"
+        publishAlways()
+    }
+}
