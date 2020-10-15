1433734cdfc90fc94d5901499c4114b47be7d948
Daniel Shokouhi <dshokouhi@gmail.com>
2020-10-14 21:25:04 -0700

    Attempt to fix minimal errors again

---
+++ app/src/minimal/java/io/homeassistant/companion/android/settings/notification/NotificationHistoryFragment.kt
@@ -0,0 +1,16 @@
+package io.homeassistant.companion.android.settings.notification
+
+import android.os.Bundle
+
+class NotificationHistoryFragment : PreferenceFragmentCompat() {
+
+    companion object {
+        fun newInstance(): NotificationHistoryFragment {
+            // No op
+        }
+    }
+
+    override fun onCreatePreferences(savedInstanceState: Bundle?, rootKey: String?) {
+        // No op
+    }
+}
