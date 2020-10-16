c620bbabd233ffc91635743261844549aac0a878
Daniel Shokouhi <dshokouhi@gmail.com>
2020-10-15 17:49:00 -0700

    Allow notifications from history to be deleted one at a time or all at once (#1058)

--- app/src/main/java/io/homeassistant/companion/android/settings/notification/NotificationDetailFragment.kt
+++ app/src/main/java/io/homeassistant/companion/android/settings/notification/NotificationDetailFragment.kt
@@ -4,6 +4,7 @@ import android.os.Bundle
 import androidx.preference.Preference
 import androidx.preference.PreferenceFragmentCompat
 import io.homeassistant.companion.android.R
+import io.homeassistant.companion.android.database.AppDatabase
 import io.homeassistant.companion.android.database.notification.NotificationItem
 import java.util.Calendar
 import java.util.GregorianCalendar
@@ -24,6 +25,7 @@ class NotificationDetailFragment(
     override fun onCreatePreferences(savedInstanceState: Bundle?, rootKey: String?) {
 
         addPreferencesFromResource(R.xml.notification_detail)
+
         findPreference<Preference>("received_at")?.let {
             val cal: Calendar = GregorianCalendar()
             cal.timeInMillis = notification.received
@@ -37,5 +39,40 @@ class NotificationDetailFragment(
         findPreference<Preference>("data")?.let {
             it.summary = notification.data
         }
+
+        findPreference<Preference>("delete_notification")?.let {
+            it.setOnPreferenceClickListener {
+
+                deleteConfirmation()
+
+                return@setOnPreferenceClickListener true
+            }
+        }
+    }
+
+    private fun deleteConfirmation() {
+        val notificationDao = AppDatabase.getInstance(requireContext()).notificationDao()
+
+        val builder: android.app.AlertDialog.Builder = android.app.AlertDialog.Builder(requireContext())
+
+        builder.setTitle(R.string.confirm_delete_this_notification_title)
+        builder.setMessage(R.string.confirm_delete_this_notification_message)
+
+        builder.setPositiveButton(
+            R.string.confirm_positive
+        ) { dialog, _ ->
+            notificationDao.delete(notification.id)
+            dialog.dismiss()
+            parentFragmentManager.popBackStack()
+        }
+
+        builder.setNegativeButton(
+            R.string.confirm_negative
+        ) { dialog, _ -> // Do nothing
+            dialog.dismiss()
+        }
+
+        val alert: android.app.AlertDialog? = builder.create()
+        alert?.show()
     }
 }
--- app/src/main/java/io/homeassistant/companion/android/settings/notification/NotificationHistoryFragment.kt
+++ app/src/main/java/io/homeassistant/companion/android/settings/notification/NotificationHistoryFragment.kt
@@ -1,11 +1,13 @@
 package io.homeassistant.companion.android.settings.notification
 
+import android.app.AlertDialog
 import android.os.Bundle
 import androidx.preference.Preference
 import androidx.preference.PreferenceCategory
 import androidx.preference.PreferenceFragmentCompat
 import io.homeassistant.companion.android.R
 import io.homeassistant.companion.android.database.AppDatabase
+import io.homeassistant.companion.android.database.notification.NotificationDao
 import java.util.Calendar
 import java.util.GregorianCalendar
 
@@ -18,16 +20,19 @@ class NotificationHistoryFragment : PreferenceFragmentCompat() {
     }
 
     override fun onCreatePreferences(savedInstanceState: Bundle?, rootKey: String?) {
-
         setPreferencesFromResource(R.xml.notifications, rootKey)
+    }
+
+    override fun onResume() {
+        super.onResume()
+
         val notificationDao = AppDatabase.getInstance(requireContext()).notificationDao()
         val notificationList = notificationDao.getLast25()
 
-        val prefCategory = PreferenceCategory(preferenceScreen.context)
+        val prefCategory = findPreference<PreferenceCategory>("list_notifications")
         if (!notificationList.isNullOrEmpty()) {
-            prefCategory.title = requireContext().getString(R.string.last_25_notifications)
-            prefCategory.isIconSpaceReserved = false
-            preferenceScreen.addPreference(prefCategory)
+            prefCategory?.isVisible = true
+            prefCategory?.removeAll()
             for (item in notificationList) {
                 val pref = Preference(preferenceScreen.context)
                 val cal: Calendar = GregorianCalendar()
@@ -51,12 +56,54 @@ class NotificationHistoryFragment : PreferenceFragmentCompat() {
                     return@setOnPreferenceClickListener true
                 }
 
-                prefCategory.addPreference(pref)
+                prefCategory?.addPreference(pref)
+            }
+
+            findPreference<PreferenceCategory>("manage_notifications")?.let {
+                it.isVisible = true
+            }
+
+            findPreference<Preference>("delete_all")?.let {
+                it.isVisible = true
+                it.setOnPreferenceClickListener { _ ->
+                    deleteAllConfirmation(notificationDao)
+                    return@setOnPreferenceClickListener true
+                }
             }
         } else {
+            findPreference<PreferenceCategory>("manage_notifications")?.let {
+                it.isVisible = false
+            }
+            findPreference<PreferenceCategory>("list_notifications")?.let {
+                it.isVisible = false
+            }
             findPreference<Preference>("no_notifications")?.let {
                 it.isVisible = true
             }
         }
+    }
+
+    private fun deleteAllConfirmation(notificationDao: NotificationDao) {
+        val builder: AlertDialog.Builder = AlertDialog.Builder(requireContext())
+
+        builder.setTitle(R.string.confirm_delete_all_notification_title)
+        builder.setMessage(R.string.confirm_delete_all_notification_message)
+
+        builder.setPositiveButton(
+            R.string.confirm_positive
+        ) { dialog, _ ->
+            notificationDao.deleteAll()
+            dialog.dismiss()
+            parentFragmentManager.popBackStack()
+        }
+
+        builder.setNegativeButton(
+            R.string.confirm_negative
+        ) { dialog, _ -> // Do nothing
+            dialog.dismiss()
+        }
+
+        val alert: AlertDialog? = builder.create()
+        alert?.show()
     }
 }
