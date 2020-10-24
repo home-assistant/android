63fded36f160848798bbdf46196f0e5637f5fdfa
Tom Schneider <tom.schneider-github@sutomaji.net>
2020-10-24 15:38:20 +0200

    Add lock support to power menu

---
+++ app/src/main/java/io/homeassistant/companion/android/controls/LockControl.kt
@@ -0,0 +1,68 @@
+package io.homeassistant.companion.android.controls
+
+import android.app.PendingIntent
+import android.content.Context
+import android.content.Intent
+import android.os.Build
+import android.service.controls.Control
+import android.service.controls.DeviceTypes
+import android.service.controls.actions.BooleanAction
+import android.service.controls.actions.ControlAction
+import android.service.controls.templates.ControlButton
+import android.service.controls.templates.ToggleTemplate
+import androidx.annotation.RequiresApi
+import io.homeassistant.companion.android.R
+import io.homeassistant.companion.android.common.data.integration.Entity
+import io.homeassistant.companion.android.common.data.integration.IntegrationRepository
+import io.homeassistant.companion.android.webview.WebViewActivity
+import kotlinx.coroutines.runBlocking
+
+@RequiresApi(Build.VERSION_CODES.R)
+class LockControl {
+    companion object : HaControl {
+        override fun createControl(
+            context: Context,
+            entity: Entity<Map<String, Any>>
+        ): Control {
+            val control = Control.StatefulBuilder(
+                entity.entityId,
+                PendingIntent.getActivity(
+                    context,
+                    0,
+                    WebViewActivity.newInstance(context.applicationContext).addFlags(Intent.FLAG_ACTIVITY_NEW_TASK),
+                    PendingIntent.FLAG_CANCEL_CURRENT
+                )
+            )
+            control.setTitle(entity.attributes["friendly_name"].toString())
+            control.setDeviceType(
+                DeviceTypes.TYPE_LOCK
+            )
+            control.setStatus(Control.STATUS_OK)
+            control.setStatusText(if (entity.state == "locked") context.getString(R.string.state_locked) else context.getString(R.string.state_unlocked))
+            control.setControlTemplate(
+                ToggleTemplate(
+                    entity.entityId,
+                    ControlButton(
+                        entity.state == "locked",
+                        "Description"
+                    )
+                )
+            )
+            return control.build()
+        }
+
+        override fun performAction(
+            integrationRepository: IntegrationRepository,
+            action: ControlAction
+        ): Boolean {
+            return runBlocking {
+                integrationRepository.callService(
+                    action.templateId.split(".")[0],
+                    if ((action as? BooleanAction)?.newState == true) "lock" else "unlock",
+                    hashMapOf("entity_id" to action.templateId)
+                )
+                return@runBlocking true
+            }
+        }
+    }
+}
