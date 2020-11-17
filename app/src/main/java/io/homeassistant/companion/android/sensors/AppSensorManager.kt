f432d959ddc849cc52e6b3b5cdf87942e71edbff
Daniel Shokouhi <dshokouhi@gmail.com>
2020-11-16 21:36:15 -0800

    Add app memory sensor

--- app/src/main/java/io/homeassistant/companion/android/sensors/AppSensorManager.kt
+++ app/src/main/java/io/homeassistant/companion/android/sensors/AppSensorManager.kt
@@ -24,14 +24,24 @@ class AppSensorManager : SensorManager {
             "app_rx_gb",
             "sensor",
             R.string.basic_sensor_name_app_rx_gb,
-            R.string.sensor_description_app_rx_gb
+            R.string.sensor_description_app_rx_gb,
+            unitOfMeasurement = "GB"
         )
 
         val app_tx_gb = SensorManager.BasicSensor(
             "app_tx_gb",
             "sensor",
             R.string.basic_sensor_name_app_tx_gb,
-            R.string.sensor_description_app_tx_gb
+            R.string.sensor_description_app_tx_gb,
+            unitOfMeasurement = "GB"
+        )
+
+        val app_memory = SensorManager.BasicSensor(
+            "app_memory",
+            "sensor",
+            R.string.basic_sensor_name_app_memory,
+            R.string.sensor_description_app_memory,
+            unitOfMeasurement = "GB"
         )
     }
 
@@ -41,7 +51,7 @@ class AppSensorManager : SensorManager {
         get() = R.string.sensor_name_app_sensor
 
     override val availableSensors: List<SensorManager.BasicSensor>
-        get() = listOf(currentVersion, app_rx_gb, app_tx_gb)
+        get() = listOf(currentVersion, app_rx_gb, app_tx_gb, app_memory)
 
     override fun requiredPermissions(sensorId: String): Array<String> {
         return emptyArray()
@@ -52,6 +62,7 @@ class AppSensorManager : SensorManager {
     ) {
         val myUid = Process.myUid()
         updateCurrentVersion(context)
+        updateAppMemory(context)
         updateAppRxGb(context, myUid)
         updateAppTxGb(context, myUid)
     }
@@ -113,6 +124,30 @@ class AppSensorManager : SensorManager {
             appTx.toBigDecimal().setScale(4, RoundingMode.HALF_EVEN),
             icon,
             mapOf()
+        )
+    }
+
+    private fun updateAppMemory(context: Context) {
+
+        if (!isEnabled(context, app_memory.id))
+            return
+
+        val runTime = Runtime.getRuntime()
+        val freeSize = runTime.freeMemory().toFloat() / GB
+        val totalSize = runTime.totalMemory().toFloat() / GB
+        val usedSize = totalSize - freeSize
+
+        val icon = "mdi:memory"
+
+        onSensorUpdated(
+            context,
+            app_memory,
+            usedSize.toBigDecimal().setScale(3, RoundingMode.HALF_EVEN),
+            icon,
+            mapOf(
+                "free_memory" to freeSize.toBigDecimal().setScale(3, RoundingMode.HALF_EVEN),
+                "total_memory" to totalSize.toBigDecimal().setScale(3, RoundingMode.HALF_EVEN)
+            )
         )
     }
 }
