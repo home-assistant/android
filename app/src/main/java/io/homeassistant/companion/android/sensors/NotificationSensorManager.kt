d518ec32e3a40c2977a5f9358148a317bcf97e7f
nestor <nestor@yzapp.cn>
2021-01-12 22:13:35 +0800

    优化消息信息

--- app/src/main/java/io/homeassistant/companion/android/sensors/NotificationSensorManager.kt
+++ app/src/main/java/io/homeassistant/companion/android/sensors/NotificationSensorManager.kt
@@ -10,10 +10,10 @@ import io.homeassistant.companion.android.R
 class NotificationSensorManager : NotificationListenerService(), SensorManager {
     companion object {
         val lastNotification = SensorManager.BasicSensor(
-            "last_notification",
-            "sensor",
-            R.string.basic_sensor_name_last_notification,
-            R.string.sensor_description_last_notification
+                "last_notification",
+                "sensor",
+                R.string.basic_sensor_name_last_notification,
+                R.string.sensor_description_last_notification
         )
     }
 
@@ -30,8 +30,8 @@ class NotificationSensorManager : NotificationListenerService(), SensorManager {
 
     override fun checkPermission(context: Context, sensorId: String): Boolean {
         return NotificationManagerCompat
-            .getEnabledListenerPackages(context)
-            .contains(context.packageName)
+                .getEnabledListenerPackages(context)
+                .contains(context.packageName)
     }
 
     override fun requestSensorUpdate(context: Context) {
@@ -44,44 +44,40 @@ class NotificationSensorManager : NotificationListenerService(), SensorManager {
         if (!isEnabled(applicationContext, lastNotification.id))
             return
 
-        if(sbn.packageName == "com.huawei.systemmanager"){
-            cancelNotification(sbn.key)
-            return
-        }
-
-//        if(sbn.packageName == "com.xiaomi.smarthome"){
+//        if (sbn.packageName == "com.huawei.systemmanager") {
 //            cancelNotification(sbn.key)
 //            return
 //        }
 
         val allowPackages = getSetting(
-            applicationContext,
-            lastNotification,
-            "Allow List",
-            "list-apps",
-            ""
+                applicationContext,
+                lastNotification,
+                "Allow List",
+                "list-apps",
+                ""
         ).split(", ").filter { it.isNotBlank() }
 
         if (sbn.packageName == application.packageName ||
-            (allowPackages.isNotEmpty() && sbn.packageName !in allowPackages)
+                (allowPackages.isNotEmpty() && sbn.packageName !in allowPackages)
         ) {
             return
         }
 
         val attr = sbn.notification.extras.keySet()
-            .map { it to sbn.notification.extras.get(it) }
-            .toMap()
-            .plus("package" to sbn.packageName)
+                .map { it to sbn.notification.extras.get(it) }
+                .toMap()
+                .plus("package" to sbn.packageName)
 
         // Attempt to use the text of the notification but fallback to package name if all else fails.
         val state = attr["android.title"].toString() + "-" + attr["android.text"].toString()
+        if (attr["android.title"] == null && attr["android.text"] == null) return
 
         onSensorUpdated(
-            applicationContext,
-            lastNotification,
-            state.take(255),
-            "mdi:bell-ring",
-            attr
+                applicationContext,
+                lastNotification,
+                state.take(255),
+                "mdi:bell-ring",
+                attr
         )
 
         // Need to send update!
