222ad7e0cd6619777d9ab04536bdbb22284c5513
Daniel Shokouhi <dshokouhi@gmail.com>
2021-01-31 10:51:01 -0800

    Direct user to permissions to launch activity in background

--- app/src/full/java/io/homeassistant/companion/android/notifications/MessagingService.kt
+++ app/src/full/java/io/homeassistant/companion/android/notifications/MessagingService.kt
@@ -18,6 +18,7 @@ import android.os.Build
 import android.os.Bundle
 import android.os.Handler
 import android.os.Looper
+import android.provider.Settings
 import android.speech.tts.TextToSpeech
 import android.speech.tts.UtteranceProgressListener
 import android.text.Spanned
@@ -445,43 +446,13 @@ class MessagingService : FirebaseMessagingService() {
                 }
             }
             COMMAND_ACTIVITY -> {
-                try {
-                    val packageName = data["channel"]
-                    val action = data["group"]
-                    val intentUri = if (!title.isNullOrEmpty()) Uri.parse(title) else null
-                    val intent = if (intentUri != null) Intent(action, intentUri) else Intent(action)
-                    val type = data["subject"]
-                    if (!type.isNullOrEmpty())
-                        intent.type = type
-                    val extras = data["tag"]
-                    if (!extras.isNullOrEmpty()) {
-                        val items = extras.split(',')
-                        for (item in items) {
-                            val pair = item.split(":")
-                            intent.putExtra(pair[0], if (pair[1].isDigitsOnly()) pair[1].toInt() else pair[1])
-                        }
-                    }
-                    intent.flags = Intent.FLAG_ACTIVITY_NEW_TASK
-                    if (!packageName.isNullOrEmpty()) {
-                        intent.setPackage(packageName)
-                        startActivity(intent)
-                    } else if (intent.resolveActivity(applicationContext.packageManager) != null)
-                        startActivity(intent)
+                if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.M) {
+                    if (!Settings.canDrawOverlays(applicationContext))
+                        requestSystemAlertPermission()
                     else
-                        mainScope.launch {
-                            Log.d(TAG, "Posting notification as we do not have enough data to start the activity")
-                            sendNotification(data)
-                        }
-                } catch (e: Exception) {
-                    Log.e(TAG, "Unable to send activity intent please check command format", e)
-                    Handler(Looper.getMainLooper()).post {
-                        Toast.makeText(
-                            applicationContext,
-                            R.string.activity_intent_error,
-                            Toast.LENGTH_LONG
-                        ).show()
-                    }
-                }
+                        processActivityCommand(data)
+                } else
+                    processActivityCommand(data)
             }
             else -> Log.d(TAG, "No command received")
         }
@@ -1025,7 +996,15 @@ class MessagingService : FirebaseMessagingService() {
     @RequiresApi(Build.VERSION_CODES.M)
     private fun requestDNDPermission() {
         val intent =
-            Intent(android.provider.Settings.ACTION_NOTIFICATION_POLICY_ACCESS_SETTINGS)
+            Intent(Settings.ACTION_NOTIFICATION_POLICY_ACCESS_SETTINGS)
+        intent.flags = Intent.FLAG_ACTIVITY_NEW_TASK
+        startActivity(intent)
+    }
+
+    @RequiresApi(Build.VERSION_CODES.M)
+    private fun requestSystemAlertPermission() {
+        val intent = Intent(Settings.ACTION_MANAGE_OVERLAY_PERMISSION,
+        Uri.parse("package:$packageName"))
         intent.flags = Intent.FLAG_ACTIVITY_NEW_TASK
         startActivity(intent)
     }
@@ -1074,6 +1053,45 @@ class MessagingService : FirebaseMessagingService() {
         }
     }
 
+    private fun processActivityCommand(data: Map<String, String>) {
+        try {
+            val packageName = data["channel"]
+            val action = data["group"]
+            val intentUri = if (!data[TITLE].isNullOrEmpty()) Uri.parse(data[TITLE]) else null
+            val intent = if (intentUri != null) Intent(action, intentUri) else Intent(action)
+            val type = data["subject"]
+            if (!type.isNullOrEmpty())
+                intent.type = type
+            val extras = data["tag"]
+            if (!extras.isNullOrEmpty()) {
+                val items = extras.split(',')
+                for (item in items) {
+                    val pair = item.split(":")
+                    intent.putExtra(pair[0], if (pair[1].isDigitsOnly()) pair[1].toInt() else pair[1])
+                }
+            }
+            intent.flags = Intent.FLAG_ACTIVITY_NEW_TASK
+            if (!packageName.isNullOrEmpty()) {
+                intent.setPackage(packageName)
+                startActivity(intent)
+            } else if (intent.resolveActivity(applicationContext.packageManager) != null)
+                startActivity(intent)
+            else
+                mainScope.launch {
+                    Log.d(TAG, "Posting notification as we do not have enough data to start the activity")
+                    sendNotification(data)
+                }
+        } catch (e: Exception) {
+            Log.e(TAG, "Unable to send activity intent please check command format", e)
+            Handler(Looper.getMainLooper()).post {
+                Toast.makeText(
+                    applicationContext,
+                    R.string.activity_intent_error,
+                    Toast.LENGTH_LONG
+                ).show()
+            }
+        }
+    }
     /**
      * Called if InstanceID token is updated. This may occur if the security of
      * the previous token had been compromised. Note that this is called when the InstanceID token
