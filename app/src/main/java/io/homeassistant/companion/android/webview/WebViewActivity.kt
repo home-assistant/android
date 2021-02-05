70ae3633b75ecdbe6b81c60b3782971b06952f00
Daniel Shokouhi <dshokouhi@gmail.com>
2021-02-05 08:26:53 -0800

    Update app/src/main/java/io/homeassistant/companion/android/webview/WebViewActivity.kt
    Co-authored-by: Justin Bassett <bassett.justint@gmail.com>

--- app/src/main/java/io/homeassistant/companion/android/webview/WebViewActivity.kt
+++ app/src/main/java/io/homeassistant/companion/android/webview/WebViewActivity.kt
@@ -285,7 +285,7 @@ class WebViewActivity : BaseActivity(), io.homeassistant.companion.android.webvi
                                 startActivity(browserIntent)
                                 return true
                             } else
-                                Log.w(TAG, "No unique cases found to override")
+                                Log.d(TAG, "No unique cases found to override")
                         } catch (e: Exception) {
                             Log.e(TAG, "Unable to override the URL", e)
                         }
