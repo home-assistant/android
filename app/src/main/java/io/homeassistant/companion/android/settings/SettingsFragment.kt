90b9c52540b3a9c20b723603b866d77105e1955f
Daniel Shokouhi <dshokouhi@gmail.com>
2021-03-10 12:33:29 -0800

    Fix blank title for authenticator (#1416)
    * Fix blank title for authenticator
    
    * Review comments

--- app/src/main/java/io/homeassistant/companion/android/settings/SettingsFragment.kt
+++ app/src/main/java/io/homeassistant/companion/android/settings/SettingsFragment.kt
@@ -92,8 +92,7 @@ class SettingsFragment : PreferenceFragmentCompat(), SettingsView {
                 isValid = true
                 if (BiometricManager.from(requireActivity()).canAuthenticate() == BiometricManager.BIOMETRIC_SUCCESS) {
                     setLock = true
-                    authenticator.title = getString(R.string.biometric_set_title)
-                    authenticator.authenticate()
+                    authenticator.authenticate(getString(R.string.biometric_set_title))
                 } else {
                     isValid = false
                     AlertDialog.Builder(requireActivity())
