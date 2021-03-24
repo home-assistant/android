7a15d72e1e79f6517709e7c25c4215b52b3b1d96
Daniel Shokouhi <dshokouhi@gmail.com>
2021-03-23 20:01:39 -0700

    Lint

--- app/src/main/java/io/homeassistant/companion/android/sensors/NextAlarmManager.kt
+++ app/src/main/java/io/homeassistant/companion/android/sensors/NextAlarmManager.kt
@@ -87,8 +87,7 @@ class NextAlarmManager : SensorManager {
                 val sdf = SimpleDateFormat(dateFormat)
                 sdf.timeZone = TimeZone.getTimeZone("UTC")
                 utc = sdf.format(Date(triggerTime))
-            }
-            else
+            } else
                 Log.d(TAG, "No alarm is scheduled, sending unavailable")
         } catch (e: Exception) {
             Log.e(TAG, "Error getting the next alarm info", e)
