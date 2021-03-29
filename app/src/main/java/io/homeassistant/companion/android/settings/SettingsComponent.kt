9ef83b71e89454f5652c46881b40f68b4862a4f6
Daniel Shokouhi <dshokouhi@gmail.com>
2021-03-29 11:32:06 -0700

    New shortcut type of entity

--- app/src/main/java/io/homeassistant/companion/android/settings/SettingsComponent.kt
+++ app/src/main/java/io/homeassistant/companion/android/settings/SettingsComponent.kt
@@ -3,9 +3,12 @@ package io.homeassistant.companion.android.settings
 import dagger.Component
 import io.homeassistant.companion.android.BaseActivity
 import io.homeassistant.companion.android.common.dagger.AppComponent
+import io.homeassistant.companion.android.settings.shortcuts.ManageShortcutsSettingsFragment
 
 @Component(dependencies = [AppComponent::class])
 interface SettingsComponent {
 
     fun inject(activity: BaseActivity)
+
+    fun inject(fragment: ManageShortcutsSettingsFragment)
 }
