fe8d593d3ae3d8e220d93ff83b7220d8e6b57562
hasenbolle <edgi@arcor.de>
2021-03-31 10:04:54 +0200

    Only read logs from the app itself

--- app/src/main/java/io/homeassistant/companion/android/util/LogcatReader.kt
+++ app/src/main/java/io/homeassistant/companion/android/util/LogcatReader.kt
@@ -6,7 +6,7 @@ import java.util.concurrent.TimeUnit
 object LogcatReader {
 
     fun saveLog(path: String): File {
-        val command = arrayOf("logcat", "-d", "-f", path)
+        val command = arrayOf("logcat", "--pid=" + android.os.Process.myPid(), "-d", "-f", path)
         val process = Runtime.getRuntime().exec(command)
         if (android.os.Build.VERSION.SDK_INT >= android.os.Build.VERSION_CODES.O) {
             process.waitFor(10, TimeUnit.SECONDS)
