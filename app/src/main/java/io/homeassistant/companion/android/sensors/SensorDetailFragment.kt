a48716e8361b620529051d9f4a39c33b2b7333c9
hasenbolle <edgi@arcor.de>
2021-04-06 21:49:45 +0200

    Set pref summary from it's values

--- app/src/main/java/io/homeassistant/companion/android/sensors/SensorDetailFragment.kt
+++ app/src/main/java/io/homeassistant/companion/android/sensors/SensorDetailFragment.kt
@@ -391,10 +391,11 @@ class SensorDetailFragment(
         pref.entryValues = entries.toTypedArray()
         pref.isIconSpaceReserved = false
         pref.isSingleLineTitle = false
-        pref.summary = setting.value
 
         // If selected list values are empty, but the setting.value is filled, then set the selected list value to the setting value
         if ((pref.values == null || pref.values.isEmpty()) && setting.value.isNotEmpty()) pref.values = setting.value.split(", ").map { it }.toSet()
+
+        pref.summary = pref.values.toString()
 
         pref.setOnPreferenceChangeListener { _, newValue ->
             sensorDao.add(
