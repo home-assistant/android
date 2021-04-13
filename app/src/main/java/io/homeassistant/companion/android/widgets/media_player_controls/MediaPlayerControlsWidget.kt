cbe1f6f4b8ad8922ce9f74d78c5634f0442e7d71
Daniel Shokouhi <dshokouhi@gmail.com>
2021-04-13 13:27:21 -0700

    Reduce image size to account for varying screen resolutions in media player widget (#1504)
    * Reduce image size and catch error
    
    * Use better resolution for aspect ratio

--- app/src/main/java/io/homeassistant/companion/android/widgets/media_player_controls/MediaPlayerControlsWidget.kt
+++ app/src/main/java/io/homeassistant/companion/android/widgets/media_player_controls/MediaPlayerControlsWidget.kt
@@ -181,11 +181,15 @@ class MediaPlayerControlsWidget : AppWidgetProvider() {
                     Handler(Looper.getMainLooper()).post {
                         if (BuildConfig.DEBUG)
                             Picasso.get().isLoggingEnabled = true
-                        Picasso.get().load(url).resize(1920, 1080).into(
-                            this,
-                            R.id.widgetMediaImage,
-                            intArrayOf(appWidgetId)
-                        )
+                        try {
+                            Picasso.get().load(url).resize(1024, 600).into(
+                                this,
+                                R.id.widgetMediaImage,
+                                intArrayOf(appWidgetId)
+                            )
+                        } catch (e: Exception) {
+                            Log.e(TAG, "Unable to load image", e)
+                        }
                         Log.d(TAG, "Fetch and load complete")
                     }
                 }
