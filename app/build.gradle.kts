831707996ef1d390f6c9cb2d45a2de9a2c2ec85c
Basset, Justin <Bassett.JustinT@gmail.com>
2021-08-06 20:58:00 -0400

    Bump Dagger

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -133,8 +133,8 @@ dependencies {
     implementation("org.jetbrains.kotlinx:kotlinx-coroutines-core:1.5.1")
     implementation("org.jetbrains.kotlinx:kotlinx-coroutines-android:1.5.1")
 
-    implementation("com.google.dagger:dagger:2.38")
-    kapt("com.google.dagger:dagger-compiler:2.38")
+    implementation("com.google.dagger:dagger:2.38.1")
+    kapt("com.google.dagger:dagger-compiler:2.38.1")
 
     implementation("androidx.appcompat:appcompat:1.3.1")
     implementation("androidx.lifecycle:lifecycle-runtime-ktx:2.3.1")
