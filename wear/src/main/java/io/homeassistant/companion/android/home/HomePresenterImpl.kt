50cf1b9f35052234d448ce52106885e1dd1a947a
apo-mak <apo-mak@users.noreply.github.com>
2021-10-12 20:35:22 +0300

    add lights entity

--- wear/src/main/java/io/homeassistant/companion/android/home/HomePresenterImpl.kt
+++ wear/src/main/java/io/homeassistant/companion/android/home/HomePresenterImpl.kt
@@ -42,6 +42,30 @@ class HomePresenterImpl @Inject constructor(
     }
 
     override fun onEntityClicked(entity: Entity<Any>) {
+
+            Log.i(TAG,"")
+        if (entity.entityId.split(".")[0] == "light"){
+            Log.i(TAG,"Entity data: "+ entity.state)
+            if (entity.state == "on"){
+                
+                mainScope.launch {
+                    integrationUseCase.callService(
+                        entity.entityId.split(".")[0],
+                        "turn_off",
+                        hashMapOf("entity_id" to entity.entityId)
+                    )
+                }
+
+            }else{
+                mainScope.launch {
+                    integrationUseCase.callService(
+                        entity.entityId.split(".")[0],
+                        "turn_on",
+                        hashMapOf("entity_id" to entity.entityId)
+                    )
+                }
+            }
+        }
         mainScope.launch {
             integrationUseCase.callService(
                 entity.entityId.split(".")[0],
@@ -69,9 +93,12 @@ class HomePresenterImpl @Inject constructor(
     }
 
     private fun processEntities(entities: Array<Entity<Any>>) {
-        val scenes = entities.sortedBy { it.entityId }.filter { it.entityId.split(".")[0] == "scene" }
-        val scripts = entities.sortedBy { it.entityId }.filter { it.entityId.split(".")[0] == "script" }
-        view.showHomeList(scenes, scripts)
+        val scenes = entities.filter { it.entityId.split(".")[0] == "scene" }
+        val scripts = entities.filter { it.entityId.split(".")[0] == "script" }
+        val lights = entities.filter { it.entityId.split(".")[0] == "light" }
+        val covers = entities.filter { it.entityId.split(".")[0] == "cover" }
+        view.showHomeList(scenes, scripts, lights, covers)
+        Log.i(TAG, "Cover data: "+ covers[0].attributes.toString())
     }
 
     private fun resyncRegistration() {
