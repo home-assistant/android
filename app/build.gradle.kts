430740ca4e44e51148272d64a65aecccde9b7cfa
Daniel Shokouhi <dshokouhi@gmail.com>
2021-10-22 09:19:11 -0700

    Migrate welcome fragment to use jetpack compose (#1818)
    * Migrate welcome fragment to use jetpack compose
    
    * Ditch XML

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -35,6 +35,15 @@ android {
 
     buildFeatures {
         viewBinding = true
+        compose = true
+    }
+
+    composeOptions {
+        kotlinCompilerExtensionVersion = "1.0.4"
+    }
+
+    kotlinOptions {
+        jvmTarget = "1.8"
     }
 
     compileOptions {
@@ -173,6 +182,19 @@ dependencies {
     implementation("com.google.android.exoplayer:exoplayer-hls:2.15.1")
     implementation("com.google.android.exoplayer:exoplayer-ui:2.15.1")
     implementation("com.google.android.exoplayer:extension-cronet:2.15.1")
+
+    implementation("androidx.compose.animation:animation:1.0.4")
+    implementation("androidx.compose.compiler:compiler:1.0.4")
+    implementation("androidx.compose.foundation:foundation:1.0.4")
+    implementation("androidx.compose.material:material:1.0.4")
+    implementation("androidx.compose.material:material-icons-core:1.0.4")
+    implementation("androidx.compose.material:material-icons-extended:1.0.4")
+    implementation("androidx.compose.runtime:runtime:1.0.4")
+    implementation("androidx.compose.ui:ui:1.0.4")
+    implementation("androidx.compose.ui:ui-tooling:1.0.4")
+    implementation("androidx.activity:activity-compose:1.3.1")
+    implementation("com.google.android.material:compose-theme-adapter:1.0.4")
+    implementation("com.google.accompanist:accompanist-appcompat-theme:0.16.0")
 }
 
 // Disable to fix memory leak and be compatible with the configuration cache.
--- app/src/main/java/io/homeassistant/companion/android/onboarding/WelcomeFragment.kt
+++ app/src/main/java/io/homeassistant/companion/android/onboarding/WelcomeFragment.kt
@@ -2,14 +2,36 @@ package io.homeassistant.companion.android.onboarding
 
 import android.os.Build
 import android.os.Bundle
-import android.text.Html
-import android.text.method.LinkMovementMethod
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-import android.widget.Button
-import android.widget.TextView
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.size
+import androidx.compose.foundation.text.ClickableText
+import androidx.compose.material.Button
+import androidx.compose.material.MaterialTheme
+import androidx.compose.material.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.platform.ComposeView
+import androidx.compose.ui.platform.LocalUriHandler
+import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.res.stringResource
+import androidx.compose.ui.text.SpanStyle
+import androidx.compose.ui.text.buildAnnotatedString
+import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.text.style.TextAlign
+import androidx.compose.ui.text.style.TextDecoration
+import androidx.compose.ui.text.withStyle
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
 import androidx.fragment.app.Fragment
+import com.google.android.material.composethemeadapter.MdcTheme
 import io.homeassistant.companion.android.R
 import io.homeassistant.companion.android.onboarding.discovery.DiscoveryFragment
 import io.homeassistant.companion.android.onboarding.manual.ManualSetupFragment
@@ -26,32 +48,101 @@ class WelcomeFragment : Fragment() {
         inflater: LayoutInflater,
         container: ViewGroup?,
         savedInstanceState: Bundle?
-    ): View? {
-        return inflater.inflate(R.layout.fragment_welcome, container, false).apply {
-
-            val learnMore = findViewById<TextView>(R.id.learn_more)
-            learnMore.movementMethod = LinkMovementMethod.getInstance()
-            learnMore.text = Html.fromHtml("<a href='https://www.home-assistant.io'>Learn More</a>")
-
-            findViewById<Button>(R.id.welcome).setOnClickListener {
-                if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.N) {
-                    val discoveryFragment = DiscoveryFragment.newInstance()
-                    discoveryFragment.retainInstance = true
-                    parentFragmentManager
-                        .beginTransaction()
-                        .replace(R.id.content, discoveryFragment)
-                        .addToBackStack("Welcome")
-                        .commit()
-                } else {
-                    val manualFragment = ManualSetupFragment.newInstance()
-                    manualFragment.retainInstance = true
-                    parentFragmentManager
-                        .beginTransaction()
-                        .replace(R.id.content, manualFragment)
-                        .addToBackStack("Welcome")
-                        .commit()
+    ): View {
+        return ComposeView(requireContext()).apply {
+            setContent {
+                MdcTheme {
+                    WelcomeSection()
                 }
             }
         }
+    }
+
+    @Composable
+    private fun WelcomeSection() {
+        Column(
+            verticalArrangement = Arrangement.Center
+        ) {
+            Image(
+                painter = painterResource(id = R.drawable.app_icon),
+                contentDescription = stringResource(
+                    id = R.string.app_name
+                ),
+                modifier = Modifier
+                    .size(width = 274.dp, height = 202.dp)
+                    .align(Alignment.CenterHorizontally)
+                    .padding(bottom = 15.dp)
+            )
+            Text(
+                fontSize = 19.sp,
+                fontWeight = FontWeight.Bold,
+                textAlign = TextAlign.Center,
+                text = stringResource(R.string.welcome_hass),
+                modifier = Modifier
+                    .align(Alignment.CenterHorizontally)
+            )
+            Text(
+                fontSize = 17.sp,
+                text = stringResource(R.string.welcome_hass_desc),
+                modifier = Modifier
+                    .padding(bottom = 15.dp, start = 30.dp, end = 20.dp, top = 10.dp)
+            )
+            val annotatedString = buildAnnotatedString {
+                pushStringAnnotation("learn", "https://www.home-assistant.io")
+                withStyle(
+                    style = SpanStyle(
+                        color = MaterialTheme.colors.primary,
+                        textDecoration = TextDecoration.Underline
+                    )
+                ) {
+                    append(stringResource(id = R.string.learn_more))
+                }
+                pop()
+            }
+            val uriHandler = LocalUriHandler.current
+            ClickableText(
+                text = annotatedString,
+                onClick = {
+                    annotatedString.getStringAnnotations("learn", it, it).firstOrNull()?.let { link ->
+                        uriHandler.openUri(link.item)
+                    }
+                },
+                modifier = Modifier
+                    .padding(bottom = 15.dp)
+                    .align(Alignment.CenterHorizontally)
+            )
+
+            Button(
+                onClick = {
+                    if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.N) {
+                        val discoveryFragment = DiscoveryFragment.newInstance()
+                        discoveryFragment.retainInstance = true
+                        parentFragmentManager
+                            .beginTransaction()
+                            .replace(R.id.content, discoveryFragment)
+                            .addToBackStack("Welcome")
+                            .commit()
+                    } else {
+                        val manualFragment = ManualSetupFragment.newInstance()
+                        manualFragment.retainInstance = true
+                        parentFragmentManager
+                            .beginTransaction()
+                            .replace(R.id.content, manualFragment)
+                            .addToBackStack("Welcome")
+                            .commit()
+                    }
+                },
+                modifier = Modifier
+                    .align(Alignment.CenterHorizontally)
+            ) {
+                Text(text = stringResource(id = R.string.continue_connect))
+            }
+        }
+    }
+
+    @Composable
+    @Preview
+    private fun PreviewWelcome() {
+        WelcomeSection()
     }
 }
