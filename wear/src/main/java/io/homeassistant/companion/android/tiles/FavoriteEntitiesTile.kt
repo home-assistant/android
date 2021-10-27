8ec03d7a2e93e8c7387a71247d60dc24e89537c4
leroyboerefijn <34075651+leroyboerefijn@users.noreply.github.com>
2021-10-27 21:13:02 +0200

    Add icons of the entities

--- wear/src/main/java/io/homeassistant/companion/android/tiles/FavoriteEntitiesTile.kt
+++ wear/src/main/java/io/homeassistant/companion/android/tiles/FavoriteEntitiesTile.kt
@@ -1,42 +1,52 @@
 package io.homeassistant.companion.android.tiles
 
+import android.graphics.Bitmap
+import android.graphics.Color
 import androidx.core.content.ContextCompat
+import androidx.core.graphics.drawable.toBitmap
 import androidx.wear.tiles.ActionBuilders
 import androidx.wear.tiles.ColorBuilders.argb
 import androidx.wear.tiles.DimensionBuilders.dp
+import androidx.wear.tiles.LayoutElementBuilders
 import androidx.wear.tiles.LayoutElementBuilders.Box
 import androidx.wear.tiles.LayoutElementBuilders.Column
 import androidx.wear.tiles.LayoutElementBuilders.HORIZONTAL_ALIGN_CENTER
 import androidx.wear.tiles.LayoutElementBuilders.Row
 import androidx.wear.tiles.LayoutElementBuilders.Spacer
-import androidx.wear.tiles.LayoutElementBuilders.Text
 import androidx.wear.tiles.LayoutElementBuilders.LayoutElement
 import androidx.wear.tiles.LayoutElementBuilders.Layout
 import androidx.wear.tiles.ModifiersBuilders
 import androidx.wear.tiles.RequestBuilders.TileRequest
 import androidx.wear.tiles.RequestBuilders.ResourcesRequest
+import androidx.wear.tiles.ResourceBuilders
 import androidx.wear.tiles.ResourceBuilders.Resources
 import androidx.wear.tiles.TileBuilders.Tile
 import androidx.wear.tiles.TileService
 import androidx.wear.tiles.TimelineBuilders.Timeline
 import androidx.wear.tiles.TimelineBuilders.TimelineEntry
-import com.google.common.util.concurrent.Futures
 import com.google.common.util.concurrent.ListenableFuture
+import com.mikepenz.iconics.IconicsColor
+import com.mikepenz.iconics.IconicsDrawable
+import com.mikepenz.iconics.utils.backgroundColor
+import com.mikepenz.iconics.utils.colorInt
+import com.mikepenz.iconics.utils.sizeDp
 import io.homeassistant.companion.android.R
 import io.homeassistant.companion.android.common.dagger.GraphComponentAccessor
 import io.homeassistant.companion.android.common.data.integration.Entity
 import io.homeassistant.companion.android.common.data.integration.IntegrationRepository
-import io.homeassistant.companion.android.home.HomeActivity
 import kotlinx.coroutines.CoroutineScope
 import kotlinx.coroutines.Dispatchers
 import kotlinx.coroutines.Job
 import kotlinx.coroutines.guava.future
+import java.nio.ByteBuffer
 import javax.inject.Inject
+import kotlin.math.roundToInt
 
-private const val RESOURCES_VERSION = "0.3"
+private const val RESOURCES_VERSION = "1"
 
 // Dimensions (dp)
 private const val CIRCLE_SIZE = 56f
+private const val ICON_SIZE = 48f*0.7071f // square that fits in 48dp circle
 private const val SPACING = 8f
 
 class FavoriteEntitiesTile : TileService() {
@@ -48,36 +58,82 @@ class FavoriteEntitiesTile : TileService() {
 
     override fun onTileRequest(requestParams: TileRequest): ListenableFuture<Tile> =
         serviceScope.future {
-            DaggerTilesComponent.builder()
-                .appComponent((applicationContext as GraphComponentAccessor).appComponent)
-                .build()
-                .inject(this@FavoriteEntitiesTile)
-            val entities = integrationUseCase.getEntities()
-                .sortedBy { it.entityId }
-                .filter { it.entityId.split(".")[0] == "scene" }
+            val entities = getEntities()
+
             Tile.Builder()
                 .setResourcesVersion(RESOURCES_VERSION)
-                .setTimeline(Timeline.Builder().addTimelineEntry(
-                    TimelineEntry.Builder().setLayout(
-                        Layout.Builder().setRoot(
-                            layout(entities)
+                .setTimeline(
+                    Timeline.Builder().addTimelineEntry(
+                        TimelineEntry.Builder().setLayout(
+                            Layout.Builder().setRoot(
+                                layout(entities)
+                            ).build()
                         ).build()
                     ).build()
-                ).build()
                 ).build()
         }
 
     override fun onResourcesRequest(requestParams: ResourcesRequest): ListenableFuture<Resources> =
-        Futures.immediateFuture(
+        serviceScope.future {
+            val density = requestParams.deviceParameters!!.screenDensity
+            val iconSizePx = (ICON_SIZE * density).roundToInt()
+            val entities = getEntities()
+
             Resources.Builder()
                 .setVersion(RESOURCES_VERSION)
+                .apply {
+                    entities.map { entity ->
+                        val entityAttributes = entity.attributes as Map<String, String>
+                        val iconName: String = if (entityAttributes["icon"]?.startsWith("mdi") == true) {
+                            entityAttributes["icon"]!!.split(":")[1]
+                        } else {
+                            "palette" // Default scene icon
+                        }
+
+                        val iconBitmap = IconicsDrawable(this@FavoriteEntitiesTile, "cmd-$iconName").apply {
+                            colorInt = Color.WHITE
+                            sizeDp = ICON_SIZE.roundToInt()
+                            backgroundColor = IconicsColor.colorRes(R.color.colorOverlay)
+                        }.toBitmap(iconSizePx, iconSizePx, Bitmap.Config.RGB_565)
+
+                        val bitmapData = ByteBuffer.allocate(iconBitmap.byteCount).apply {
+                            iconBitmap.copyPixelsToBuffer(this)
+                        }.array()
+
+                        // link the entity id to the drawable
+                        entity.entityId to ResourceBuilders.ImageResource.Builder()
+                            .setInlineResource(
+                                ResourceBuilders.InlineImageResource.Builder()
+                                    .setData(bitmapData)
+                                    .setWidthPx(iconSizePx)
+                                    .setHeightPx(iconSizePx)
+                                    .setFormat(ResourceBuilders.IMAGE_FORMAT_RGB_565)
+                                    .build()
+                            )
+                            .build()
+                    }.forEach { (id, imageResource) ->
+                        addIdToImageMapping(id, imageResource)
+                    }
+                }
                 .build()
-        )
+        }
 
     override fun onDestroy() {
         super.onDestroy()
         // Cleans up the coroutine
         serviceJob.cancel()
+    }
+
+    private suspend fun getEntities(): List<Entity<Any>> {
+        //TODO this should actually be a list specified by the user in settings
+        DaggerTilesComponent.builder()
+            .appComponent((applicationContext as GraphComponentAccessor).appComponent)
+            .build()
+            .inject(this@FavoriteEntitiesTile)
+
+        return integrationUseCase.getEntities()
+            .sortedBy { it.entityId }
+            .filter { it.entityId.split(".")[0] == "scene" }
     }
 
     fun layout(entities: List<Entity<Any>>): LayoutElement = Column.Builder()
@@ -107,9 +163,6 @@ class FavoriteEntitiesTile : TileService() {
         .build()
 
     private fun iconLayout(entity: Entity<Any>): LayoutElement = Box.Builder().apply {
-        val entityAttributes = entity.attributes as Map<String, String>
-        val name = entityAttributes["friendly_name"]
-
         setWidth(dp(CIRCLE_SIZE))
         setHeight(dp(CIRCLE_SIZE))
         setHorizontalAlignment(HORIZONTAL_ALIGN_CENTER)
@@ -141,8 +194,10 @@ class FavoriteEntitiesTile : TileService() {
             .build()
         )
         addContent(
-            Text.Builder()
-                .setText(name!!.substring(0,2))
+            LayoutElementBuilders.Image.Builder()
+                .setResourceId(entity.entityId)
+                .setWidth(dp(ICON_SIZE))
+                .setHeight(dp(ICON_SIZE))
                 .build()
         )
     }
