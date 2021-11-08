0aece4ac3cf38918e2d843e96151696da4042abe
Daniel Shokouhi <dshokouhi@gmail.com>
2021-11-07 18:14:14 -0800

    Request proper android 12 permissions for BLE transmitter (#1876)
    * Request proper android 12 permissions for BLE transmitter
    
    * Reorder permissions so we don't skip advertise when needed

--- app/src/main/java/io/homeassistant/companion/android/sensors/BluetoothSensorManager.kt
+++ app/src/main/java/io/homeassistant/companion/android/sensors/BluetoothSensorManager.kt
@@ -83,10 +83,22 @@ class BluetoothSensorManager : SensorManager {
     }
 
     override fun requiredPermissions(sensorId: String): Array<String> {
-        return if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.S) {
-            arrayOf(Manifest.permission.BLUETOOTH, Manifest.permission.BLUETOOTH_CONNECT)
-        } else
-            arrayOf(Manifest.permission.BLUETOOTH)
+        return when {
+            (sensorId == bleTransmitter.id && Build.VERSION.SDK_INT >= Build.VERSION_CODES.S) -> {
+                arrayOf(
+                    Manifest.permission.BLUETOOTH,
+                    Manifest.permission.BLUETOOTH_ADVERTISE,
+                    Manifest.permission.BLUETOOTH_CONNECT
+                )
+            }
+            (Build.VERSION.SDK_INT >= Build.VERSION_CODES.S) -> {
+                arrayOf(
+                    Manifest.permission.BLUETOOTH,
+                    Manifest.permission.BLUETOOTH_CONNECT
+                )
+            }
+            else -> arrayOf(Manifest.permission.BLUETOOTH)
+        }
     }
 
     override fun requestSensorUpdate(
