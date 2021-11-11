35e2ee80e4f867d78a2e1254a6fe9d14e7a2bd55
Daniel Shokouhi <dshokouhi@gmail.com>
2021-11-10 22:11:16 -0800

    Lint

--- wear/src/main/java/io/homeassistant/companion/android/home/views/MainView.kt
+++ wear/src/main/java/io/homeassistant/companion/android/home/views/MainView.kt
@@ -13,9 +13,9 @@ import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
-import androidx.compose.ui.res.colorResource
 import androidx.compose.ui.platform.LocalContext
 import androidx.compose.ui.platform.LocalHapticFeedback
+import androidx.compose.ui.res.colorResource
 import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.text.style.TextOverflow
