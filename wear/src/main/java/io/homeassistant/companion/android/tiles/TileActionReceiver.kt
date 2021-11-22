b400454ca0292f448246ddf2b1ae190ba23b39f3
leroyboerefijn <34075651+leroyboerefijn@users.noreply.github.com>
2021-11-22 21:13:38 +0100

    Add haptic feedback to shortcut tile

--- wear/src/main/java/io/homeassistant/companion/android/tiles/TileActionReceiver.kt
+++ wear/src/main/java/io/homeassistant/companion/android/tiles/TileActionReceiver.kt
@@ -3,6 +3,10 @@ package io.homeassistant.companion.android.tiles
 import android.content.BroadcastReceiver
 import android.content.Context
 import android.content.Intent
+import android.os.Build
+import android.os.VibrationEffect
+import android.os.Vibrator
+import android.os.VibratorManager
 import dagger.hilt.android.AndroidEntryPoint
 import io.homeassistant.companion.android.common.data.integration.IntegrationRepository
 import io.homeassistant.companion.android.home.HomePresenterImpl
@@ -20,6 +24,19 @@ class TileActionReceiver : BroadcastReceiver() {
 
         if (entityId != null) {
             runBlocking {
+                if (integrationUseCase.getWearHapticFeedback()) {
+                    if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.S) {
+                        val vibratorManager =
+                            context?.getSystemService(Context.VIBRATOR_MANAGER_SERVICE) as VibratorManager
+                        val vibrator = vibratorManager.defaultVibrator
+                        vibrator.vibrate(VibrationEffect.createPredefined(VibrationEffect.EFFECT_CLICK))
+                    } else {
+                        val vibrator =
+                            context?.getSystemService(Context.VIBRATOR_SERVICE) as Vibrator
+                        vibrator.vibrate(200)
+                    }
+                }
+
                 if (entityId.split(".")[0] in HomePresenterImpl.toggleDomains) {
                     integrationUseCase.callService(
                         entityId.split(".")[0],
