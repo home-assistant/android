110800281193f0a897a4fce6a1d7b83bcda0adcb
Tim Kist <zboarda@gmail.com>
2021-11-25 21:38:57 +0000

    Listen for screen updates in wear app

--- wear/src/main/java/io/homeassistant/companion/android/HomeAssistantApplication.kt
+++ wear/src/main/java/io/homeassistant/companion/android/HomeAssistantApplication.kt
@@ -3,6 +3,7 @@ package io.homeassistant.companion.android
 import android.app.Application
 import android.content.Intent
 import android.content.IntentFilter
+import android.os.PowerManager
 import dagger.hilt.android.HiltAndroidApp
 import io.homeassistant.companion.android.sensors.SensorReceiver
 
@@ -22,6 +23,16 @@ open class HomeAssistantApplication : Application() {
                 addAction(Intent.ACTION_BATTERY_OKAY)
                 addAction(Intent.ACTION_POWER_CONNECTED)
                 addAction(Intent.ACTION_POWER_DISCONNECTED)
+            }
+        )
+
+        // This will cause interactive and power save to update upon a state change
+        registerReceiver(
+            sensorReceiver,
+            IntentFilter().apply {
+                addAction(Intent.ACTION_SCREEN_OFF)
+                addAction(Intent.ACTION_SCREEN_ON)
+                addAction(PowerManager.ACTION_POWER_SAVE_MODE_CHANGED)
             }
         )
     }
