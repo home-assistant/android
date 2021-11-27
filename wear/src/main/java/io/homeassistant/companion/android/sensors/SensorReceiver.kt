e059db3c877a4411b77d5de494952896929f563c
Tim Kist <zboarda@gmail.com>
2021-11-27 16:04:15 +0000

    Remove commented code due to absent sensors... for now. Mwahahaha!

--- wear/src/main/java/io/homeassistant/companion/android/sensors/SensorReceiver.kt
+++ wear/src/main/java/io/homeassistant/companion/android/sensors/SensorReceiver.kt
@@ -22,30 +22,7 @@ class SensorReceiver : SensorReceiverBase() {
     companion object {
         const val TAG = "SensorReceiver"
         val MANAGERS = listOf(
-//            ActivitySensorManager(),
-//            AppSensorManager(),
-//            AudioSensorManager(),
-            BatterySensorManager(),
-//            BluetoothSensorManager(),
-//            DNDSensorManager(),
-//            GeocodeSensorManager(),
-//            KeyguardSensorManager(),
-//            LastRebootSensorManager(),
-//            LastUpdateManager(),
-//            LightSensorManager(),
-//            LocationSensorManager(),
-//            MobileDataManager(),
-//            NetworkSensorManager(),
-//            NextAlarmManager(),
-//            NotificationSensorManager(),
-//            PhoneStateSensorManager(),
-//            PowerSensorManager(),
-//            PressureSensorManager(),
-//            ProximitySensorManager(),
-//            StepsSensorManager(),
-//            StorageSensorManager(),
-//            TimeZoneManager(),
-//            TrafficStatsManager()
+            BatterySensorManager()
         )
 
         const val ACTION_REQUEST_SENSORS_UPDATE =
@@ -54,18 +31,5 @@ class SensorReceiver : SensorReceiverBase() {
 
     // Suppress Lint because we only register for the receiver if the android version matches the intent
     @SuppressLint("InlinedApi")
-    override val skippableActions = mapOf<String, String>()/*
-        "android.app.action.NEXT_ALARM_CLOCK_CHANGED" to NextAlarmManager.nextAlarm.id,
-        "android.bluetooth.device.action.ACL_CONNECTED" to BluetoothSensorManager.bluetoothConnection.id,
-        "android.bluetooth.device.action.ACL_DISCONNECTED" to BluetoothSensorManager.bluetoothConnection.id,
-        BluetoothAdapter.ACTION_STATE_CHANGED to BluetoothSensorManager.bluetoothState.id,
-        Intent.ACTION_SCREEN_OFF to PowerSensorManager.interactiveDevice.id,
-        Intent.ACTION_SCREEN_ON to PowerSensorManager.interactiveDevice.id,
-        PowerManager.ACTION_POWER_SAVE_MODE_CHANGED to PowerSensorManager.powerSave.id,
-        PowerManager.ACTION_DEVICE_IDLE_MODE_CHANGED to PowerSensorManager.doze.id,
-        NotificationManager.ACTION_INTERRUPTION_FILTER_CHANGED to DNDSensorManager.dndSensor.id,
-        AudioManager.ACTION_MICROPHONE_MUTE_CHANGED to AudioSensorManager.micMuted.id,
-        AudioManager.ACTION_SPEAKERPHONE_STATE_CHANGED to AudioSensorManager.speakerphoneState.id,
-        AudioManager.RINGER_MODE_CHANGED_ACTION to AudioSensorManager.audioSensor.id
-    )*/
+    override val skippableActions = mapOf<String, String>()
 }
