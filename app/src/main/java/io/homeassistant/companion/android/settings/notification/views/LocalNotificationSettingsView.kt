0b9ab54079ee7a0b9e46c0af7527bca8ade1e4d6
Justin Bassett <bassett.justint@gmail.com>
2022-01-01 12:39:56 -0500

    Make whole row selectable.

--- app/src/main/java/io/homeassistant/companion/android/settings/notification/views/LocalNotificationSettingsView.kt
+++ app/src/main/java/io/homeassistant/companion/android/settings/notification/views/LocalNotificationSettingsView.kt
@@ -2,7 +2,9 @@ package io.homeassistant.companion.android.settings.notification.views
 
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.selection.selectable
 import androidx.compose.material.RadioButton
 import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
@@ -45,6 +47,7 @@ fun RadioButtonRow(
     onClick: () -> Unit
 ) {
     Row(
+        modifier = Modifier.fillMaxWidth().selectable(selected = selected, onClick = onClick),
         verticalAlignment = Alignment.CenterVertically
     ) {
         RadioButton(selected = selected, onClick = onClick)
