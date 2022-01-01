53da9d1a4b3bf3cb3ed27e5cc73b0ea662b7acf2
Justin Bassett <bassett.justint@gmail.com>
2022-01-01 15:18:32 -0500

    Extract Strings. Also fix strang build issue?

--- app/src/main/java/io/homeassistant/companion/android/settings/notification/views/LocalNotificationSettingsView.kt
+++ app/src/main/java/io/homeassistant/companion/android/settings/notification/views/LocalNotificationSettingsView.kt
@@ -10,7 +10,9 @@ import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.unit.dp
+import io.homeassistant.companion.android.common.R
 import io.homeassistant.companion.android.database.settings.LocalNotificationSetting
 
 @Composable
@@ -20,20 +22,20 @@ fun LocalNotificationSettingsView(
 ) {
     Column(modifier = Modifier.padding(20.dp)) {
         Row(verticalAlignment = Alignment.CenterVertically) {
-            Text("Please select when you would like the application to attempt to directly communicate with your Home Assistant instance to attempt to bypass Firebase Cloud Messaging for push notifications.")
+            Text(stringResource(R.string.local_notif_description))
         }
         RadioButtonRow(
-            text = "Never",
+            text = stringResource(R.string.local_notif_never),
             selected = localNotificationSetting == LocalNotificationSetting.NEVER,
             onClick = { onSettingChanged(LocalNotificationSetting.NEVER) }
         )
         RadioButtonRow(
-            text = "While screen on",
+            text = stringResource(R.string.local_notif_while_screen_on),
             selected = localNotificationSetting == LocalNotificationSetting.SCREEN_ON,
             onClick = { onSettingChanged(LocalNotificationSetting.SCREEN_ON) }
         )
         RadioButtonRow(
-            text = "All the time",
+            text = stringResource(R.string.local_notif_always),
             selected = localNotificationSetting == LocalNotificationSetting.ALWAYS,
             onClick = { onSettingChanged(LocalNotificationSetting.ALWAYS) }
         )
@@ -47,7 +49,9 @@ fun RadioButtonRow(
     onClick: () -> Unit
 ) {
     Row(
-        modifier = Modifier.fillMaxWidth().selectable(selected = selected, onClick = onClick),
+        modifier = Modifier
+            .fillMaxWidth()
+            .selectable(selected = selected, onClick = onClick),
         verticalAlignment = Alignment.CenterVertically
     ) {
         RadioButton(selected = selected, onClick = onClick)
