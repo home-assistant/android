70e6207a049268db84e93f1a6eb60ae1e8d4fd62
Joris Pelgröm <jpelgrom@users.noreply.github.com>
2022-01-12 03:39:44 +0100

    Support button entity type for device controls (#2134)
    - Add support for entities in the new button domain (2021.12) and input_button domain (2022.2) to device controls
     - Rename the existing SceneControl as that already acted as a button

--- app/src/main/java/io/homeassistant/companion/android/controls/HaControlsProviderService.kt
+++ app/src/main/java/io/homeassistant/companion/android/controls/HaControlsProviderService.kt
@@ -17,7 +17,6 @@ import io.homeassistant.companion.android.util.RegistriesDataHandler
 import kotlinx.coroutines.CoroutineScope
 import kotlinx.coroutines.Dispatchers
 import kotlinx.coroutines.cancel
-import kotlinx.coroutines.flow.collect
 import kotlinx.coroutines.launch
 import java.util.concurrent.Flow
 import java.util.function.Consumer
@@ -42,17 +41,19 @@ class HaControlsProviderService : ControlsProviderService() {
     private val domainToHaControl = mapOf(
         "automation" to DefaultSwitchControl,
         "camera" to null,
+        "button" to DefaultButtonControl,
         "climate" to ClimateControl,
         "cover" to CoverControl,
         "fan" to FanControl,
         "input_boolean" to DefaultSwitchControl,
+        "input_button" to DefaultButtonControl,
         "input_number" to DefaultSliderControl,
         "light" to LightControl,
         "lock" to LockControl,
         "media_player" to null,
         "remote" to null,
-        "scene" to SceneControl,
-        "script" to SceneControl,
+        "scene" to DefaultButtonControl,
+        "script" to DefaultButtonControl,
         "switch" to DefaultSwitchControl,
         "vacuum" to VacuumControl
     )
