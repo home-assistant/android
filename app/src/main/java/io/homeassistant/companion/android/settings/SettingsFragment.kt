e4ea0eb10ea1fc63ae7c90523a4ff0d8b5a11657
Daniel Shokouhi <dshokouhi@gmail.com>
2022-02-28 18:14:26 -0800

    Add a setting screen to easily delete notification channels and edit them

--- app/src/main/java/io/homeassistant/companion/android/settings/SettingsFragment.kt
+++ app/src/main/java/io/homeassistant/companion/android/settings/SettingsFragment.kt
@@ -36,6 +36,7 @@ import io.homeassistant.companion.android.nfc.NfcSetupActivity
 import io.homeassistant.companion.android.sensors.SensorsSettingsFragment
 import io.homeassistant.companion.android.settings.language.LanguagesProvider
 import io.homeassistant.companion.android.settings.log.LogFragment
+import io.homeassistant.companion.android.settings.notification.NotificationChannelFragment
 import io.homeassistant.companion.android.settings.notification.NotificationHistoryFragment
 import io.homeassistant.companion.android.settings.qs.ManageTilesFragment
 import io.homeassistant.companion.android.settings.sensor.SensorUpdateFrequencyFragment
@@ -220,10 +221,17 @@ class SettingsFragment constructor(
         }
 
         if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.O) {
-            findPreference<Preference>("notification_channels")?.let {
+            findPreference<Preference>("notification_channels")?.let { pref ->
                 val uiManager = requireContext().getSystemService<UiModeManager>()
-                it.isVisible = uiManager?.currentModeType != Configuration.UI_MODE_TYPE_TELEVISION
-                it.intent = Intent(Settings.ACTION_APP_NOTIFICATION_SETTINGS).putExtra(Settings.EXTRA_APP_PACKAGE, context?.packageName)
+                pref.isVisible = uiManager?.currentModeType != Configuration.UI_MODE_TYPE_TELEVISION
+                pref.setOnPreferenceClickListener {
+                    parentFragmentManager
+                        .beginTransaction()
+                        .replace(R.id.content, NotificationChannelFragment::class.java, null)
+                        .addToBackStack(getString(commonR.string.notification_channels))
+                        .commit()
+                    return@setOnPreferenceClickListener true
+                }
             }
         }
 
---
+++ app/src/main/java/io/homeassistant/companion/android/settings/notification/views/NotificationChannelView.kt
@@ -0,0 +1,81 @@
+package io.homeassistant.companion.android.settings.notification.views
+
+import android.os.Build
+import androidx.annotation.RequiresApi
+import androidx.compose.foundation.clickable
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.PaddingValues
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.lazy.LazyColumn
+import androidx.compose.material.Divider
+import androidx.compose.material.Icon
+import androidx.compose.material.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.asImageBitmap
+import androidx.compose.ui.platform.LocalContext
+import androidx.compose.ui.res.stringResource
+import androidx.compose.ui.unit.dp
+import com.mikepenz.iconics.IconicsDrawable
+import io.homeassistant.companion.android.common.R
+import io.homeassistant.companion.android.settings.notification.NotificationViewModel
+
+@RequiresApi(Build.VERSION_CODES.O)
+@Composable
+fun NotificationChannelView(
+    notificationViewModel: NotificationViewModel
+) {
+    val context = LocalContext.current
+    LazyColumn(contentPadding = PaddingValues(20.dp)) {
+        item {
+            Text(
+                text = stringResource(id = R.string.notification_channels_description),
+                modifier = Modifier.padding(bottom = 20.dp)
+            )
+            Divider()
+        }
+
+        items(notificationViewModel.channelList[0].size) { index ->
+            val channelId = notificationViewModel.channelList[0][index].id
+            Row(
+                modifier = Modifier
+                    .padding(10.dp)
+                    .fillMaxWidth(),
+                verticalAlignment = Alignment.CenterVertically,
+                horizontalArrangement = Arrangement.SpaceBetween
+            ) {
+
+                Text(
+                    text = notificationViewModel.channelList[0][index].name.toString()
+                )
+                val editIcon =
+                    IconicsDrawable(context, "cmd-pencil").toBitmap().asImageBitmap()
+                Row {
+                    Icon(
+                        editIcon,
+                        "",
+                        modifier = Modifier
+                            .clickable { notificationViewModel.editChannelDetails(channelId) }
+                            .padding(end = 10.dp)
+                    )
+                    val deleteIcon =
+                        IconicsDrawable(context, "cmd-delete").toBitmap().asImageBitmap()
+                    Icon(
+                        deleteIcon,
+                        "",
+                        modifier = Modifier
+                            .clickable {
+                                notificationViewModel.deleteChannel(channelId)
+                                notificationViewModel.updateChannelList()
+                            }
+                            .padding(start = 10.dp)
+                    )
+                }
+            }
+            Divider()
+        }
+    }
+}
