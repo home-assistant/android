349bdc0e89e73c8ece57e364c328238fec960f2e
Joris Pelgröm <joris.pelgrom@gmail.com>
2022-03-14 19:27:56 +0100

    Remove internet connectivity requirement for WebsocketManager
     - Replace the NetworkType.CONNECTED constraint on the job for WebsocketManager with a check that does not take the validation status (= internet access) of the network into account. HA may be accessible without an internet connection.

--- app/src/main/java/io/homeassistant/companion/android/websocket/WebsocketManager.kt
+++ app/src/main/java/io/homeassistant/companion/android/websocket/WebsocketManager.kt
@@ -5,16 +5,15 @@ import android.app.NotificationManager
 import android.app.PendingIntent
 import android.content.Context
 import android.content.Intent
+import android.net.ConnectivityManager
 import android.os.Build
 import android.os.PowerManager
 import android.util.Log
 import androidx.core.app.NotificationCompat
 import androidx.core.content.getSystemService
-import androidx.work.Constraints
 import androidx.work.CoroutineWorker
 import androidx.work.ExistingPeriodicWorkPolicy
 import androidx.work.ForegroundInfo
-import androidx.work.NetworkType
 import androidx.work.PeriodicWorkRequestBuilder
 import androidx.work.WorkInfo
 import androidx.work.WorkManager
@@ -54,13 +53,8 @@ class WebsocketManager(
         private val DEFAULT_WEBSOCKET_SETTING = if (BuildConfig.FLAVOR == "full") WebsocketSetting.NEVER else WebsocketSetting.ALWAYS
 
         fun start(context: Context) {
-            val constraints = Constraints.Builder()
-                .setRequiredNetworkType(NetworkType.CONNECTED)
-                .build()
-
             val websocketNotifications =
                 PeriodicWorkRequestBuilder<WebsocketManager>(15, TimeUnit.MINUTES)
-                    .setConstraints(constraints)
                     .build()
 
             val workManager = WorkManager.getInstance(context)
@@ -124,13 +118,18 @@ class WebsocketManager(
         return@withContext Result.success()
     }
 
+    @Suppress("DEPRECATION")
     private suspend fun shouldWeRun(): Boolean {
+        // Check for connectivity but not internet access, based on WorkManager's NetworkConnectedController API <26
+        val connectivityManager = applicationContext.getSystemService<ConnectivityManager>()
+        val networkInfo = connectivityManager?.activeNetworkInfo
         val powerManager = applicationContext.getSystemService<PowerManager>()!!
         val displayOff = !powerManager.isInteractive
         val setting = settingsDao.get(0)?.websocketSetting ?: DEFAULT_WEBSOCKET_SETTING
         val isHome = urlRepository.isInternal()
         return when {
             (setting == WebsocketSetting.NEVER) -> false
+            (networkInfo != null && !networkInfo.isConnected) -> false
             (displayOff && setting == WebsocketSetting.SCREEN_ON) -> false
             (!isHome && setting == WebsocketSetting.HOME_WIFI) -> false
             else -> true
