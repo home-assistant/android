5caf5d47766cb964f80d62fbf5d43ddd581727de
Tiger Oakes <contact@tigeroakes.com>
2022-03-26 10:44:23 -0700

    Remove unused imports

--- common/src/main/java/io/homeassistant/companion/android/database/widget/MediaPlayerControlsWidgetDao.kt
+++ common/src/main/java/io/homeassistant/companion/android/database/widget/MediaPlayerControlsWidgetDao.kt
@@ -4,7 +4,6 @@ import androidx.room.Dao
 import androidx.room.Insert
 import androidx.room.OnConflictStrategy
 import androidx.room.Query
-import androidx.room.Update
 import kotlinx.coroutines.flow.Flow
 
 @Dao
