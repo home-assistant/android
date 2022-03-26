69c315a86bd6598fae76e8b1335e7deb8f3345c9
Tiger Oakes <contact@tigeroakes.com>
2022-03-26 10:00:47 -0700

    Add base interface for widget entities

--- common/src/main/java/io/homeassistant/companion/android/database/widget/StaticWidgetEntity.kt
+++ common/src/main/java/io/homeassistant/companion/android/database/widget/StaticWidgetEntity.kt
@@ -7,7 +7,7 @@ import androidx.room.PrimaryKey
 @Entity(tableName = "static_widget")
 data class StaticWidgetEntity(
     @PrimaryKey
-    val id: Int,
+    override val id: Int,
     @ColumnInfo(name = "entity_id")
     val entityId: String,
     @ColumnInfo(name = "attribute_ids")
@@ -22,4 +22,4 @@ data class StaticWidgetEntity(
     val attributeSeparator: String = "",
     @ColumnInfo(name = "last_update")
     val lastUpdate: String
-)
+) : WidgetEntity
