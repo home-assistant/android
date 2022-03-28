c874afe8adf6f7ea0dc1181fda60a9548d6ebf14
Tiger Oakes <contact@tigeroakes.com>
2022-03-28 09:02:41 -0700

    Change performAction to suspend function

--- app/src/main/java/io/homeassistant/companion/android/controls/DefaultButtonControl.kt
+++ app/src/main/java/io/homeassistant/companion/android/controls/DefaultButtonControl.kt
@@ -11,57 +11,52 @@ import io.homeassistant.companion.android.common.data.integration.Entity
 import io.homeassistant.companion.android.common.data.integration.IntegrationRepository
 import io.homeassistant.companion.android.common.data.integration.domain
 import io.homeassistant.companion.android.common.data.websocket.impl.entities.AreaRegistryResponse
-import kotlinx.coroutines.runBlocking
 import io.homeassistant.companion.android.common.R as commonR
 
 @RequiresApi(Build.VERSION_CODES.R)
-class DefaultButtonControl {
-    companion object : HaControl {
-        override fun provideControlFeatures(
-            context: Context,
-            control: Control.StatefulBuilder,
-            entity: Entity<Map<String, Any>>,
-            area: AreaRegistryResponse?
-        ): Control.StatefulBuilder {
-            control.setStatusText("")
-            control.setControlTemplate(
-                StatelessTemplate(
-                    entity.entityId
-                )
+object DefaultButtonControl : HaControl {
+    override fun provideControlFeatures(
+        context: Context,
+        control: Control.StatefulBuilder,
+        entity: Entity<Map<String, Any>>,
+        area: AreaRegistryResponse?
+    ): Control.StatefulBuilder {
+        control.setStatusText("")
+        control.setControlTemplate(
+            StatelessTemplate(
+                entity.entityId
             )
-            return control
+        )
+        return control
+    }
+
+    override fun getDeviceType(entity: Entity<Map<String, Any>>): Int =
+        when (entity.domain) {
+            "scene", "script" -> DeviceTypes.TYPE_ROUTINE
+            else -> DeviceTypes.TYPE_UNKNOWN
         }
 
-        override fun getDeviceType(entity: Entity<Map<String, Any>>): Int =
-            when (entity.domain) {
-                "scene", "script" -> DeviceTypes.TYPE_ROUTINE
-                else -> DeviceTypes.TYPE_UNKNOWN
-            }
-
-        override fun getDomainString(context: Context, entity: Entity<Map<String, Any>>): String =
-            when (entity.domain) {
-                "button" -> context.getString(commonR.string.domain_button)
-                "input_button" -> context.getString(commonR.string.domain_input_button)
-                "scene" -> context.getString(commonR.string.domain_scene)
-                "script" -> context.getString(commonR.string.domain_script)
-                else -> entity.domain.capitalize()
-            }
-
-        override fun performAction(
-            integrationRepository: IntegrationRepository,
-            action: ControlAction
-        ): Boolean {
-            return runBlocking {
-                integrationRepository.callService(
-                    action.templateId.split(".")[0],
-                    when (action.templateId.split(".")[0]) {
-                        "button", "input_button" -> "press"
-                        else -> "turn_on"
-                    },
-                    hashMapOf("entity_id" to action.templateId)
-                )
-                return@runBlocking true
-            }
+    override fun getDomainString(context: Context, entity: Entity<Map<String, Any>>): String =
+        when (entity.domain) {
+            "button" -> context.getString(commonR.string.domain_button)
+            "input_button" -> context.getString(commonR.string.domain_input_button)
+            "scene" -> context.getString(commonR.string.domain_scene)
+            "script" -> context.getString(commonR.string.domain_script)
+            else -> entity.domain.replaceFirstChar { it.titlecase() }
         }
+
+    override suspend fun performAction(
+        integrationRepository: IntegrationRepository,
+        action: ControlAction
+    ): Boolean {
+        integrationRepository.callService(
+            action.templateId.split(".")[0],
+            when (action.templateId.split(".")[0]) {
+                "button", "input_button" -> "press"
+                else -> "turn_on"
+            },
+            hashMapOf("entity_id" to action.templateId)
+        )
+        return true
     }
 }
