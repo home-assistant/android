bee9c446b71c45fa59afda591e1e59dff13b147c
Joris Pelgröm <joris.pelgrom@gmail.com>
2022-04-09 13:26:59 +0200

    Also convert tag reader activity to Compose
     - Shouldn't be seen in most cases by users

--- app/src/main/java/io/homeassistant/companion/android/nfc/TagReaderActivity.kt
+++ app/src/main/java/io/homeassistant/companion/android/nfc/TagReaderActivity.kt
@@ -6,15 +6,14 @@ import android.nfc.NfcAdapter
 import android.os.Bundle
 import android.util.Log
 import android.widget.Toast
+import androidx.activity.compose.setContent
+import androidx.lifecycle.lifecycleScope
+import com.google.android.material.composethemeadapter.MdcTheme
 import dagger.hilt.android.AndroidEntryPoint
 import io.homeassistant.companion.android.BaseActivity
-import io.homeassistant.companion.android.R
 import io.homeassistant.companion.android.common.data.integration.IntegrationRepository
+import io.homeassistant.companion.android.nfc.views.TagReaderView
 import io.homeassistant.companion.android.util.UrlHandler
-import kotlinx.coroutines.CoroutineScope
-import kotlinx.coroutines.Dispatchers
-import kotlinx.coroutines.Job
-import kotlinx.coroutines.cancel
 import kotlinx.coroutines.launch
 import javax.inject.Inject
 import io.homeassistant.companion.android.common.R as commonR
@@ -22,60 +21,58 @@ import io.homeassistant.companion.android.common.R as commonR
 @AndroidEntryPoint
 class TagReaderActivity : BaseActivity() {
 
-    val TAG = TagReaderActivity::class.simpleName
-
-    private val mainScope: CoroutineScope = CoroutineScope(Dispatchers.Main + Job())
+    companion object {
+        const val TAG = "TagReaderActivity"
+    }
 
     @Inject
     lateinit var integrationUseCase: IntegrationRepository
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_tag_reader)
 
-        setSupportActionBar(findViewById(R.id.toolbar))
+        setContent {
+            MdcTheme {
+                TagReaderView()
+            }
+        }
 
-        mainScope.launch {
-            if (intent.action == NfcAdapter.ACTION_NDEF_DISCOVERED) {
-                val url = NFCUtil.extractUrlFromNFCIntent(intent)
+        lifecycleScope.launch {
+            if (intent.action == NfcAdapter.ACTION_NDEF_DISCOVERED || intent.action == Intent.ACTION_VIEW) {
+                val isNfcTag = intent.action == NfcAdapter.ACTION_NDEF_DISCOVERED
+
+                val url =
+                    if (isNfcTag) NFCUtil.extractUrlFromNFCIntent(intent)
+                    else intent.data
                 try {
-                    handleTag(url)
+                    handleTag(url, isNfcTag)
                 } catch (e: Exception) {
-                    val message = commonR.string.nfc_processing_tag_error
-                    Toast.makeText(this@TagReaderActivity, message, Toast.LENGTH_LONG).show()
-                    Log.e(TAG, "Unable to handle url (nfc): $url", e)
-                    finish()
-                }
-            } else if (Intent.ACTION_VIEW == intent.action) {
-                val url: Uri? = intent.data
-                try {
-                    handleTag(url)
-                } catch (e: Exception) {
-                    val message = commonR.string.qrcode_processing_tag_error
-                    Toast.makeText(this@TagReaderActivity, message, Toast.LENGTH_LONG).show()
-                    Log.e(TAG, "Unable to handle url (qrcode): $url", e)
-                    finish()
+                    showProcessingError(isNfcTag)
+                    Log.e(TAG, "Unable to handle url (${if (isNfcTag) "nfc" else "qr"}}): $url", e)
                 }
             }
+            finish()
         }
     }
 
-    override fun onDestroy() {
-        mainScope.cancel()
-        super.onDestroy()
-    }
-
-    private suspend fun handleTag(url: Uri?) {
+    private suspend fun handleTag(url: Uri?, isNfcTag: Boolean) {
         // https://www.home-assistant.io/tag/5f0ba733-172f-430d-a7f8-e4ad940c88d7
 
         val nfcTagId = UrlHandler.splitNfcTagId(url)
-        Log.d(TAG, "nfcTagId: $nfcTagId")
+        Log.d(TAG, "Tag ID: $nfcTagId")
         if (nfcTagId != null) {
             integrationUseCase.scanTag(hashMapOf("tag_id" to nfcTagId))
             Log.d(TAG, "Tag scanned to HA successfully")
         } else {
-            Toast.makeText(this, commonR.string.nfc_processing_tag_error, Toast.LENGTH_LONG).show()
+            showProcessingError(isNfcTag)
         }
-        finish()
+    }
+
+    private fun showProcessingError(isNfcTag: Boolean) {
+        Toast.makeText(
+            this,
+            if (isNfcTag) commonR.string.nfc_processing_tag_error else commonR.string.qrcode_processing_tag_error,
+            Toast.LENGTH_LONG
+        ).show()
     }
 }
