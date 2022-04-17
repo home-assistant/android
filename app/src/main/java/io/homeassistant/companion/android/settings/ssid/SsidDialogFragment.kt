f4c4d72518f1e8308df5ae09068a651bb2b2f621
Joris Pelgröm <jpelgrom@users.noreply.github.com>
2022-04-17 14:48:40 +0200

    Suggest or highlight current SSID in home SSID dialog (#2457)
    - Show a chip suggesting the user to add the SSID of the currently connected network, if not already added to the list of home SSIDs, in the home SSID dialog
      - Change icon for listed SSIDs in home SSID dialog to DKGRAY by default, and only show currently connected SSID icon in accent color
      - Add missing contentDescription for ImageButtons
      - Use correct tint namespace
      - Update deprecated code

--- app/src/main/java/io/homeassistant/companion/android/settings/ssid/SsidDialogFragment.kt
+++ app/src/main/java/io/homeassistant/companion/android/settings/ssid/SsidDialogFragment.kt
@@ -2,15 +2,21 @@ package io.homeassistant.companion.android.settings.ssid
 
 import android.annotation.SuppressLint
 import android.content.Context
+import android.net.wifi.WifiManager
+import android.os.Build
 import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.inputmethod.EditorInfo
 import androidx.preference.PreferenceDialogFragmentCompat
+import dagger.hilt.android.AndroidEntryPoint
 import io.homeassistant.companion.android.R
+import io.homeassistant.companion.android.common.data.wifi.WifiHelper
 import io.homeassistant.companion.android.databinding.DialogSsidBinding
+import javax.inject.Inject
 import io.homeassistant.companion.android.common.R as commonR
 
+@AndroidEntryPoint
 class SsidDialogFragment : PreferenceDialogFragmentCompat() {
 
     companion object {
@@ -20,6 +26,9 @@ class SsidDialogFragment : PreferenceDialogFragmentCompat() {
             }
         }
     }
+
+    @Inject
+    lateinit var wifiHelper: WifiHelper
 
     private lateinit var binding: DialogSsidBinding
     private val ssidAdapter = SsidRecyclerViewAdapter()
@@ -44,12 +53,25 @@ class SsidDialogFragment : PreferenceDialogFragmentCompat() {
         binding.rvSsids.adapter = ssidAdapter
 
         val ssids = getSsidPreference().getSsids()
-        ssidAdapter.submitSet(ssids)
+        ssidAdapter.submitSet(ssids.map { SsidRecyclerViewAdapter.SsidEntry(it, isConnectedToSsid(it)) }.toSet())
+
+        val currentSsid = wifiHelper.getWifiSsid().removeSurrounding("\"")
+        if (currentSsid.isBlank() || currentSsid in ssids ||
+            (Build.VERSION.SDK_INT >= Build.VERSION_CODES.R && currentSsid == WifiManager.UNKNOWN_SSID)
+        ) {
+            binding.suggestCurrentSsid.visibility = View.GONE
+        } else {
+            binding.suggestCurrentSsid.visibility = View.VISIBLE
+            binding.suggestCurrentSsid.text = getString(commonR.string.add_ssid_name_suggestion, currentSsid)
+            binding.suggestCurrentSsid.setOnClickListener {
+                addSsidFromSuggestion()
+            }
+        }
     }
 
     private fun addSsidFromInput(): Boolean {
         val input = binding.inputSsid.text?.toString() ?: return false
-        val result = if (!input.isBlank()) submitSsid(input) else return false
+        val result = if (input.isNotBlank()) submitSsid(input) else return false
         if (result) {
             binding.inputContainer.error = null
             binding.inputSsid.text = null
@@ -59,12 +81,21 @@ class SsidDialogFragment : PreferenceDialogFragmentCompat() {
         return result
     }
 
+    private fun addSsidFromSuggestion(): Boolean {
+        val input = wifiHelper.getWifiSsid().removeSurrounding("\"")
+        val result = if (input.isNotBlank()) submitSsid(input) else return false
+        if (result) {
+            binding.suggestCurrentSsid.visibility = View.GONE
+        } // else shouldn't happen, so don't do anything
+        return result
+    }
+
     private fun submitSsid(ssid: String): Boolean {
         val ssids = ssidAdapter.currentList
-        if (ssids.contains(ssid)) {
+        if (ssids.any { it.name == ssid }) {
             return false
         }
-        ssidAdapter.submitList(ssids + ssid)
+        ssidAdapter.submitList(ssids + SsidRecyclerViewAdapter.SsidEntry(ssid, isConnectedToSsid(ssid)))
         return true
     }
 
@@ -72,16 +103,20 @@ class SsidDialogFragment : PreferenceDialogFragmentCompat() {
         return preference as SsidPreference
     }
 
+    private fun isConnectedToSsid(ssid: String): Boolean {
+        return wifiHelper.getWifiSsid().removeSurrounding("\"") == ssid
+    }
+
     override fun onDialogClosed(positiveResult: Boolean) {
         if (positiveResult) {
-            val ssids = ssidAdapter.currentList.toSortedSet()
+            val ssids = ssidAdapter.currentList.toMutableList()
             val input = binding.inputSsid.text?.toString()
             if (!input.isNullOrBlank()) {
-                ssids.add(input)
+                ssids.add(SsidRecyclerViewAdapter.SsidEntry(input, isConnectedToSsid(input)))
             }
             val ssidPreference = getSsidPreference()
             if (preference.callChangeListener(ssids)) {
-                ssidPreference.setSsids(ssids)
+                ssidPreference.setSsids(ssids.map { it.name }.sorted().toSet())
             }
         }
     }
