a06f96fd4de7ef32866ef9385390dd9afc6b12f1
Joris Pelgröm <jpelgrom@users.noreply.github.com>
2022-05-23 23:13:31 +0200

    Provide Compose preview for Play Store listing screens (#2542)
    * Provide Compose preview for Play Store listing screens
    
    * Update previews for onboarding
    
     - Set theme correctly and show system UI
     - Add variant for dark mode
    
    * Update Wear OS listing image
    
    * Remove specific icon, we can use domain default

--- wear/src/main/java/io/homeassistant/companion/android/home/views/EntityListView.kt
+++ wear/src/main/java/io/homeassistant/companion/android/home/views/EntityListView.kt
@@ -10,6 +10,9 @@ import androidx.wear.compose.material.ChipDefaults
 import androidx.wear.compose.material.Text
 import io.homeassistant.companion.android.common.data.integration.Entity
 import io.homeassistant.companion.android.theme.WearAppTheme
+import io.homeassistant.companion.android.util.playPreviewEntityScene1
+import io.homeassistant.companion.android.util.playPreviewEntityScene2
+import io.homeassistant.companion.android.util.playPreviewEntityScene3
 import io.homeassistant.companion.android.util.previewEntity1
 import io.homeassistant.companion.android.util.previewEntity2
 import io.homeassistant.companion.android.common.R as commonR
@@ -83,6 +86,20 @@ private fun PreviewEntityListView() {
     EntityViewList(
         entityLists = mapOf(stringResource(commonR.string.lights) to listOf(previewEntity1, previewEntity2)),
         entityListsOrder = listOf(stringResource(commonR.string.lights)),
+        entityListFilter = { true },
+        onEntityClicked = { _, _ -> },
+        onEntityLongClicked = { _ -> },
+        isHapticEnabled = false,
+        isToastEnabled = false
+    )
+}
+
+@Preview
+@Composable
+private fun PreviewEntityListScenes() {
+    EntityViewList(
+        entityLists = mapOf(stringResource(commonR.string.scenes) to listOf(playPreviewEntityScene1, playPreviewEntityScene2, playPreviewEntityScene3)),
+        entityListsOrder = listOf(stringResource(commonR.string.scenes)),
         entityListFilter = { true },
         onEntityClicked = { _, _ -> },
         onEntityLongClicked = { _ -> },
