1d33d1e179ad7b362fe66fbc7c4cf563f59a2ed7
dimitri <d@magicbuffer.net>
2022-06-18 15:47:18 +0200

    Use a safe call on errorResponse

--- app/src/main/java/io/homeassistant/companion/android/onboarding/authentication/AuthenticationFragment.kt
+++ app/src/main/java/io/homeassistant/companion/android/onboarding/authentication/AuthenticationFragment.kt
@@ -87,7 +87,7 @@ class AuthenticationFragment : Fragment() {
                                     errorResponse: WebResourceResponse?
                                 ) {
                                     super.onReceivedHttpError(view, request, errorResponse)
-                                    if (isTLSClientAuthNeeded && errorResponse.statusCode == 400) {
+                                    if (isTLSClientAuthNeeded && errorResponse?.statusCode == 400) {
                                         showError(commonR.string.tls_cert_not_found_message, null, null)
                                     } else {
                                         showError(commonR.string.error_ssl, null, null)
