e4f805ae5c4648d397d38e6357e0b6fae6d0caad
Joris Pelgröm <joris.pelgrom@gmail.com>
2022-06-21 21:56:20 +0200

    Show entity state on entity chip in Wear OS app
     - Shows the color and position of an entity state on the entity's chip in the Wear OS app by using a custom background class that accepts more than a single Color

--- wear/src/main/java/io/homeassistant/companion/android/home/views/EntityUi.kt
+++ wear/src/main/java/io/homeassistant/companion/android/home/views/EntityUi.kt
@@ -5,23 +5,37 @@ import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Brush
+import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.graphics.ColorFilter
+import androidx.compose.ui.graphics.painter.BrushPainter
+import androidx.compose.ui.graphics.painter.Painter
 import androidx.compose.ui.input.pointer.pointerInput
 import androidx.compose.ui.platform.LocalContext
 import androidx.compose.ui.platform.LocalHapticFeedback
+import androidx.compose.ui.platform.LocalLayoutDirection
 import androidx.compose.ui.text.style.TextOverflow
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.LayoutDirection
 import androidx.wear.compose.material.Chip
 import androidx.wear.compose.material.ChipDefaults
+import androidx.wear.compose.material.ContentAlpha
+import androidx.wear.compose.material.MaterialTheme
 import androidx.wear.compose.material.Text
 import androidx.wear.compose.material.ToggleChip
+import androidx.wear.compose.material.ToggleChipColors
 import androidx.wear.compose.material.ToggleChipDefaults
 import com.mikepenz.iconics.compose.Image
 import com.mikepenz.iconics.typeface.library.community.material.CommunityMaterial
 import io.homeassistant.companion.android.common.data.integration.Entity
+import io.homeassistant.companion.android.common.data.integration.EntityPosition
 import io.homeassistant.companion.android.common.data.integration.domain
+import io.homeassistant.companion.android.common.data.integration.getFanSpeed
+import io.homeassistant.companion.android.common.data.integration.getLightBrightness
+import io.homeassistant.companion.android.common.data.integration.getLightColor
 import io.homeassistant.companion.android.home.HomePresenterImpl
 import io.homeassistant.companion.android.theme.wearColorPalette
+import io.homeassistant.companion.android.util.WearToggleChipDefaults
 import io.homeassistant.companion.android.util.getIcon
 import io.homeassistant.companion.android.util.onEntityClickedFeedback
 import io.homeassistant.companion.android.util.previewEntity1
@@ -82,7 +96,8 @@ fun EntityUi(
                 )
             },
             enabled = entity.state != "unavailable",
-            toggleIcon = { ToggleChipDefaults.SwitchIcon(isChecked) }
+            toggleIcon = { ToggleChipDefaults.SwitchIcon(isChecked) },
+            colors = entityToggleChipBackgroundColors(entity, isChecked)
         )
     } else {
         Chip(
@@ -125,6 +140,120 @@ fun EntityUi(
             },
             colors = ChipDefaults.secondaryChipColors()
         )
+    }
+}
+
+/**
+ * A function that provides chip colors that mostly follow the default toggle chip colors, but when supported
+ * provide a background for active entities that reflects their state (position and color). Gradient code
+ * is based on [androidx.wear.compose.material.ToggleChipDefaults.toggleChipColors].
+ *
+ * @param entity The entity state on which the background for the active state should be based
+ */
+@Composable
+private fun entityToggleChipBackgroundColors(entity: Entity<*>, checked: Boolean): ToggleChipColors {
+    // For a toggleable entity, a custom background should only be used if the on is enabled and has:
+    // a. a position (eg. fan speed, light brightness)
+    // b. a custom color (eg. light color)
+    // If there is a position (a) but no color (b), use the default (theme) color for the 'active' part.
+    // If there is a color (b) but no position (a), use a smooth gradient similar to ToggleChip.
+    // If it doesn't have either or is off, it should use the default chip background.
+
+    val hasPosition = when (entity.domain) {
+        "fan" -> entity.getFanSpeed() != null
+        "light" -> entity.getLightBrightness() != null
+        else -> false
+    }
+    val hasColor = entity.getLightColor() != null
+    val gradientDirection = LocalLayoutDirection.current
+
+    val contentBackgroundColor = if (hasColor) {
+        val entityColor = entity.getLightColor()
+        if (entityColor != null) Color(entityColor) else MaterialTheme.colors.primary
+    } else {
+        MaterialTheme.colors.primary
+    }
+
+    return when {
+        checked && (hasPosition || hasColor) -> {
+            val checkedStartBackgroundColor = contentBackgroundColor.copy(alpha = 0.325f)
+            val checkedEndBackgroundColor = MaterialTheme.colors.surface
+
+            var checkedBackgroundColors = listOf(
+                checkedStartBackgroundColor,
+                checkedEndBackgroundColor
+            )
+            var disabledCheckedBackgroundColors = listOf(
+                checkedStartBackgroundColor.copy(alpha = ContentAlpha.disabled),
+                checkedEndBackgroundColor.copy(alpha = ContentAlpha.disabled)
+            )
+            if (gradientDirection != LayoutDirection.Ltr) {
+                checkedBackgroundColors = checkedBackgroundColors.reversed()
+                disabledCheckedBackgroundColors = disabledCheckedBackgroundColors.reversed()
+            }
+
+            val backgroundPainter: Painter
+            val disabledBackgroundPainter: Painter
+            if (hasPosition) {
+                // Insert colors in the middle again to act as a 'hard stop'
+                checkedBackgroundColors = checkedBackgroundColors.toMutableList().apply {
+                    addAll(1, checkedBackgroundColors)
+                }
+                disabledCheckedBackgroundColors = disabledCheckedBackgroundColors.toMutableList().apply {
+                    addAll(1, disabledCheckedBackgroundColors)
+                }
+
+                // Use position info to provide stop points
+                // Minimum/maximum stops are not set to 0f/1f to make 1%/100% values visible
+                val position = when (entity.domain) {
+                    "fan" -> entity.getFanSpeed()
+                    "light" -> entity.getLightBrightness()
+                    else -> null
+                } ?: EntityPosition(value = 1f, min = 0f, max = 2f) // This should never happen
+                // TODO fix gradient stop should be vertical, not diagonal
+                // TODO stop positions for RTL languages
+                val checkedColorStops = checkedBackgroundColors.mapIndexed { index, color ->
+                    when (index) {
+                        0 -> 0.025f to color
+                        1, 2 -> ((position.value - position.min) / (position.max - position.min)) to color
+                        else -> 0.975f to color // index: 3
+                    }
+                }.toTypedArray()
+                val disabledCheckedColorStops = disabledCheckedBackgroundColors.mapIndexed { index, color ->
+                    when (index) {
+                        0 -> 0.025f to color
+                        1, 2 -> ((position.value - position.min) / (position.max - position.min)) to color
+                        else -> 0.975f to color // index: 3
+                    }
+                }.toTypedArray()
+
+                // Painters that use the color stops
+                backgroundPainter = BrushPainter(
+                    Brush.linearGradient(
+                        colorStops = checkedColorStops
+                    )
+                )
+                disabledBackgroundPainter = BrushPainter(
+                    Brush.linearGradient(
+                        colorStops = disabledCheckedColorStops
+                    )
+                )
+            } else {
+                // Color should be towards the end to match other enabled ToggleChips
+                checkedBackgroundColors = checkedBackgroundColors.reversed()
+                disabledCheckedBackgroundColors = disabledCheckedBackgroundColors.reversed()
+
+                // Painters that match ToggleChipDefaults
+                backgroundPainter = BrushPainter(Brush.linearGradient(checkedBackgroundColors))
+                disabledBackgroundPainter = BrushPainter(Brush.linearGradient(disabledCheckedBackgroundColors))
+            }
+
+            WearToggleChipDefaults.defaultChipColors().apply {
+                checkedBackgroundPainter = backgroundPainter
+                disabledCheckedBackgroundPainter = disabledBackgroundPainter
+            }
+        }
+        else -> ToggleChipDefaults.toggleChipColors()
     }
 }
 
---
+++ wear/src/main/java/io/homeassistant/companion/android/util/WearToggleChipColors.kt
@@ -0,0 +1,273 @@
+package io.homeassistant.companion.android.util
+
+import androidx.compose.runtime.Composable
+import androidx.compose.runtime.State
+import androidx.compose.runtime.rememberUpdatedState
+import androidx.compose.ui.graphics.Brush
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.graphics.painter.BrushPainter
+import androidx.compose.ui.graphics.painter.Painter
+import androidx.compose.ui.platform.LocalLayoutDirection
+import androidx.compose.ui.unit.LayoutDirection
+import androidx.wear.compose.material.ContentAlpha
+import androidx.wear.compose.material.MaterialTheme
+import androidx.wear.compose.material.ToggleChipColors
+import androidx.wear.compose.material.contentColorFor
+
+object WearToggleChipDefaults {
+    /**
+     * A copy of [androidx.wear.compose.material.ToggleChipDefaults.toggleChipColors] that returns
+     * [WearToggleChipColors] which allows the app to set the Painter for advanced use cases instead
+     * of only providing a Color.
+     */
+    @Composable
+    fun defaultChipColors(
+        checkedStartBackgroundColor: Color = MaterialTheme.colors.surface.copy(alpha = 0.75f),
+        checkedEndBackgroundColor: Color = MaterialTheme.colors.primary.copy(alpha = 0.325f),
+        checkedContentColor: Color = MaterialTheme.colors.onSurface,
+        checkedSecondaryContentColor: Color = MaterialTheme.colors.onSurfaceVariant,
+        checkedToggleIconTintColor: Color = MaterialTheme.colors.secondary,
+        uncheckedStartBackgroundColor: Color = MaterialTheme.colors.surface,
+        uncheckedEndBackgroundColor: Color = MaterialTheme.colors.surface,
+        uncheckedContentColor: Color = contentColorFor(checkedEndBackgroundColor),
+        uncheckedSecondaryContentColor: Color = uncheckedContentColor,
+        uncheckedToggleIconTintColor: Color = uncheckedContentColor,
+        splitBackgroundOverlayColor: Color = Color.White.copy(alpha = 0.05f),
+        gradientDirection: LayoutDirection = LocalLayoutDirection.current
+    ): WearToggleChipColors {
+        val checkedBackgroundColors: List<Color>
+        val disabledCheckedBackgroundColors: List<Color>
+        if (gradientDirection == LayoutDirection.Ltr) {
+            checkedBackgroundColors = listOf(
+                checkedStartBackgroundColor,
+                checkedEndBackgroundColor
+            )
+            disabledCheckedBackgroundColors = listOf(
+                checkedStartBackgroundColor.copy(alpha = ContentAlpha.disabled),
+                checkedEndBackgroundColor.copy(alpha = ContentAlpha.disabled)
+            )
+        } else {
+            checkedBackgroundColors = listOf(
+                checkedEndBackgroundColor,
+                checkedStartBackgroundColor
+            )
+            disabledCheckedBackgroundColors = listOf(
+                checkedEndBackgroundColor.copy(alpha = ContentAlpha.disabled),
+                checkedStartBackgroundColor.copy(alpha = ContentAlpha.disabled),
+            )
+        }
+        val uncheckedBackgroundColors: List<Color>
+        val disabledUncheckedBackgroundColors: List<Color>
+        if (gradientDirection == LayoutDirection.Ltr) {
+            uncheckedBackgroundColors = listOf(
+                uncheckedStartBackgroundColor,
+                uncheckedEndBackgroundColor
+            )
+            disabledUncheckedBackgroundColors = listOf(
+                uncheckedStartBackgroundColor.copy(alpha = ContentAlpha.disabled),
+                uncheckedEndBackgroundColor.copy(alpha = ContentAlpha.disabled)
+            )
+        } else {
+            uncheckedBackgroundColors = listOf(
+                uncheckedEndBackgroundColor,
+                uncheckedStartBackgroundColor
+            )
+            disabledUncheckedBackgroundColors = listOf(
+                uncheckedEndBackgroundColor.copy(alpha = ContentAlpha.disabled),
+                uncheckedStartBackgroundColor.copy(alpha = ContentAlpha.disabled),
+            )
+        }
+
+        return WearToggleChipColors(
+            checkedBackgroundPainter = BrushPainter(Brush.linearGradient(checkedBackgroundColors)),
+            checkedContentColor = checkedContentColor,
+            checkedSecondaryContentColor = checkedSecondaryContentColor,
+            checkedIconTintColor = checkedToggleIconTintColor,
+            checkedSplitBackgroundOverlay = splitBackgroundOverlayColor,
+            uncheckedBackgroundPainter = BrushPainter(
+                Brush.linearGradient(uncheckedBackgroundColors)
+            ),
+            uncheckedContentColor = uncheckedContentColor,
+            uncheckedSecondaryContentColor = uncheckedSecondaryContentColor,
+            uncheckedIconTintColor = uncheckedToggleIconTintColor,
+            uncheckedSplitBackgroundOverlay = splitBackgroundOverlayColor,
+            disabledCheckedBackgroundPainter = BrushPainter(
+                Brush.linearGradient(disabledCheckedBackgroundColors)
+            ),
+            disabledCheckedContentColor = checkedContentColor.copy(alpha = ContentAlpha.disabled),
+            disabledCheckedSecondaryContentColor = checkedSecondaryContentColor.copy(
+                alpha = ContentAlpha.disabled
+            ),
+            disabledCheckedIconTintColor = checkedToggleIconTintColor.copy(
+                alpha = ContentAlpha.disabled
+            ),
+            disabledCheckedSplitBackgroundOverlay = splitBackgroundOverlayColor,
+            disabledUncheckedBackgroundPainter = BrushPainter(
+                Brush.linearGradient(disabledUncheckedBackgroundColors)
+            ),
+            disabledUncheckedContentColor = uncheckedContentColor.copy(
+                alpha = ContentAlpha.disabled
+            ),
+            disabledUncheckedSecondaryContentColor = uncheckedSecondaryContentColor.copy(
+                alpha = ContentAlpha.disabled
+            ),
+            disabledUncheckedIconTintColor = uncheckedToggleIconTintColor.copy(
+                alpha = ContentAlpha.disabled
+            ),
+            disabledUncheckedSplitBackgroundOverlay = splitBackgroundOverlayColor
+        )
+    }
+}
+
+/**
+ * A copy of [androidx.wear.compose.material.DefaultToggleChipColors] with a public constructor and mutable
+ * properties to allow the app to set the Painter for advanced use cases instead of only providing a Color.
+ */
+class WearToggleChipColors(
+    var checkedBackgroundPainter: Painter,
+    var checkedContentColor: Color,
+    var checkedSecondaryContentColor: Color,
+    var checkedIconTintColor: Color,
+    var checkedSplitBackgroundOverlay: Color,
+    var disabledCheckedBackgroundPainter: Painter,
+    var disabledCheckedContentColor: Color,
+    var disabledCheckedSecondaryContentColor: Color,
+    var disabledCheckedIconTintColor: Color,
+    var disabledCheckedSplitBackgroundOverlay: Color,
+    var uncheckedBackgroundPainter: Painter,
+    var uncheckedContentColor: Color,
+    var uncheckedSecondaryContentColor: Color,
+    var uncheckedIconTintColor: Color,
+    var uncheckedSplitBackgroundOverlay: Color,
+    var disabledUncheckedBackgroundPainter: Painter,
+    var disabledUncheckedContentColor: Color,
+    var disabledUncheckedSecondaryContentColor: Color,
+    var disabledUncheckedIconTintColor: Color,
+    var disabledUncheckedSplitBackgroundOverlay: Color,
+) : ToggleChipColors {
+
+    @Composable
+    override fun background(enabled: Boolean, checked: Boolean): State<Painter> {
+        return rememberUpdatedState(
+            if (enabled) {
+                if (checked) checkedBackgroundPainter else uncheckedBackgroundPainter
+            } else {
+                if (checked) disabledCheckedBackgroundPainter else
+                    disabledUncheckedBackgroundPainter
+            }
+        )
+    }
+
+    @Composable
+    override fun contentColor(enabled: Boolean, checked: Boolean): State<Color> {
+        return rememberUpdatedState(
+            if (enabled) {
+                if (checked) checkedContentColor else uncheckedContentColor
+            } else {
+                if (checked) disabledCheckedContentColor else disabledUncheckedContentColor
+            }
+        )
+    }
+
+    @Composable
+    override fun secondaryContentColor(enabled: Boolean, checked: Boolean): State<Color> {
+        return rememberUpdatedState(
+            if (enabled) {
+                if (checked) checkedSecondaryContentColor else uncheckedSecondaryContentColor
+            } else {
+                if (checked) disabledCheckedSecondaryContentColor else
+                    disabledUncheckedSecondaryContentColor
+            }
+        )
+    }
+
+    @Composable
+    override fun toggleIconTintColor(enabled: Boolean, checked: Boolean): State<Color> {
+        return rememberUpdatedState(
+            if (enabled) {
+                if (checked) checkedIconTintColor else uncheckedIconTintColor
+            } else {
+                if (checked) disabledCheckedIconTintColor else disabledUncheckedIconTintColor
+            }
+        )
+    }
+
+    @Composable
+    override fun splitBackgroundOverlay(enabled: Boolean, checked: Boolean): State<Color> {
+        return rememberUpdatedState(
+            if (enabled) {
+                if (checked) checkedSplitBackgroundOverlay else uncheckedSplitBackgroundOverlay
+            } else {
+                if (checked) disabledCheckedSplitBackgroundOverlay else
+                    disabledUncheckedSplitBackgroundOverlay
+            }
+        )
+    }
+
+    override fun equals(other: Any?): Boolean {
+        if (this === other) return true
+        if (other == null) return false
+        if (this::class != other::class) return false
+
+        other as WearToggleChipColors
+
+        if (checkedBackgroundPainter != other.checkedBackgroundPainter) return false
+        if (checkedContentColor != other.checkedContentColor) return false
+        if (checkedIconTintColor != other.checkedIconTintColor) return false
+        if (checkedSecondaryContentColor != other.checkedSecondaryContentColor) return false
+        if (checkedSplitBackgroundOverlay != other.checkedSplitBackgroundOverlay) return false
+        if (uncheckedBackgroundPainter != other.uncheckedBackgroundPainter) return false
+        if (uncheckedContentColor != other.uncheckedContentColor) return false
+        if (uncheckedIconTintColor != other.uncheckedIconTintColor) return false
+        if (uncheckedSecondaryContentColor != other.uncheckedSecondaryContentColor) return false
+        if (uncheckedSplitBackgroundOverlay != other.uncheckedSplitBackgroundOverlay) return false
+        if (disabledCheckedBackgroundPainter != other.disabledCheckedBackgroundPainter) return false
+        if (disabledCheckedContentColor != other.disabledCheckedContentColor) return false
+        if (disabledCheckedIconTintColor != other.disabledCheckedIconTintColor) return false
+        if (disabledCheckedSecondaryContentColor !=
+            other.disabledCheckedSecondaryContentColor
+        ) return false
+        if (disabledCheckedSplitBackgroundOverlay !=
+            other.disabledCheckedSplitBackgroundOverlay
+        ) return false
+        if (disabledUncheckedBackgroundPainter !=
+            other.disabledUncheckedBackgroundPainter
+        ) return false
+        if (disabledUncheckedContentColor != other.disabledUncheckedContentColor) return false
+        if (disabledUncheckedIconTintColor != other.disabledUncheckedIconTintColor) return false
+        if (disabledUncheckedSecondaryContentColor !=
+            other.disabledUncheckedSecondaryContentColor
+        ) return false
+        if (disabledUncheckedSplitBackgroundOverlay !=
+            other.disabledUncheckedSplitBackgroundOverlay
+        ) return false
+
+        return true
+
+        return true
+    }
+
+    override fun hashCode(): Int {
+        var result = checkedBackgroundPainter.hashCode()
+        result = 31 * result + checkedContentColor.hashCode()
+        result = 31 * result + checkedSecondaryContentColor.hashCode()
+        result = 31 * result + checkedIconTintColor.hashCode()
+        result = 31 * result + checkedSplitBackgroundOverlay.hashCode()
+        result = 31 * result + uncheckedBackgroundPainter.hashCode()
+        result = 31 * result + uncheckedContentColor.hashCode()
+        result = 31 * result + uncheckedSecondaryContentColor.hashCode()
+        result = 31 * result + uncheckedIconTintColor.hashCode()
+        result = 31 * result + uncheckedSplitBackgroundOverlay.hashCode()
+        result = 31 * result + disabledCheckedBackgroundPainter.hashCode()
+        result = 31 * result + disabledCheckedContentColor.hashCode()
+        result = 31 * result + disabledCheckedSecondaryContentColor.hashCode()
+        result = 31 * result + disabledCheckedIconTintColor.hashCode()
+        result = 31 * result + disabledCheckedSplitBackgroundOverlay.hashCode()
+        result = 31 * result + disabledUncheckedBackgroundPainter.hashCode()
+        result = 31 * result + disabledUncheckedContentColor.hashCode()
+        result = 31 * result + disabledUncheckedSecondaryContentColor.hashCode()
+        result = 31 * result + disabledUncheckedIconTintColor.hashCode()
+        result = 31 * result + disabledUncheckedSplitBackgroundOverlay.hashCode()
+        return result
+    }
+}
