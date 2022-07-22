d072b90843237d03488a03450e5332c1d50d6722
Daniel Shokouhi <dshokouhi@gmail.com>
2022-07-21 17:39:14 -0700

    Breaking Change: Add friendly BT name next to the mac address if we have it (#2697)
    * Breaking Change: Add friendly BT name next to the mac address if we have it
    
    * Only add the name if we have it

--- app/src/main/java/io/homeassistant/companion/android/sensors/BluetoothSensorManager.kt
+++ app/src/main/java/io/homeassistant/companion/android/sensors/BluetoothSensorManager.kt
@@ -5,6 +5,7 @@ import android.content.Context
 import android.os.Build
 import io.homeassistant.companion.android.bluetooth.ble.IBeaconTransmitter
 import io.homeassistant.companion.android.bluetooth.ble.TransmitterManager
+import io.homeassistant.companion.android.common.bluetooth.BluetoothDevice
 import io.homeassistant.companion.android.common.bluetooth.BluetoothUtils
 import io.homeassistant.companion.android.common.bluetooth.BluetoothUtils.supportsTransmitter
 import io.homeassistant.companion.android.common.sensors.SensorManager
@@ -40,7 +41,6 @@ class BluetoothSensorManager : SensorManager {
         private const val DEFAULT_MEASURED_POWER_AT_1M = -59
         private var priorBluetoothStateEnabled = false
 
-        // private const val TAG = "BluetoothSM"
         private var bleTransmitterDevice = IBeaconTransmitter("", "", "", transmitPowerSetting = "", measuredPowerSetting = 0, advertiseModeSetting = "", transmitting = false, state = "", restartRequired = false)
         val bluetoothConnection = SensorManager.BasicSensor(
             "bluetooth_connection",
@@ -136,10 +136,10 @@ class BluetoothSensorManager : SensorManager {
         if (checkPermission(context, bluetoothConnection.id)) {
 
             val bluetoothDevices = BluetoothUtils.getBluetoothDevices(context, true)
-            pairedDevices = bluetoothDevices.filter { b -> b.paired }.map { it.address }
-            connectedPairedDevices = bluetoothDevices.filter { b -> b.paired && b.connected }.map { it.address }
-            connectedNotPairedDevices = bluetoothDevices.filter { b -> !b.paired && b.connected }.map { it.address }
-            totalConnectedDevices = bluetoothDevices.filter { b -> b.connected }.count()
+            pairedDevices = bluetoothDevices.filter { b -> b.paired }.map { checkNameAddress(it) }
+            connectedPairedDevices = bluetoothDevices.filter { b -> b.paired && b.connected }.map { checkNameAddress(it) }
+            connectedNotPairedDevices = bluetoothDevices.filter { b -> !b.paired && b.connected }.map { checkNameAddress(it) }
+            totalConnectedDevices = bluetoothDevices.count { b -> b.connected }
         }
         onSensorUpdated(
             context,
@@ -259,5 +259,9 @@ class BluetoothSensorManager : SensorManager {
                 "Supports transmitter" to supportsTransmitter(context)
             )
         )
+    }
+
+    private fun checkNameAddress(bt: BluetoothDevice): String {
+        return if (bt.address != bt.name) "${bt.address} (${bt.name})" else bt.address
     }
 }
