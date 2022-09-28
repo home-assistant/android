387e8249a58590d4719a9ee3ce79f38375a53b86
Daniel Shokouhi <dshokouhi@gmail.com>
2022-09-28 10:13:58 -0700

    Update iBeacon format to have underscores

--- app/src/main/java/io/homeassistant/companion/android/bluetooth/ble/IBeaconNameFormat.kt
+++ app/src/main/java/io/homeassistant/companion/android/bluetooth/ble/IBeaconNameFormat.kt
@@ -1,5 +1,5 @@
 package io.homeassistant.companion.android.bluetooth.ble
 
 fun <T> name(uuid: T, major: T, minor: T): String {
-    return "$uuid $major.$minor"
+    return "${uuid}_${major}_$minor"
 }
