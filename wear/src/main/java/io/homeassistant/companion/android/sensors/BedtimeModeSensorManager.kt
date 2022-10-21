a1cc72d7f239dcfcf7c3a5e730a62d31e7c5ca64
Daniel Shokouhi <dshokouhi@gmail.com>
2022-10-21 09:04:13 -0700

    Add more Wear OS sensors

---
+++ wear/src/main/java/io/homeassistant/companion/android/sensors/BedtimeModeSensorManager.kt
@@ -0,0 +1,69 @@
+package io.homeassistant.companion.android.sensors
+
+import android.content.Context
+import android.os.Build
+import android.provider.Settings
+import android.util.Log
+import io.homeassistant.companion.android.common.sensors.SensorManager
+import io.homeassistant.companion.android.common.R as commonR
+
+class BedtimeModeSensorManager : SensorManager {
+    companion object {
+        private const val TAG = "BedtimeSensor"
+
+        val bedtimeMode = SensorManager.BasicSensor(
+            "bedtime_mode",
+            "binary_sensor",
+            commonR.string.sensor_name_bedtime_mode,
+            commonR.string.sensor_description_bedtime_mode,
+            "mdi:sleep",
+            entityCategory = SensorManager.ENTITY_CATEGORY_DIAGNOSTIC,
+            updateType = SensorManager.BasicSensor.UpdateType.INTENT
+        )
+    }
+
+    override fun docsLink(): String {
+        return "https://companion.home-assistant.io/docs/core/sensors#bedtime-mode-sensor"
+    }
+    override val enabledByDefault: Boolean
+        get() = false
+    override val name: Int
+        get() = commonR.string.sensor_name_bedtime_mode
+
+    override fun getAvailableSensors(context: Context): List<SensorManager.BasicSensor> {
+        return listOf(bedtimeMode)
+    }
+
+    override fun requiredPermissions(sensorId: String): Array<String> {
+        return emptyArray()
+    }
+
+    override fun requestSensorUpdate(context: Context) {
+        updateBedtimeMode(context)
+    }
+
+    override fun hasSensor(context: Context): Boolean {
+        return Build.VERSION.SDK_INT >= Build.VERSION_CODES.R
+    }
+
+    private fun updateBedtimeMode(context: Context) {
+
+        if (!isEnabled(context, bedtimeMode.id))
+            return
+
+        val state = try {
+            Settings.Global.getInt(context.contentResolver, "bedtime_mode") == 1
+        } catch (e: Exception) {
+            Log.e(TAG, "Unable to update bedtime mode sensor", e)
+            false
+        }
+
+        onSensorUpdated(
+            context,
+            bedtimeMode,
+            state,
+            if (!state) "mdi:sleep-off" else bedtimeMode.statelessIcon,
+            mapOf()
+        )
+    }
+}
--- wear/src/main/java/io/homeassistant/companion/android/sensors/SensorReceiver.kt
+++ wear/src/main/java/io/homeassistant/companion/android/sensors/SensorReceiver.kt
@@ -1,15 +1,25 @@
 package io.homeassistant.companion.android.sensors
 
 import android.annotation.SuppressLint
+import android.app.NotificationManager
 import android.content.Context
 import android.content.Intent
+import android.media.AudioManager
 import android.net.wifi.WifiManager
+import android.os.PowerManager
 import dagger.hilt.android.AndroidEntryPoint
 import io.homeassistant.companion.android.BuildConfig
+import io.homeassistant.companion.android.common.sensors.AppSensorManager
+import io.homeassistant.companion.android.common.sensors.AudioSensorManager
 import io.homeassistant.companion.android.common.sensors.BatterySensorManager
+import io.homeassistant.companion.android.common.sensors.DNDSensorManager
+import io.homeassistant.companion.android.common.sensors.LastUpdateManager
 import io.homeassistant.companion.android.common.sensors.NetworkSensorManager
+import io.homeassistant.companion.android.common.sensors.NextAlarmManager
+import io.homeassistant.companion.android.common.sensors.PowerSensorManager
 import io.homeassistant.companion.android.common.sensors.SensorManager
 import io.homeassistant.companion.android.common.sensors.SensorReceiverBase
+import io.homeassistant.companion.android.common.sensors.StepsSensorManager
 
 @AndroidEntryPoint
 class SensorReceiver : SensorReceiverBase() {
@@ -26,9 +36,18 @@ class SensorReceiver : SensorReceiverBase() {
     companion object {
         const val TAG = "SensorReceiver"
         val MANAGERS = listOf(
+            AppSensorManager(),
+            AudioSensorManager(),
             BatterySensorManager(),
+            BedtimeModeSensorManager(),
+            DNDSensorManager(),
+            LastUpdateManager(),
             NetworkSensorManager(),
-            OnBodySensorManager()
+            NextAlarmManager(),
+            OnBodySensorManager(),
+            PowerSensorManager(),
+            StepsSensorManager(),
+            TheaterModeSensorManager()
         )
 
         const val ACTION_REQUEST_SENSORS_UPDATE =
@@ -38,7 +57,16 @@ class SensorReceiver : SensorReceiverBase() {
     // Suppress Lint because we only register for the receiver if the android version matches the intent
     @SuppressLint("InlinedApi")
     override val skippableActions = mapOf(
-        WifiManager.WIFI_STATE_CHANGED_ACTION to NetworkSensorManager.wifiState.id
+        WifiManager.WIFI_STATE_CHANGED_ACTION to NetworkSensorManager.wifiState.id,
+        "android.app.action.NEXT_ALARM_CLOCK_CHANGED" to NextAlarmManager.nextAlarm.id,
+        Intent.ACTION_SCREEN_OFF to PowerSensorManager.interactiveDevice.id,
+        Intent.ACTION_SCREEN_ON to PowerSensorManager.interactiveDevice.id,
+        PowerManager.ACTION_POWER_SAVE_MODE_CHANGED to PowerSensorManager.powerSave.id,
+        PowerManager.ACTION_DEVICE_IDLE_MODE_CHANGED to PowerSensorManager.doze.id,
+        NotificationManager.ACTION_INTERRUPTION_FILTER_CHANGED to DNDSensorManager.dndSensor.id,
+        AudioManager.ACTION_MICROPHONE_MUTE_CHANGED to AudioSensorManager.micMuted.id,
+        AudioManager.ACTION_SPEAKERPHONE_STATE_CHANGED to AudioSensorManager.speakerphoneState.id,
+        AudioManager.RINGER_MODE_CHANGED_ACTION to AudioSensorManager.audioSensor.id,
     )
 
     override fun getSensorSettingsIntent(context: Context, id: String): Intent? = null
