1faf03a490c68ba84c4eef1d1dada5fb4bfe49d8
Jorden <37350695+SkechyWolf@users.noreply.github.com>
2022-10-25 14:12:28 -0700

    Add Wet Mode Sensor (#2994)
    * Add Wet Mode Sensor
    
    * Add Wet Mode Sensor
    
    * Change Description
    
    * Fix spelling
    
    * Fix Sloppy mistakes

--- wear/src/main/java/io/homeassistant/companion/android/HomeAssistantApplication.kt
+++ wear/src/main/java/io/homeassistant/companion/android/HomeAssistantApplication.kt
@@ -83,6 +83,15 @@ open class HomeAssistantApplication : Application() {
                 addAction(PowerManager.ACTION_DEVICE_IDLE_MODE_CHANGED)
             }
         )
+
+        // Listen to changes to Wet Mode State
+        registerReceiver(
+            sensorReceiver,
+            IntentFilter().apply {
+                addAction("com.google.android.clockwork.actions.WET_MODE_STARTED")
+                addAction("com.google.android.clockwork.actions.WET_MODE_ENDED")
+            }
+        )
         // Update complications when the screen is on
         val complicationReceiver = ComplicationReceiver()
 
