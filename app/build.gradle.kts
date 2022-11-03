027953cf2b2eb12a71751c3b7784149704a05b09
Daniel Shokouhi <dshokouhi@gmail.com>
2022-11-03 10:28:45 -0700

    Revert reorderable bump until upstream issue is fixed

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -201,7 +201,7 @@ dependencies {
     implementation("com.mikepenz:iconics-compose:5.4.0")
     implementation("com.mikepenz:community-material-typeface:7.0.96.0-kotlin@aar")
 
-    "fullImplementation"("org.burnoutcrew.composereorderable:reorderable:0.9.4")
+    "fullImplementation"("org.burnoutcrew.composereorderable:reorderable:0.9.2")
     implementation("com.github.AppDevNext:ChangeLog:3.4")
 }
 
