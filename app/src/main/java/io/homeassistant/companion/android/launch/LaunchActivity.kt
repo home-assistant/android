ac36c89ef713c9727d1795553936cca71c1d181f
nestor <nestorgu@foxmail.com>
2022-11-28 11:13:27 +0800

    feat:去除firebase错误提示

--- app/src/main/java/io/homeassistant/companion/android/launch/LaunchActivity.kt
+++ app/src/main/java/io/homeassistant/companion/android/launch/LaunchActivity.kt
@@ -102,34 +102,42 @@ class LaunchActivity : AppCompatActivity(), LaunchView {
         mainScope.launch {
             if (result != null) {
                 val (url, authCode, deviceName, deviceTrackingEnabled, notificationsEnabled) = result
-                val messagingToken = getMessagingToken()
-                if (messagingToken.isBlank() && BuildConfig.FLAVOR == "full") {
-                    AlertDialog.Builder(this@LaunchActivity)
-                        .setTitle(commonR.string.firebase_error_title)
-                        .setMessage(commonR.string.firebase_error_message)
-                        .setPositiveButton(commonR.string.continue_connect) { _, _ ->
-                            mainScope.launch {
-                                registerAndOpenWebview(
-                                    url,
-                                    authCode,
-                                    deviceName,
-                                    messagingToken,
-                                    deviceTrackingEnabled,
-                                    notificationsEnabled
-                                )
-                            }
-                        }
-                        .show()
-                } else {
-                    registerAndOpenWebview(
-                        url,
-                        authCode,
-                        deviceName,
-                        messagingToken,
-                        deviceTrackingEnabled,
-                        notificationsEnabled
-                    )
-                }
+                registerAndOpenWebview(
+                    url,
+                    authCode,
+                    deviceName,
+                    "",
+                    deviceTrackingEnabled,
+                    notificationsEnabled
+                )
+//                val messagingToken = getMessagingToken()
+//                if (messagingToken.isBlank() && BuildConfig.FLAVOR == "full") {
+//                    AlertDialog.Builder(this@LaunchActivity)
+//                        .setTitle(commonR.string.firebase_error_title)
+//                        .setMessage(commonR.string.firebase_error_message)
+//                        .setPositiveButton(commonR.string.continue_connect) { _, _ ->
+//                            mainScope.launch {
+//                                registerAndOpenWebview(
+//                                    url,
+//                                    authCode,
+//                                    deviceName,
+//                                    messagingToken,
+//                                    deviceTrackingEnabled,
+//                                    notificationsEnabled
+//                                )
+//                            }
+//                        }
+//                        .show()
+//                } else {
+//                    registerAndOpenWebview(
+//                        url,
+//                        authCode,
+//                        deviceName,
+//                        messagingToken,
+//                        deviceTrackingEnabled,
+//                        notificationsEnabled
+//                    )
+//                }
             } else
                 Log.e(TAG, "onOnboardingComplete: Activity result returned null intent data")
         }
