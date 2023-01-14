cdff2f8a9cf44e47137c91e3b9e9ba2dd477b437
Justin Bassett <Bassett.JustinT@gmail.com>
2023-01-14 17:06:16 -0500

    Add and Android Auto sensor!

---
+++ app/src/full/java/io/homeassistant/companion/android/sensors/AndroidAutoSensorManager.kt
@@ -0,0 +1,89 @@
+package io.homeassistant.companion.android.sensors
+
+import android.content.Context
+import androidx.car.app.connection.CarConnection
+import androidx.lifecycle.Observer
+import io.homeassistant.companion.android.common.sensors.SensorManager
+import kotlinx.coroutines.CoroutineScope
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.Job
+import kotlinx.coroutines.launch
+import io.homeassistant.companion.android.common.R as commonR
+
+class AndroidAutoSensorManager : SensorManager, Observer<Int> {
+
+    companion object {
+
+        internal const val TAG = "AndroidAutoSM"
+
+        private val androidAutoConnected = SensorManager.BasicSensor(
+            "android_auto",
+            "binary_sensor",
+            commonR.string.basic_sensor_name_android_auto,
+            commonR.string.sensor_description_android_auto,
+            "mdi:car",
+            updateType = SensorManager.BasicSensor.UpdateType.INTENT
+        )
+    }
+
+    override val enabledByDefault: Boolean
+        get() = false
+    override val name: Int
+        get() = commonR.string.sensor_name_android_auto
+
+    override suspend fun getAvailableSensors(context: Context): List<SensorManager.BasicSensor> {
+        return listOf(androidAutoConnected)
+    }
+
+    override fun requiredPermissions(sensorId: String): Array<String> {
+        return emptyArray()
+    }
+
+    private lateinit var context: Context
+    private var carConnection: CarConnection? = null
+
+    override fun requestSensorUpdate(context: Context) {
+        this.context = context
+        if (!isEnabled(context, androidAutoConnected.id)) {
+            return
+        }
+        CoroutineScope(Dispatchers.Main + Job()).launch {
+            if (carConnection == null) {
+                carConnection = CarConnection(context)
+            }
+            carConnection?.type?.observeForever(this@AndroidAutoSensorManager)
+        }
+    }
+
+    override fun onChanged(type: Int?) {
+        if (!isEnabled(context, androidAutoConnected.id)) {
+            CoroutineScope(Dispatchers.Main + Job()).launch {
+                carConnection?.type?.removeObserver(this@AndroidAutoSensorManager)
+            }
+            return
+        }
+        val (connected, typeString) = when (type) {
+            CarConnection.CONNECTION_TYPE_NOT_CONNECTED -> {
+                false to "Disconnected"
+            }
+            CarConnection.CONNECTION_TYPE_PROJECTION -> {
+                true to "Projection"
+            }
+            CarConnection.CONNECTION_TYPE_NATIVE -> {
+                true to "Native"
+            }
+            else -> {
+                false to "Unknown($type)"
+            }
+        }
+        onSensorUpdated(
+            context,
+            androidAutoConnected,
+            connected,
+            androidAutoConnected.statelessIcon,
+            mapOf(
+                "connection_type" to typeString
+            ),
+        )
+    }
+}
