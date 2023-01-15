bde563d2b8b470f03e27611d19b5c37a832e1f78
Justin Bassett <Bassett.JustinT@gmail.com>
2023-01-15 15:53:22 -0500

    Fix build.

--- app/src/minimal/java/io/homeassistant/companion/android/sensors/AndroidAutoSensorManager.kt
+++ app/src/minimal/java/io/homeassistant/companion/android/sensors/AndroidAutoSensorManager.kt
@@ -1,3 +1,5 @@
+package io.homeassistant.companion.android.sensors
+
 import android.content.Context
 import io.homeassistant.companion.android.common.sensors.SensorManager
 import io.homeassistant.companion.android.common.R as commonR
