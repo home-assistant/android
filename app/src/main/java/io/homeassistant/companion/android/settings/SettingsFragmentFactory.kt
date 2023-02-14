6f80a0603db965e7f438d7e79f0258723b674e14
Joris Pelgröm <joris.pelgrom@gmail.com>
2023-02-14 21:55:21 +0100

    Remove unused ServerManager

--- app/src/main/java/io/homeassistant/companion/android/settings/SettingsFragmentFactory.kt
+++ app/src/main/java/io/homeassistant/companion/android/settings/SettingsFragmentFactory.kt
@@ -3,22 +3,17 @@ package io.homeassistant.companion.android.settings
 import android.annotation.SuppressLint
 import androidx.fragment.app.Fragment
 import androidx.fragment.app.FragmentFactory
-import io.homeassistant.companion.android.common.data.servers.ServerManager
 import io.homeassistant.companion.android.settings.language.LanguagesProvider
 import javax.inject.Inject
 
 class SettingsFragmentFactory @Inject constructor(
     private val settingsPresenter: SettingsPresenter,
-    private val languagesProvider: LanguagesProvider,
-    private val serverManager: ServerManager
+    private val languagesProvider: LanguagesProvider
 ) : FragmentFactory() {
     @SuppressLint("NewApi")
     override fun instantiate(classLoader: ClassLoader, className: String): Fragment {
         return when (className) {
-            SettingsFragment::class.java.name -> SettingsFragment(
-                settingsPresenter,
-                languagesProvider
-            )
+            SettingsFragment::class.java.name -> SettingsFragment(settingsPresenter, languagesProvider)
             else -> super.instantiate(classLoader, className)
         }
     }
