10657691ec7aab0cbbc7d5748a1cbe35b49a7cf1
Daniel Shokouhi <dshokouhi@gmail.com>
2023-02-24 17:04:29 -0800

    Remove sensors that do not function on wear OS

--- wear/src/main/java/io/homeassistant/companion/android/sensors/ActivitySensorManager.kt
+++ wear/src/main/java/io/homeassistant/companion/android/sensors/ActivitySensorManager.kt
@@ -10,17 +10,9 @@ import dagger.hilt.android.AndroidEntryPoint
 import io.homeassistant.companion.android.common.sensors.SensorManager
 import io.homeassistant.companion.android.google.sensors.ActivityIntentData
 import io.homeassistant.companion.android.google.sensors.enableActivityUpdates
-import io.homeassistant.companion.android.google.sensors.enableSleepUpdates
 import io.homeassistant.companion.android.google.sensors.getActivityAttributes
 import io.homeassistant.companion.android.google.sensors.getProbActivity
 import io.homeassistant.companion.android.google.sensors.getSensorIcon
-import io.homeassistant.companion.android.google.sensors.getSleepClassifyEvent
-import io.homeassistant.companion.android.google.sensors.getSleepPendingIntent
-import io.homeassistant.companion.android.google.sensors.getSleepSegmentEvent
-import io.homeassistant.companion.android.google.sensors.getSleepSegmentStatus
-import io.homeassistant.companion.android.google.sensors.hasSleepClassifyEvents
-import io.homeassistant.companion.android.google.sensors.hasSleepSegmentEvent
-import io.homeassistant.companion.android.google.sensors.removeSleepUpdates
 import io.homeassistant.companion.android.common.R as commonR
 
 @AndroidEntryPoint
@@ -37,34 +29,12 @@ class ActivitySensorManager : BroadcastReceiver(), SensorManager {
             commonR.string.sensor_description_detected_activity,
             "mdi:walk"
         )
-
-        private val sleepConfidence = SensorManager.BasicSensor(
-            "sleep_confidence",
-            "sensor",
-            commonR.string.basic_sensor_name_sleep_confidence,
-            commonR.string.sensor_description_sleep_confidence,
-            "mdi:sleep",
-            unitOfMeasurement = "%",
-            stateClass = SensorManager.STATE_CLASS_MEASUREMENT,
-            updateType = SensorManager.BasicSensor.UpdateType.CUSTOM
-        )
-
-        private val sleepSegment = SensorManager.BasicSensor(
-            "sleep_segment",
-            "sensor",
-            commonR.string.basic_sensor_name_sleep_segment,
-            commonR.string.sensor_description_sleep_segment,
-            "mdi:sleep",
-            unitOfMeasurement = "ms",
-            updateType = SensorManager.BasicSensor.UpdateType.CUSTOM
-        )
     }
 
     override fun onReceive(context: Context, intent: Intent) {
 
         when (intent.action) {
             ActivityIntentData.ACTION_UPDATE_ACTIVITY -> handleActivityUpdate(intent, context)
-            ActivityIntentData.ACTION_SLEEP_ACTIVITY -> handleSleepUpdate(intent, context)
             else -> Log.w(TAG, "Unknown intent action: ${intent.action}!")
         }
     }
@@ -83,49 +53,6 @@ class ActivitySensorManager : BroadcastReceiver(), SensorManager {
             )
     }
 
-    private fun handleSleepUpdate(intent: Intent, context: Context) {
-        Log.d(TAG, "Received sleep update")
-        if (hasSleepClassifyEvents(intent) && isEnabled(context, sleepConfidence)) {
-            Log.d(TAG, "Sleep classify event detected")
-            val sleepClassifyEvent = getSleepClassifyEvent(intent)
-            if (!sleepClassifyEvent.isNullOrEmpty()) {
-                Log.d(TAG, "Sleep classify has an actual event")
-                onSensorUpdated(
-                    context,
-                    sleepConfidence,
-                    sleepClassifyEvent["confidence"]!!,
-                    sleepConfidence.statelessIcon,
-                    mapOf(
-                        "light" to sleepClassifyEvent["light"],
-                        "motion" to sleepClassifyEvent["motion"],
-                        "timestamp" to sleepClassifyEvent["timestamp"]
-                    )
-                )
-
-                // Send the update immediately
-                SensorReceiver.updateAllSensors(context)
-            }
-        }
-        if (hasSleepSegmentEvent(intent) && isEnabled(context, sleepSegment)) {
-            Log.d(TAG, "Sleep segment event detected")
-            val sleepSegmentEvent = getSleepSegmentEvent(intent)
-            if (!sleepSegmentEvent.isNullOrEmpty()) {
-                Log.d(TAG, "Sleep segment has an actual event")
-                onSensorUpdated(
-                    context,
-                    sleepSegment,
-                    sleepSegmentEvent["duration"]!!,
-                    sleepSegment.statelessIcon,
-                    mapOf(
-                        "start" to sleepSegmentEvent["start"],
-                        "end" to sleepSegmentEvent["end"],
-                        "status" to getSleepSegmentStatus(sleepSegmentEvent["status"] as Int)
-                    )
-                )
-            }
-        }
-    }
-
     override fun docsLink(): String {
         return "https://companion.home-assistant.io/docs/core/sensors#activity-sensors"
     }
@@ -134,7 +61,7 @@ class ActivitySensorManager : BroadcastReceiver(), SensorManager {
         get() = commonR.string.sensor_name_activity
 
     override suspend fun getAvailableSensors(context: Context): List<SensorManager.BasicSensor> {
-        return listOf(activity, sleepConfidence, sleepSegment)
+        return listOf(activity)
     }
 
     override fun requiredPermissions(sensorId: String): Array<String> {
@@ -149,32 +76,5 @@ class ActivitySensorManager : BroadcastReceiver(), SensorManager {
 
     override fun requestSensorUpdate(context: Context) {
         enableActivityUpdates(context, ActivitySensorManager::class.java, isEnabled(context, activity))
-        val pendingIntent = getSleepPendingIntent(context, ActivitySensorManager::class.java)
-        if (isEnabled(context, sleepConfidence) || isEnabled(context, sleepSegment)) {
-            Log.d(TAG, "Registering for sleep updates")
-            val task = enableSleepUpdates(
-                context,
-                if (isEnabled(context, sleepConfidence) &&
-                    !isEnabled(context, sleepSegment)
-                )
-                    "classify_only"
-                else if (
-                    !isEnabled(context, sleepConfidence) &&
-                    isEnabled(context, sleepSegment)
-                )
-                    "events_only"
-                else
-                    "both",
-                pendingIntent
-            )
-            task.addOnSuccessListener {
-                Log.d(TAG, "Successfully registered for sleep updates")
-            }
-            task.addOnFailureListener {
-                Log.e(TAG, "Failed to register for sleep updates", it)
-                removeSleepUpdates(context, pendingIntent)
-            }
-        } else
-            removeSleepUpdates(context, pendingIntent)
     }
 }
