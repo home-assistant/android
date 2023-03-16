e565867dd14e053e9d33b8ef7a193a22a093fc93
Diego Valenzuela <dvalenzuela@google.com>
2023-03-15 20:08:24 -0700

    Remove unused import.

--- app/src/main/java/io/homeassistant/companion/android/onboarding/OnboardingActivity.kt
+++ app/src/main/java/io/homeassistant/companion/android/onboarding/OnboardingActivity.kt
@@ -5,7 +5,6 @@ import android.os.Bundle
 import android.view.KeyEvent
 import androidx.activity.OnBackPressedCallback
 import androidx.activity.viewModels
-import androidx.appcompat.app.AppCompatActivity
 import androidx.core.app.NotificationManagerCompat
 import androidx.fragment.app.commit
 import dagger.hilt.android.AndroidEntryPoint
