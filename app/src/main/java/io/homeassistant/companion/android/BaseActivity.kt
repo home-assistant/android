114fec86b90a59f44777c812cae417c48b11dff3
Diego Valenzuela <dvalenzuela@google.com>
2023-03-20 14:14:41 -0700

    Code to support transition from Full app to Car App Library.

--- app/src/main/java/io/homeassistant/companion/android/BaseActivity.kt
+++ app/src/main/java/io/homeassistant/companion/android/BaseActivity.kt
@@ -1,5 +1,57 @@
 package io.homeassistant.companion.android
 
+import android.car.Car
+import android.car.drivingstate.CarUxRestrictions
+import android.car.drivingstate.CarUxRestrictionsManager
+import android.content.Intent
+import android.content.Intent.FLAG_ACTIVITY_NEW_TASK
+import android.content.pm.PackageManager
 import androidx.appcompat.app.AppCompatActivity
 
-open class BaseActivity : AppCompatActivity()
+open class BaseActivity : AppCompatActivity() {
+    var car: Car? = null
+    var carRestrictionManager: CarUxRestrictionsManager? = null
+
+    override fun onResume() {
+        super.onResume()
+        registerListener()
+    }
+
+
+    override fun onStop() {
+        super.onStop()
+        carRestrictionManager?.unregisterListener()
+        car?.disconnect()
+        car = null
+    }
+
+    fun registerListener() {
+        if (packageManager.hasSystemFeature(PackageManager.FEATURE_AUTOMOTIVE)) {
+            car = Car.createCar(this)
+            carRestrictionManager =
+                car?.getCarManager(Car.CAR_UX_RESTRICTION_SERVICE) as CarUxRestrictionsManager;
+            var listener =
+                CarUxRestrictionsManager.OnUxRestrictionsChangedListener { restrictions ->
+                    if (restrictions.isRequiresDistractionOptimization) {
+                        startCarAppActivity()
+                    }
+                }
+            carRestrictionManager?.registerListener(listener)
+
+        }
+    }
+
+    private fun startCarAppActivity() {
+
+        startActivity(
+            Intent(
+                this,
+                Class.forName("androidx.car.app.activity.CarAppActivity")
+            ).addFlags(FLAG_ACTIVITY_NEW_TASK)
+        )
+        overridePendingTransition(
+            androidx.appcompat.R.anim.abc_slide_in_bottom,
+            androidx.appcompat.R.anim.abc_slide_in_bottom
+        )
+    }
+}
