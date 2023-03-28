9f06233644deac87cd253ce4b0a3f048f31fd96e
Joris Pelgröm <joris.pelgrom@gmail.com>
2023-03-28 17:53:37 +0200

    Share Wear session with main app to fix mismatch
     - Exchange server information between the Wear app and phone app, and create a temporary server on the phone that holds the Wear server information, to ensure that the same server is used on both devices

--- common/src/main/java/io/homeassistant/companion/android/common/data/authentication/impl/AuthenticationRepositoryImpl.kt
+++ common/src/main/java/io/homeassistant/companion/android/common/data/authentication/impl/AuthenticationRepositoryImpl.kt
@@ -55,6 +55,15 @@ class AuthenticationRepositoryImpl @AssistedInject constructor(
         }
     }
 
+    override suspend fun registerRefreshToken(refreshToken: String) {
+        val url = server.connection.getUrl()?.toHttpUrlOrNull()
+        if (url == null) {
+            Log.e(TAG, "Unable to register session with refresh token.")
+            return
+        }
+        refreshSessionWithToken(refreshToken)
+    }
+
     override suspend fun retrieveExternalAuthentication(forceRefresh: Boolean): String {
         ensureValidSession(forceRefresh)
         return jacksonObjectMapper().writeValueAsString(
@@ -133,33 +142,37 @@ class AuthenticationRepositoryImpl @AssistedInject constructor(
         }
 
         if (server.session.isExpired() || forceRefresh) {
-            return authenticationService.refreshToken(
-                url.newBuilder().addPathSegments("auth/token").build(),
-                AuthenticationService.GRANT_TYPE_REFRESH,
-                server.session.refreshToken!!,
-                AuthenticationService.CLIENT_ID
-            ).let {
-                if (it.isSuccessful) {
-                    val refreshedToken = it.body() ?: throw AuthorizationException()
-                    serverManager.updateServer(
-                        server.copy(
-                            session = ServerSessionInfo(
-                                refreshedToken.accessToken,
-                                server.session.refreshToken,
-                                System.currentTimeMillis() / 1000 + refreshedToken.expiresIn,
-                                refreshedToken.tokenType,
-                                installId
-                            )
+            refreshSessionWithToken(server.session.refreshToken!!)
+        }
+    }
+
+    private suspend fun refreshSessionWithToken(refreshToken: String) {
+        return authenticationService.refreshToken(
+            server.connection.getUrl()?.toHttpUrlOrNull()!!.newBuilder().addPathSegments("auth/token").build(),
+            AuthenticationService.GRANT_TYPE_REFRESH,
+            refreshToken,
+            AuthenticationService.CLIENT_ID
+        ).let {
+            if (it.isSuccessful) {
+                val refreshedToken = it.body() ?: throw AuthorizationException()
+                serverManager.updateServer(
+                    server.copy(
+                        session = ServerSessionInfo(
+                            refreshedToken.accessToken,
+                            refreshToken,
+                            System.currentTimeMillis() / 1000 + refreshedToken.expiresIn,
+                            refreshedToken.tokenType,
+                            installId
                         )
                     )
-                    return@let
-                } else if (it.code() == 400 &&
-                    it.errorBody()?.string()?.contains("invalid_grant") == true
-                ) {
-                    revokeSession()
-                }
-                throw AuthorizationException()
+                )
+                return@let
+            } else if (it.code() == 400 &&
+                it.errorBody()?.string()?.contains("invalid_grant") == true
+            ) {
+                revokeSession()
             }
+            throw AuthorizationException()
         }
     }
 
