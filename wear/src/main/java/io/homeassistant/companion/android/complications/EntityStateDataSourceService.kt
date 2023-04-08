a020b3e329dd87ecb5534f0bf7ccd34fb47cd2a4
Joris Pelgröm <joris.pelgrom@gmail.com>
2023-04-08 13:38:53 +0200

    Add ComplicationType.LONG_TEXT support, friendly state
     - Makes it possible for watch faces to request a long text complication
     - Use the friendly state to support translated states and dates

--- wear/src/main/java/io/homeassistant/companion/android/complications/EntityStateDataSourceService.kt
+++ wear/src/main/java/io/homeassistant/companion/android/complications/EntityStateDataSourceService.kt
@@ -3,8 +3,10 @@ package io.homeassistant.companion.android.complications
 import android.graphics.Color
 import android.graphics.drawable.Icon
 import android.util.Log
+import androidx.annotation.StringRes
 import androidx.wear.watchface.complications.data.ComplicationData
 import androidx.wear.watchface.complications.data.ComplicationType
+import androidx.wear.watchface.complications.data.LongTextComplicationData
 import androidx.wear.watchface.complications.data.MonochromaticImage
 import androidx.wear.watchface.complications.data.PlainComplicationText
 import androidx.wear.watchface.complications.data.ShortTextComplicationData
@@ -15,6 +17,8 @@ import com.mikepenz.iconics.typeface.library.community.material.CommunityMateria
 import com.mikepenz.iconics.utils.colorInt
 import dagger.hilt.android.AndroidEntryPoint
 import io.homeassistant.companion.android.common.R
+import io.homeassistant.companion.android.common.data.integration.friendlyName
+import io.homeassistant.companion.android.common.data.integration.friendlyState
 import io.homeassistant.companion.android.common.data.integration.getIcon
 import io.homeassistant.companion.android.common.data.servers.ServerManager
 import io.homeassistant.companion.android.database.wear.EntityStateComplicationsDao
@@ -34,60 +38,112 @@ class EntityStateDataSourceService : SuspendingComplicationDataSourceService() {
     }
 
     override suspend fun onComplicationRequest(request: ComplicationRequest): ComplicationData? {
-        if (request.complicationType != ComplicationType.SHORT_TEXT) {
+        if (request.complicationType != ComplicationType.SHORT_TEXT && request.complicationType != ComplicationType.LONG_TEXT) {
             return null
         }
 
         val id = request.complicationInstanceId
+        val isShort = request.complicationType == ComplicationType.SHORT_TEXT
 
         val entityId = entityStateComplicationsDao.get(id)?.entityId
-            ?: return ShortTextComplicationData.Builder(
-                text = PlainComplicationText.Builder(getText(R.string.complication_entity_invalid)).build(),
-                contentDescription = PlainComplicationText.Builder(getText(R.string.complication_entity_state_content_description))
-                    .build()
-            ).build()
+            ?: return getErrorComplication(isShort, R.string.complication_entity_invalid)
 
         val entity = try {
             serverManager.integrationRepository().getEntity(entityId)
-                ?: return ShortTextComplicationData.Builder(
-                    text = PlainComplicationText.Builder(getText(R.string.state_unknown)).build(),
-                    contentDescription = PlainComplicationText.Builder(getText(R.string.complication_entity_state_content_description))
-                        .build()
-                ).build()
+                ?: return getErrorComplication(isShort, R.string.state_unknown)
         } catch (t: Throwable) {
             Log.e(TAG, "Unable to get entity state for $entityId: ${t.message}")
             return null
         }
 
-        val attributes = entity.attributes as Map<*, *>
         val icon = entity.getIcon(applicationContext) ?: CommunityMaterial.Icon.cmd_bookmark
         val iconBitmap = IconicsDrawable(this, icon).apply {
             colorInt = Color.WHITE
         }.toBitmap()
-        return ShortTextComplicationData.Builder(
-            text = PlainComplicationText.Builder(entity.state).build(),
-            contentDescription = PlainComplicationText.Builder(getText(R.string.complication_entity_state_content_description))
-                .build()
-        )
-            .setTapAction(ComplicationReceiver.getComplicationToggleIntent(this, request.complicationInstanceId))
-            .setMonochromaticImage(MonochromaticImage.Builder(Icon.createWithBitmap(iconBitmap)).build())
-            .setTitle(PlainComplicationText.Builder(attributes["friendly_name"] as String? ?: entity.entityId).build())
-            .build()
+
+        val title = PlainComplicationText.Builder(entity.friendlyName).build()
+        val text = PlainComplicationText.Builder(entity.friendlyState(this)).build()
+        val contentDescription = PlainComplicationText.Builder(getText(R.string.complication_entity_state_content_description)).build()
+        val monochromaticImage = MonochromaticImage.Builder(Icon.createWithBitmap(iconBitmap)).build()
+        val tapAction = ComplicationReceiver.getComplicationToggleIntent(this, request.complicationInstanceId)
+
+        return when (request.complicationType) {
+            ComplicationType.SHORT_TEXT -> {
+                ShortTextComplicationData.Builder(
+                    text = text,
+                    contentDescription = contentDescription
+                )
+                    .setTitle(title)
+                    .setTapAction(tapAction)
+                    .setMonochromaticImage(monochromaticImage)
+                    .build()
+            }
+            ComplicationType.LONG_TEXT -> {
+                LongTextComplicationData.Builder(
+                    text = text,
+                    contentDescription = contentDescription
+                )
+                    .setTitle(title)
+                    .setTapAction(tapAction)
+                    .setMonochromaticImage(monochromaticImage)
+                    .build()
+            }
+            else -> null // Already handled at the start of the function
+        }
     }
 
-    override fun getPreviewData(type: ComplicationType): ComplicationData =
-        ShortTextComplicationData.Builder(
-            text = PlainComplicationText.Builder(getText(R.string.complication_entity_state_preview)).build(),
-            contentDescription = PlainComplicationText.Builder(getText(R.string.complication_entity_state_content_description)).build()
-        )
-            .setMonochromaticImage(
-                MonochromaticImage.Builder(
-                    Icon.createWithResource(
-                        this,
-                        io.homeassistant.companion.android.R.drawable.ic_lightbulb
-                    )
-                ).build()
+    override fun getPreviewData(type: ComplicationType): ComplicationData? {
+        val text = PlainComplicationText.Builder(getText(R.string.complication_entity_state_preview)).build()
+        val contentDescription = PlainComplicationText.Builder(getText(R.string.complication_entity_state_content_description)).build()
+        val title = PlainComplicationText.Builder(getText(R.string.entity)).build()
+        val monochromaticImage = MonochromaticImage.Builder(
+            Icon.createWithResource(
+                this,
+                io.homeassistant.companion.android.R.drawable.ic_lightbulb
             )
-            .setTitle(PlainComplicationText.Builder(getText(R.string.entity)).build())
-            .build()
+        ).build()
+        return when (type) {
+            ComplicationType.SHORT_TEXT -> {
+                ShortTextComplicationData.Builder(
+                    text = text,
+                    contentDescription = contentDescription
+                )
+                    .setTitle(title)
+                    .setMonochromaticImage(monochromaticImage)
+                    .build()
+            }
+            ComplicationType.LONG_TEXT -> {
+                LongTextComplicationData.Builder(
+                    text = text,
+                    contentDescription = contentDescription
+                )
+                    .setTitle(title)
+                    .setMonochromaticImage(monochromaticImage)
+                    .build()
+            }
+            else -> {
+                Log.w(TAG, "Preview for unsupported complication type $type requested")
+                null
+            }
+        }
+    }
+
+    /**
+     * Get a simple complication for errors with [textRes] in the text slot
+     */
+    private fun getErrorComplication(isShort: Boolean, @StringRes textRes: Int): ComplicationData {
+        val text = PlainComplicationText.Builder(getText(textRes)).build()
+        val contentDescription = PlainComplicationText.Builder(getText(R.string.complication_entity_state_content_description)).build()
+        return if (isShort) {
+            ShortTextComplicationData.Builder(
+                text = text,
+                contentDescription = contentDescription
+            ).build()
+        } else {
+            LongTextComplicationData.Builder(
+                text = text,
+                contentDescription = contentDescription
+            ).build()
+        }
+    }
 }
