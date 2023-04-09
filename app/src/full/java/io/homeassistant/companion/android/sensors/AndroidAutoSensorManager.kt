5cfbdd22b10a3abf6428598948f93a627cc37aa0
drosoCode <contact@thomasz.me>
2023-04-09 19:36:42 +0200

    add other sensors
    car name, manufacturer, manufacturing year, odometer, ev status (connected, charging port open/close)

--- app/src/full/java/io/homeassistant/companion/android/sensors/AndroidAutoSensorManager.kt
+++ app/src/full/java/io/homeassistant/companion/android/sensors/AndroidAutoSensorManager.kt
@@ -8,8 +8,10 @@ import android.util.Log
 import androidx.annotation.RequiresApi
 import androidx.car.app.connection.CarConnection
 import androidx.car.app.hardware.common.CarValue
-import androidx.car.app.hardware.common.OnCarDataAvailableListener
 import androidx.car.app.hardware.info.EnergyLevel
+import androidx.car.app.hardware.info.EvStatus
+import androidx.car.app.hardware.info.Mileage
+import androidx.car.app.hardware.info.Model
 import androidx.car.app.notification.CarAppExtender
 import androidx.car.app.notification.CarNotificationManager
 import androidx.car.app.notification.CarPendingIntent
@@ -17,6 +19,7 @@ import androidx.core.app.NotificationChannelCompat
 import androidx.core.app.NotificationCompat
 import androidx.core.app.NotificationManagerCompat
 import androidx.core.content.ContextCompat
+import androidx.lifecycle.DefaultLifecycleObserver
 import androidx.lifecycle.Observer
 import io.homeassistant.companion.android.common.sensors.SensorManager
 import io.homeassistant.companion.android.vehicle.HaCarAppService
@@ -29,14 +32,11 @@ import io.homeassistant.companion.android.common.R as commonR
 class AndroidAutoSensorManager :
     SensorManager,
     Observer<Int>,
-    OnCarDataAvailableListener<EnergyLevel> {
+    DefaultLifecycleObserver {
 
     companion object {
 
         internal const val TAG = "AndroidAutoSM"
-
-        private var isEnergyListenerRegistered = false
-        private var energyListenerLastRegistered = 0
 
         private val androidAutoConnected = SensorManager.BasicSensor(
             "android_auto",
@@ -53,7 +53,8 @@ class AndroidAutoSensorManager :
             io.homeassistant.companion.android.common.R.string.sensor_description_android_auto_fuel_level,
             "mdi:barrel",
             unitOfMeasurement = "%",
-            stateClass = SensorManager.STATE_CLASS_MEASUREMENT
+            stateClass = SensorManager.STATE_CLASS_MEASUREMENT,
+            deviceClass = "battery"
         )
         private val batteryLevel = SensorManager.BasicSensor(
             "android_auto_battery_level",
@@ -62,7 +63,49 @@ class AndroidAutoSensorManager :
             io.homeassistant.companion.android.common.R.string.sensor_description_android_auto_battery_level,
             "mdi:car-battery",
             unitOfMeasurement = "%",
-            stateClass = SensorManager.STATE_CLASS_MEASUREMENT
+            stateClass = SensorManager.STATE_CLASS_MEASUREMENT,
+            deviceClass = "battery"
+        )
+        private val carName = SensorManager.BasicSensor(
+            "android_auto_car_name",
+            "sensor",
+            io.homeassistant.companion.android.common.R.string.basic_sensor_name_android_auto_car_name,
+            io.homeassistant.companion.android.common.R.string.sensor_description_android_auto_car_name,
+            "mdi:car-info"
+        )
+        private val carStatus = SensorManager.BasicSensor(
+            "android_auto_car_status",
+            "sensor",
+            io.homeassistant.companion.android.common.R.string.basic_sensor_name_android_auto_car_status,
+            io.homeassistant.companion.android.common.R.string.sensor_description_android_auto_car_status,
+            "mdi:ev-station",
+            deviceClass = "plug"
+        )
+        private val odometerValue = SensorManager.BasicSensor(
+            "android_auto_odometer",
+            "sensor",
+            io.homeassistant.companion.android.common.R.string.basic_sensor_name_android_auto_odometer,
+            io.homeassistant.companion.android.common.R.string.sensor_description_android_auto_odometer,
+            "mdi:map-marker-distance",
+            unitOfMeasurement = "m",
+            stateClass = SensorManager.STATE_CLASS_TOTAL_INCREASING,
+            deviceClass = "distance"
+        )
+        private val sensorsList = listOf(androidAutoConnected, batteryLevel, carName, carStatus, fuelLevel, odometerValue)
+        private enum class Listener {
+            ENERGY, MODEL, MILEAGE, STATUS,
+        }
+        private val listenerSensors = mapOf(
+            Listener.ENERGY to listOf(batteryLevel, fuelLevel),
+            Listener.MODEL to listOf(carName),
+            Listener.STATUS to listOf(carStatus),
+            Listener.MILEAGE to listOf(odometerValue)
+        )
+        private val listenerLastRegistered = mutableMapOf(
+            Listener.ENERGY to -1L,
+            Listener.MODEL to -1L,
+            Listener.STATUS to -1L,
+            Listener.MILEAGE to -1L
         )
     }
 
@@ -71,7 +114,7 @@ class AndroidAutoSensorManager :
 
     override suspend fun getAvailableSensors(context: Context): List<SensorManager.BasicSensor> {
         return if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.O) {
-            listOf(androidAutoConnected, batteryLevel, fuelLevel)
+            sensorsList
         } else {
             emptyList()
         }
@@ -82,6 +125,9 @@ class AndroidAutoSensorManager :
             (sensorId == fuelLevel.id || sensorId == batteryLevel.id) -> {
                 arrayOf("com.google.android.gms.permission.CAR_FUEL")
             }
+            sensorId == odometerValue.id -> {
+                arrayOf("com.google.android.gms.permission.CAR_MILEAGE")
+            }
             else -> emptyArray()
         }
     }
@@ -89,10 +135,14 @@ class AndroidAutoSensorManager :
     private lateinit var context: Context
     private var carConnection: CarConnection? = null
 
+    private fun allDisabled(): Boolean {
+        return !sensorsList.map { x -> isEnabled(context, x) }.contains(true)
+    }
+
     @RequiresApi(Build.VERSION_CODES.O)
     override fun requestSensorUpdate(context: Context) {
         this.context = context.applicationContext
-        if (!isEnabled(context, androidAutoConnected) && !isEnabled(context, fuelLevel) && !isEnabled(context, batteryLevel)) {
+        if (allDisabled()) {
             return
         }
         CoroutineScope(Dispatchers.Main + Job()).launch {
@@ -101,12 +151,12 @@ class AndroidAutoSensorManager :
             }
             carConnection?.type?.observeForever(this@AndroidAutoSensorManager)
         }
-        updateEnergy()
+        updateCarInfo()
     }
 
     @RequiresApi(Build.VERSION_CODES.O)
     override fun onChanged(value: Int) {
-        if (!isEnabled(context, androidAutoConnected) && !isEnabled(context, fuelLevel) && !isEnabled(context, batteryLevel)) {
+        if (allDisabled()) {
             CoroutineScope(Dispatchers.Main + Job()).launch {
                 carConnection?.type?.removeObserver(this@AndroidAutoSensorManager)
             }
@@ -171,30 +221,74 @@ class AndroidAutoSensorManager :
     }
 
     @RequiresApi(Build.VERSION_CODES.O)
-    private fun updateEnergy() {
-        if (!isEnabled(context, fuelLevel) && !isEnabled(context, batteryLevel)) {
+    @androidx.annotation.OptIn(androidx.car.app.annotations.ExperimentalCarApi::class)
+    private fun setListener(l: Listener, enable: Boolean) {
+        if (enable) {
+            Log.d(TAG, "registering AA $l listener")
+        } else {
+            Log.d(TAG, "unregistering AA $l listener")
+        }
+
+        val car = HaCarAppService.carInfo
+        if (car == null) {
+            Log.d(TAG, "AA CarInfo sensors not available")
             return
         }
 
-        val now = System.currentTimeMillis()
-        if (energyListenerLastRegistered + SensorManager.SENSOR_LISTENER_TIMEOUT < now && isEnergyListenerRegistered) {
-            Log.d(TAG, "Re-registering AA energy listener as it appears to be stuck")
-            HaCarAppService.carInfo?.removeEnergyLevelListener(this)
-            isEnergyListenerRegistered = false
+        when (l) {
+            Listener.ENERGY -> {
+                if (enable) {
+                    car.addEnergyLevelListener(ContextCompat.getMainExecutor(context), ::onEnergyAvailable)
+                } else {
+                    car.removeEnergyLevelListener(::onEnergyAvailable)
+                }
+            }
+            Listener.MILEAGE -> {
+                if (enable) {
+                    car.addMileageListener(ContextCompat.getMainExecutor(context), ::onMileageAvailable)
+                } else {
+                    car.removeMileageListener(::onMileageAvailable)
+                }
+            }
+            Listener.MODEL -> {
+                if (enable) {
+                    car.fetchModel(ContextCompat.getMainExecutor(context), ::onModelAvailable)
+                }
+            }
+            Listener.STATUS -> {
+                if (enable) {
+                    car.addEvStatusListener(ContextCompat.getMainExecutor(context), ::onStatusAvailable)
+                } else {
+                    car.removeEvStatusListener(::onStatusAvailable)
+                }
+            }
         }
 
-        if (HaCarAppService.carInfo != null) {
-            HaCarAppService.carInfo?.addEnergyLevelListener(ContextCompat.getMainExecutor(context), this)
-            Log.d(TAG, "AA energy sensor listener registered")
-            isEnergyListenerRegistered = true
-            energyListenerLastRegistered = now.toInt()
+        if (enable) {
+            listenerLastRegistered[l] = System.currentTimeMillis()
         } else {
-            Log.d(TAG, "AA energy sensor not available")
+            listenerLastRegistered[l] = -1L
         }
     }
 
     @RequiresApi(Build.VERSION_CODES.O)
-    override fun onCarDataAvailable(data: EnergyLevel) {
+    private fun updateCarInfo() {
+        for (l in listenerSensors) {
+            if (l.value.map { x -> isEnabled(context, x) }.contains(true)) {
+                if (listenerLastRegistered[l.key] != -1L && listenerLastRegistered[l.key]!! + SensorManager.SENSOR_LISTENER_TIMEOUT < System.currentTimeMillis()) {
+                    Log.d(TAG, "Re-registering AA ${l.key} listener as it appears to be stuck")
+                    setListener(l.key, false)
+                }
+
+                if (listenerLastRegistered[l.key] == -1L) {
+                    setListener(l.key, true)
+                }
+            }
+        }
+    }
+
+    @RequiresApi(Build.VERSION_CODES.O)
+    fun onEnergyAvailable(data: EnergyLevel) {
         if (data.fuelPercent.status == CarValue.STATUS_SUCCESS && isEnabled(context, fuelLevel)) {
             onSensorUpdated(
                 context,
@@ -213,8 +307,55 @@ class AndroidAutoSensorManager :
                 mapOf()
             )
         }
-        HaCarAppService.carInfo?.removeEnergyLevelListener(this)
-        Log.d(TAG, "AA energy sensor listener unregistered")
-        isEnergyListenerRegistered = false
+        setListener(Listener.ENERGY, false)
+    }
+
+    @RequiresApi(Build.VERSION_CODES.O)
+    fun onModelAvailable(data: Model) {
+        if (data.name.status == CarValue.STATUS_SUCCESS && isEnabled(context, carName)) {
+            onSensorUpdated(
+                context,
+                carName,
+                data.name.value!!,
+                carName.statelessIcon,
+                mapOf(
+                    "android_auto_car_manufacturer" to data.manufacturer.value,
+                    "android_auto_car_manufactured_year" to data.year.value
+                )
+            )
+        }
+        setListener(Listener.MODEL, false)
+    }
+
+    @RequiresApi(Build.VERSION_CODES.O)
+    @androidx.annotation.OptIn(androidx.car.app.annotations.ExperimentalCarApi::class)
+    fun onStatusAvailable(data: EvStatus) {
+        if (data.evChargePortConnected.status == CarValue.STATUS_SUCCESS && isEnabled(context, carStatus)) {
+            onSensorUpdated(
+                context,
+                carStatus,
+                data.evChargePortConnected.value == true,
+                carStatus.statelessIcon,
+                mapOf(
+                    "android_auto_status_charge_port_open" to (data.evChargePortOpen.value == true)
+                )
+            )
+        }
+        setListener(Listener.STATUS, false)
+    }
+
+    @RequiresApi(Build.VERSION_CODES.O)
+    @androidx.annotation.OptIn(androidx.car.app.annotations.ExperimentalCarApi::class)
+    fun onMileageAvailable(data: Mileage) {
+        if (data.odometerMeters.status == CarValue.STATUS_SUCCESS && isEnabled(context, odometerValue)) {
+            onSensorUpdated(
+                context,
+                odometerValue,
+                data.odometerMeters.value!!,
+                odometerValue.statelessIcon,
+                mapOf()
+            )
+        }
+        setListener(Listener.MILEAGE, false)
     }
 }
