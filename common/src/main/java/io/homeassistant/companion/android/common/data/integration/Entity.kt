73164e0304e76a642e223bff21744edc530d6308
Diego Valenzuela <dvalenzuela@google.com>
2023-05-14 20:07:24 -0700

    Entity to executing take 2

--- common/src/main/java/io/homeassistant/companion/android/common/data/integration/Entity.kt
+++ common/src/main/java/io/homeassistant/companion/android/common/data/integration/Entity.kt
@@ -643,4 +643,10 @@ fun <T> Entity<T>.friendlyState(context: Context): String {
     return friendlyState
 }
 
-fun <T> Entity<T>.isExecuting() = state.endsWith("ing")
+fun <T> Entity<T>.isExecuting() = when (state) {
+    "closing" -> true
+    "locking" -> true
+    "opening" -> true
+    "unlocking" -> true
+    else -> false
+}
