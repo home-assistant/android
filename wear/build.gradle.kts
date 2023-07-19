c4da52bdab6652a38ad24878aad195c5ddac0660
Justin Bassett <bassett.justint@gmail.com>
2023-07-19 15:35:11 -0400

    Hilt Dependency Update (#3681)
    * Bump com.google.dagger:hilt-android-gradle-plugin from 2.46.1 to 2.47
    
    Bumps [com.google.dagger:hilt-android-gradle-plugin](https://github.com/google/dagger) from 2.46.1 to 2.47.
    - [Release notes](https://github.com/google/dagger/releases)
    - [Changelog](https://github.com/google/dagger/blob/master/CHANGELOG.md)
    - [Commits](https://github.com/google/dagger/compare/dagger-2.46.1...dagger-2.47)
    
    ---
    updated-dependencies:
    - dependency-name: com.google.dagger:hilt-android-gradle-plugin
      dependency-type: direct:production
      update-type: version-update:semver-minor
    ...
    
    Signed-off-by: dependabot[bot] <support@github.com>
    
    * Bump com.google.dagger:hilt-android-compiler from 2.46.1 to 2.47
    
    Bumps [com.google.dagger:hilt-android-compiler](https://github.com/google/dagger) from 2.46.1 to 2.47.
    - [Release notes](https://github.com/google/dagger/releases)
    - [Changelog](https://github.com/google/dagger/blob/master/CHANGELOG.md)
    - [Commits](https://github.com/google/dagger/compare/dagger-2.46.1...dagger-2.47)
    
    ---
    updated-dependencies:
    - dependency-name: com.google.dagger:hilt-android-compiler
      dependency-type: direct:production
      update-type: version-update:semver-minor
    ...
    
    Signed-off-by: dependabot[bot] <support@github.com>
    
    * Bump com.google.dagger:hilt-android from 2.46.1 to 2.47
    
    Bumps [com.google.dagger:hilt-android](https://github.com/google/dagger) from 2.46.1 to 2.47.
    - [Release notes](https://github.com/google/dagger/releases)
    - [Changelog](https://github.com/google/dagger/blob/master/CHANGELOG.md)
    - [Commits](https://github.com/google/dagger/compare/dagger-2.46.1...dagger-2.47)
    
    ---
    updated-dependencies:
    - dependency-name: com.google.dagger:hilt-android
      dependency-type: direct:production
      update-type: version-update:semver-minor
    ...
    
    Signed-off-by: dependabot[bot] <support@github.com>
    
    ---------
    
    Signed-off-by: dependabot[bot] <support@github.com>
    Co-authored-by: dependabot[bot] <49699333+dependabot[bot]@users.noreply.github.com>

--- wear/build.gradle.kts
+++ wear/build.gradle.kts
@@ -102,8 +102,8 @@ dependencies {
     implementation("androidx.wear:wear-remote-interactions:1.0.0")
     implementation("androidx.wear:wear-phone-interactions:1.0.1")
 
-    implementation("com.google.dagger:hilt-android:2.46.1")
-    kapt("com.google.dagger:hilt-android-compiler:2.46.1")
+    implementation("com.google.dagger:hilt-android:2.47")
+    kapt("com.google.dagger:hilt-android-compiler:2.47")
 
     implementation("com.fasterxml.jackson.module:jackson-module-kotlin:2.13.5")
     implementation("com.squareup.okhttp3:okhttp:4.11.0")
