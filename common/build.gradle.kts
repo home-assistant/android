2a91c3adc697f047e331ea526cc1e6c86574cd9c
Bassett, Justin <Bassett.JustinT@gmail.com>
2023-07-27 16:45:52 -0400

    First Steps into making Gradle Catalogs

--- common/build.gradle.kts
+++ common/build.gradle.kts
@@ -55,34 +55,34 @@ android {
 }
 
 dependencies {
-    implementation("org.jetbrains.kotlin:kotlin-stdlib:1.8.22")
-    implementation("org.jetbrains.kotlin:kotlin-reflect:1.8.22")
-    implementation("org.jetbrains.kotlinx:kotlinx-coroutines-core:1.7.2")
+    implementation(libs.kotlin.stdlib)
+    implementation(libs.kotlin.reflect)
+    implementation(libs.kotlinx.coroutines.core)
 
-    implementation("com.google.dagger:hilt-android:2.47")
-    kapt("com.google.dagger:hilt-android-compiler:2.47")
+    implementation(libs.hilt.android)
+    kapt(libs.hilt.android.compiler)
 
-    implementation("androidx.appcompat:appcompat:1.6.1")
-    implementation("androidx.lifecycle:lifecycle-runtime-ktx:2.6.1")
-    implementation("androidx.lifecycle:lifecycle-viewmodel-ktx:2.6.1")
+    implementation(libs.appcompat)
+    implementation(libs.lifecycle.runtime.ktx)
+    implementation(libs.androidx.lifecycle.viewmodel.ktx)
 
-    api("androidx.room:room-runtime:2.5.2")
-    api("androidx.room:room-ktx:2.5.2")
-    kapt("androidx.room:room-compiler:2.5.2")
+    api(libs.androidx.room.runtime)
+    api(libs.androidx.room.ktx)
+    kapt(libs.androidx.room.compiler)
 
-    api("androidx.work:work-runtime-ktx:2.8.1")
+    api(libs.androidx.work.runtime.ktx)
 
-    api("com.squareup.retrofit2:retrofit:2.9.0")
-    implementation("com.squareup.retrofit2:converter-jackson:2.9.0")
-    implementation("com.squareup.okhttp3:okhttp:4.11.0")
-    implementation("com.squareup.okhttp3:logging-interceptor:4.11.0")
-    implementation("com.fasterxml.jackson.module:jackson-module-kotlin:2.13.5")
-    implementation("org.altbeacon:android-beacon-library:2.19.6")
+    api(libs.retrofit)
+    implementation(libs.converter.jackson)
+    implementation(libs.okhttp)
+    implementation(libs.logging.interceptor)
+    implementation(libs.jackson.module.kotlin)
+    implementation(libs.android.beacon.library)
 
-    implementation("com.mikepenz:iconics-core:5.4.0")
-    implementation("com.mikepenz:community-material-typeface:7.0.96.0-kotlin@aar")
+    implementation(libs.iconics.core)
+    implementation(libs.community.material.typeface)
 
-    implementation("com.vdurmont:emoji-java:5.1.1") {
+    implementation(libs.emojiJava) {
         exclude(group = "org.json", module = "json")
     }
 }
