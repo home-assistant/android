6c591f0a6f818a5c0ae9886af7048b31ed819937
Joris Pelgröm <joris.pelgrom@gmail.com>
2024-02-02 21:41:53 +0100

    on view -> in view

--- common/src/main/java/io/homeassistant/companion/android/util/IntervalToString.kt
+++ common/src/main/java/io/homeassistant/companion/android/util/IntervalToString.kt
@@ -6,7 +6,7 @@ import io.homeassistant.companion.android.common.R
 fun intervalToString(context: Context, interval: Int): String {
     return when {
         interval == 0 -> context.getString(R.string.interval_manual)
-        interval == 1 -> context.getString(R.string.interval_on_view)
+        interval == 1 -> context.getString(R.string.interval_in_view)
         interval >= 60 * 60 -> context.resources.getQuantityString(R.plurals.interval_hours, interval / 60 / 60, interval / 60 / 60)
         interval >= 60 -> context.resources.getQuantityString(R.plurals.interval_minutes, interval / 60, interval / 60)
         else -> context.resources.getQuantityString(R.plurals.interval_seconds, interval, interval)
