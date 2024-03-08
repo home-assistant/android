6c37fbcb78c0fc53475e31fc84dd0fe76563764a
vieck <termax02@gmail.com>
2024-03-07 23:34:43 -0500

    Revert "Filtering sensor toggles by server that you are on"
    This reverts commit 8f775f55c42d5b3d74747d8bafbb1e95daa22e04.

--- app/src/main/java/io/homeassistant/companion/android/settings/sensor/SensorDetailViewModel.kt
+++ app/src/main/java/io/homeassistant/companion/android/settings/sensor/SensorDetailViewModel.kt
@@ -17,6 +17,7 @@ import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.SavedStateHandle
 import androidx.lifecycle.viewModelScope
 import dagger.hilt.android.lifecycle.HiltViewModel
+import io.homeassistant.companion.android.common.R as commonR
 import io.homeassistant.companion.android.common.bluetooth.BluetoothUtils
 import io.homeassistant.companion.android.common.data.servers.ServerManager
 import io.homeassistant.companion.android.common.sensors.BluetoothSensorManager
@@ -32,6 +33,7 @@ import io.homeassistant.companion.android.database.settings.SensorUpdateFrequenc
 import io.homeassistant.companion.android.database.settings.SettingsDao
 import io.homeassistant.companion.android.sensors.LastAppSensorManager
 import io.homeassistant.companion.android.sensors.SensorReceiver
+import javax.inject.Inject
 import kotlinx.coroutines.async
 import kotlinx.coroutines.awaitAll
 import kotlinx.coroutines.flow.Flow
@@ -41,8 +43,6 @@ import kotlinx.coroutines.flow.asSharedFlow
 import kotlinx.coroutines.flow.asStateFlow
 import kotlinx.coroutines.launch
 import kotlinx.coroutines.runBlocking
-import javax.inject.Inject
-import io.homeassistant.companion.android.common.R as commonR
 
 @HiltViewModel
 class SensorDetailViewModel @Inject constructor(
@@ -147,8 +147,7 @@ class SensorDetailViewModel @Inject constructor(
     }
 
     init {
-        val serverId = serverManager.getServer()?.id ?: -1
-        val sensorFlow = sensorDao.getFullFlow(sensorId, serverId)
+        val sensorFlow = sensorDao.getFullFlow(sensorId)
         viewModelScope.launch {
             sensorFlow.collect { map ->
                 sensors = map.toSensorsWithAttributes()
