2a6bfcd04ea03f5b8bc988f75787b23f015c0c25
Nicolas Mowen <nickmowen213@gmail.com>
2024-03-20 15:33:58 -0600

    Support showing all frames of animated gif when sent as image in Android 14+

--- app/src/main/java/io/homeassistant/companion/android/notifications/MessagingManager.kt
+++ app/src/main/java/io/homeassistant/companion/android/notifications/MessagingManager.kt
@@ -13,6 +13,7 @@ import android.content.Intent
 import android.content.pm.PackageManager
 import android.graphics.Bitmap
 import android.graphics.BitmapFactory
+import android.graphics.drawable.Icon
 import android.media.AudioManager
 import android.media.MediaMetadataRetriever
 import android.media.RingtoneManager
@@ -38,6 +39,7 @@ import androidx.core.app.NotificationCompat
 import androidx.core.app.NotificationManagerCompat
 import androidx.core.app.RemoteInput
 import androidx.core.content.ContextCompat
+import androidx.core.content.FileProvider
 import androidx.core.content.getSystemService
 import androidx.core.text.isDigitsOnly
 import com.fasterxml.jackson.module.kotlin.jacksonObjectMapper
@@ -1199,8 +1201,15 @@ class MessagingManager @Inject constructor(
                 builder
                     .setLargeIcon(bitmap)
                     .setStyle(
-                        NotificationCompat.BigPictureStyle()
-                            .bigPicture(bitmap)
+                        NotificationCompat.BigPictureStyle().also { style ->
+                            if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.UPSIDE_DOWN_CAKE) {
+                                saveTempAnimatedImage(serverId, url, !UrlUtil.isAbsoluteUrl(dataImage))?.let { filePath ->
+                                    style.bigPicture(Icon.createWithContentUri(filePath))
+                                } ?: { style.bigPicture(bitmap) }
+                            } else {
+                                style.bigPicture(bitmap)
+                            }
+                        }
                             .bigLargeIcon(null as Bitmap?)
                     )
             }
@@ -1231,6 +1240,34 @@ class MessagingManager @Inject constructor(
                 Log.e(TAG, "Couldn't download image for notification", e)
             }
             return@withContext image
+        }
+
+    private suspend fun saveTempAnimatedImage(serverId: Int, url: URL?, requiresAuth: Boolean = false): Uri? =
+        withContext(
+            Dispatchers.IO
+        ) {
+            if (url == null || url.path.endsWith("gif").not()) {
+                return@withContext null
+            }
+
+            val file = File(context.externalCacheDir, "animated_notification_image.gif")
+            try {
+                val request = Request.Builder().apply {
+                    url(url)
+                    if (requiresAuth) {
+                        addHeader("Authorization", serverManager.authenticationRepository(serverId).buildBearerToken())
+                    }
+                }.build()
+
+                val response = okHttpClient.newCall(request).execute()
+                val bytes = response.body?.bytes() ?: return@withContext null
+                file.writeBytes(bytes)
+
+                response.close()
+            } catch (e: Exception) {
+                Log.e(TAG, "Couldn't download image for notification", e)
+            }
+            return@withContext FileProvider.getUriForFile(context, "${context.packageName}.provider", file)
         }
 
     private suspend fun handleVideo(
