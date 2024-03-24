eb255d73fb43a705286dbc0a83b97c4ddf0430d0
Joris Pelgröm <joris.pelgrom@gmail.com>
2024-03-24 21:25:45 +0100

    Add overlay with cutout and toggle flashlight
     - Add a overlay with cutout in the middle / on the side, matching design and Google's barcode scanner
     - Working button for toggling the flashlight on and off

---
+++ app/src/main/java/io/homeassistant/companion/android/barcode/view/BarcodeScannerOverlay.kt
@@ -0,0 +1,66 @@
+package io.homeassistant.companion.android.barcode.view
+
+import androidx.compose.foundation.Canvas
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.geometry.CornerRadius
+import androidx.compose.ui.geometry.Offset
+import androidx.compose.ui.geometry.Size
+import androidx.compose.ui.graphics.BlendMode
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.graphics.nativeCanvas
+import androidx.compose.ui.platform.LocalDensity
+import androidx.compose.ui.unit.Dp
+import androidx.compose.ui.unit.dp
+
+/**
+ * A semi-transparent overlay with a rounded square cutout in the middle (portrait) or on
+ * the right half (landscape), to use as an viewfinder for the barcode scanner's camera.
+ * Based on https://stackoverflow.com/a/73533699/4214819.
+ */
+@Composable
+fun BarcodeScannerOverlay(
+    modifier: Modifier,
+    cutout: Dp
+) {
+    val widthInPx: Float
+    val heightInPx: Float
+    val cornerInPx: Float
+
+    with(LocalDensity.current) {
+        widthInPx = cutout.toPx()
+        heightInPx = cutout.toPx()
+        cornerInPx = 28.dp.toPx() // Material 3 extra large rounding
+    }
+
+    Canvas(modifier = modifier) {
+        val canvasWidth = size.width
+        val canvasHeight = size.height
+
+        with(drawContext.canvas.nativeCanvas) {
+            val checkPoint = saveLayer(null, null)
+
+            // Destination
+            drawRect(barcodeScannerOverlayColor)
+
+            // Source
+            drawRoundRect(
+                topLeft = Offset(
+                    x = if (canvasWidth > canvasHeight) {
+                        (canvasWidth / 2) + (((canvasWidth / 2) - widthInPx) / 2)
+                    } else {
+                        (canvasWidth - widthInPx) / 2
+                    },
+                    y = (canvasHeight - heightInPx) / 2
+                ),
+                size = Size(widthInPx, heightInPx),
+                cornerRadius = CornerRadius(cornerInPx, cornerInPx),
+                color = Color.Transparent,
+                blendMode = BlendMode.Clear
+            )
+            restoreToCount(checkPoint)
+        }
+    }
+}
+
+val barcodeScannerOverlayColor = Color(0x77000000)
--- app/src/main/java/io/homeassistant/companion/android/barcode/view/BarcodeScannerView.kt
+++ app/src/main/java/io/homeassistant/companion/android/barcode/view/BarcodeScannerView.kt
@@ -1,11 +1,19 @@
 package io.homeassistant.companion.android.barcode.view
 
 import androidx.appcompat.view.ContextThemeWrapper
+import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.WindowInsets
+import androidx.compose.foundation.layout.asPaddingValues
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.safeDrawingPadding
+import androidx.compose.foundation.layout.systemBars
+import androidx.compose.foundation.layout.windowInsetsBottomHeight
+import androidx.compose.foundation.layout.windowInsetsTopHeight
 import androidx.compose.material.Button
 import androidx.compose.material.Icon
 import androidx.compose.material.IconButton
@@ -14,11 +22,17 @@ import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.filled.Close
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.DisposableEffect
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.platform.LocalConfiguration
 import androidx.compose.ui.platform.LocalContext
 import androidx.compose.ui.res.stringResource
+import androidx.compose.ui.unit.dp
 import androidx.compose.ui.viewinterop.AndroidView
 import androidx.core.view.isVisible
 import com.journeyapps.barcodescanner.CaptureManager
@@ -51,6 +65,7 @@ fun BarcodeScannerView(
             }
         }
     }
+    var flashlightOn by remember { mutableStateOf(false) }
 
     Box {
         AndroidView(
@@ -64,23 +79,56 @@ fun BarcodeScannerView(
                     barcodeView.pause()
                 }
             }
+            // TODO https://stackoverflow.com/a/66549433/4214819
         }
-        // TODO https://stackoverflow.com/a/73533699/4214819
+        Spacer(
+            modifier = Modifier
+                .background(barcodeScannerOverlayColor)
+                .fillMaxWidth()
+                .windowInsetsTopHeight(WindowInsets.systemBars)
+        )
+        Spacer(
+            modifier = Modifier
+                .background(barcodeScannerOverlayColor)
+                .fillMaxWidth()
+                .windowInsetsBottomHeight(WindowInsets.systemBars)
+                .align(Alignment.BottomStart)
+        )
 
-        Column(modifier = Modifier.safeDrawingPadding()) {
-            IconButton(onClick = onCancel) {
-                Icon(
-                    imageVector = Icons.Default.Close,
-                    contentDescription = stringResource(commonR.string.cancel),
-                    tint = Color.White
-                )
-            }
-            Row {
-                Button(onClick = requestPermission) {
-                    Text(text = "Grant camera permission")
+        Box(
+            modifier = Modifier
+                .safeDrawingPadding()
+                .fillMaxSize()
+        ) {
+            val configuration = LocalConfiguration.current
+            val systemBarsPadding = WindowInsets.systemBars.asPaddingValues()
+            val screenHeight = configuration.screenHeightDp.dp - systemBarsPadding.calculateTopPadding() - systemBarsPadding.calculateBottomPadding()
+            val screenWidth = configuration.screenWidthDp.dp
+            val cutoutSize = minOf(minOf(screenHeight, screenWidth) - 48.dp, 320.dp)
+
+            BarcodeScannerOverlay(modifier = Modifier.fillMaxSize(), cutout = cutoutSize)
+            Column {
+                IconButton(onClick = onCancel) {
+                    Icon(
+                        imageVector = Icons.Default.Close,
+                        contentDescription = stringResource(commonR.string.cancel),
+                        tint = Color.White
+                    )
                 }
-                Button(onClick = requestPermission) {
-                    Text(text = "Toggle flashlight")
+                Row {
+                    Button(onClick = requestPermission) {
+                        Text(text = "Grant camera permission")
+                    }
+                    Button(onClick = {
+                        if (flashlightOn) {
+                            barcodeView.setTorchOff()
+                        } else {
+                            barcodeView.setTorchOn()
+                        }
+                        flashlightOn = !flashlightOn
+                    }) {
+                        Text(text = "Toggle flashlight")
+                    }
                 }
             }
         }
