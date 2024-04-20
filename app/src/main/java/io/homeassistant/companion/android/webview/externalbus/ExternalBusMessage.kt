f37e34e9debc7b2e1b110b518b6fbc551b51c693
Joris Pelgröm <jpelgrom@users.noreply.github.com>
2024-04-20 20:09:52 +0200

    Provide interface for external bus outside WebViewActivity (#4338)
    - Adds an interface to the app to interact with the external bus outside the WebViewActivity. This should make it possible for the frontend to control more native UI of the app.

---
+++ app/src/main/java/io/homeassistant/companion/android/webview/externalbus/ExternalBusMessage.kt
@@ -0,0 +1,12 @@
+package io.homeassistant.companion.android.webview.externalbus
+
+import android.webkit.ValueCallback
+
+data class ExternalBusMessage(
+    val id: Any,
+    val type: String,
+    val success: Boolean,
+    val result: Any? = null,
+    val error: Any? = null,
+    val callback: ValueCallback<String>? = null
+)
