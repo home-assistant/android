e787c0dad950f72fb7f3a133915ebee3dd7a4f9d
Joris Pelgröm <joris.pelgrom@gmail.com>
2024-06-24 21:30:22 +0200

    Add toggle for voice command intent with Assist

--- app/src/main/java/io/homeassistant/companion/android/settings/SettingsView.kt
+++ app/src/main/java/io/homeassistant/companion/android/settings/SettingsView.kt
@@ -1,5 +1,8 @@
 package io.homeassistant.companion.android.settings
 
+import android.content.pm.PackageManager
+
 interface SettingsView {
     fun onAddServerResult(success: Boolean, serverId: Int?)
+    fun getPackageManager(): PackageManager?
 }
