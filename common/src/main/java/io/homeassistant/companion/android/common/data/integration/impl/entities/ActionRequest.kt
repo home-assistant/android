57b6fdb4e786dab4a2a3fd4a3d2b1b0f8d8ccd52
Daniel Shokouhi <dshokouhi@gmail.com>
2024-08-01 07:36:10 -0700

    Review comments

--- common/src/main/java/io/homeassistant/companion/android/common/data/integration/impl/entities/ActionRequest.kt
+++ common/src/main/java/io/homeassistant/companion/android/common/data/integration/impl/entities/ActionRequest.kt
@@ -2,6 +2,6 @@ package io.homeassistant.companion.android.common.data.integration.impl.entities
 
 data class ActionRequest(
     val domain: String,
-    val action: String,
-    val actionData: HashMap<String, Any>
+    val service: String,
+    val serviceData: HashMap<String, Any>
 )
