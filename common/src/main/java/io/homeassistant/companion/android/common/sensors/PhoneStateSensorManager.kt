ce827e8c4503b225022dba607ee40c8e0699fefc
Daniel Shokouhi <dshokouhi@gmail.com>
2024-08-12 08:50:01 -0700

    Add signal strength and data network type sensors

--- common/src/main/java/io/homeassistant/companion/android/common/sensors/PhoneStateSensorManager.kt
+++ common/src/main/java/io/homeassistant/companion/android/common/sensors/PhoneStateSensorManager.kt
@@ -5,10 +5,12 @@ import android.annotation.SuppressLint
 import android.content.Context
 import android.content.pm.PackageManager
 import android.os.Build
+import android.telephony.CellSignalStrength
 import android.telephony.SubscriptionInfo
 import android.telephony.SubscriptionManager
 import android.telephony.TelephonyManager
 import android.util.Log
+import androidx.annotation.RequiresApi
 import androidx.core.content.getSystemService
 import io.homeassistant.companion.android.common.R as commonR
 import io.homeassistant.companion.android.common.util.STATE_UNAVAILABLE
@@ -48,7 +50,50 @@ class PhoneStateSensorManager : SensorManager {
             entityCategory = SensorManager.ENTITY_CATEGORY_DIAGNOSTIC,
             updateType = SensorManager.BasicSensor.UpdateType.INTENT
         )
+
+        val signalStrength = SensorManager.BasicSensor(
+            "signal_strength",
+            "sensor",
+            commonR.string.basic_sensor_name_signal_strength,
+            commonR.string.sensor_description_signal_strength,
+            "mdi:signal",
+            entityCategory = SensorManager.ENTITY_CATEGORY_DIAGNOSTIC
+        )
+
+        val dataNetworkType = SensorManager.BasicSensor(
+            "data_network_type",
+            "sensor",
+            commonR.string.basic_sensor_name_data_network_type,
+            commonR.string.sensor_description_data_network_type,
+            "mdi:signal",
+            entityCategory = SensorManager.ENTITY_CATEGORY_DIAGNOSTIC
+        )
     }
+
+    @SuppressLint("InlinedApi")
+    @Suppress("DEPRECATION")
+    private val dataTypeMap = mapOf(
+        TelephonyManager.NETWORK_TYPE_GPRS to "GPRS",
+        TelephonyManager.NETWORK_TYPE_EDGE to "EDGE",
+        TelephonyManager.NETWORK_TYPE_UMTS to "UMTS",
+        TelephonyManager.NETWORK_TYPE_CDMA to "CDMA",
+        TelephonyManager.NETWORK_TYPE_EVDO_0 to "EVDO revision 0",
+        TelephonyManager.NETWORK_TYPE_EVDO_A to "EVDO revision A",
+        TelephonyManager.NETWORK_TYPE_1xRTT to "1xRTT",
+        TelephonyManager.NETWORK_TYPE_HSDPA to "HSDPA",
+        TelephonyManager.NETWORK_TYPE_HSUPA to "HSUPA",
+        TelephonyManager.NETWORK_TYPE_HSPA to "HSPA",
+        TelephonyManager.NETWORK_TYPE_IDEN to "iDen",
+        TelephonyManager.NETWORK_TYPE_EVDO_B to "EVDO revision B",
+        TelephonyManager.NETWORK_TYPE_LTE to "LTE",
+        TelephonyManager.NETWORK_TYPE_EHRPD to "eHRPD",
+        TelephonyManager.NETWORK_TYPE_HSPAP to "HSPA+",
+        TelephonyManager.NETWORK_TYPE_GSM to "GSM",
+        TelephonyManager.NETWORK_TYPE_TD_SCDMA to "TD_SCDMA",
+        TelephonyManager.NETWORK_TYPE_IWLAN to "IWLAN",
+        TelephonyManager.NETWORK_TYPE_NR to "NR (New Radio) 5G",
+        TelephonyManager.NETWORK_TYPE_UNKNOWN to STATE_UNKNOWN
+    )
 
     override fun docsLink(): String {
         return "https://companion.home-assistant.io/docs/core/sensors#cellular-provider-sensor"
@@ -59,10 +104,17 @@ class PhoneStateSensorManager : SensorManager {
         return context.packageManager.hasSystemFeature(PackageManager.FEATURE_TELEPHONY)
     }
     override suspend fun getAvailableSensors(context: Context): List<SensorManager.BasicSensor> {
-        return if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.LOLLIPOP_MR1) {
-            listOf(phoneState, sim_1, sim_2)
-        } else {
-            listOf(phoneState)
+        return when {
+            (Build.VERSION.SDK_INT >= Build.VERSION_CODES.Q && context.packageManager.hasSystemFeature(PackageManager.FEATURE_TELEPHONY_RADIO_ACCESS)) ->
+                listOf(phoneState, sim_1, sim_2, signalStrength, dataNetworkType)
+            (Build.VERSION.SDK_INT >= Build.VERSION_CODES.N && context.packageManager.hasSystemFeature(PackageManager.FEATURE_TELEPHONY_RADIO_ACCESS)) ->
+                listOf(phoneState, sim_1, sim_2, dataNetworkType)
+            Build.VERSION.SDK_INT >= Build.VERSION_CODES.LOLLIPOP_MR1 -> {
+                listOf(phoneState, sim_1, sim_2)
+            }
+            else -> {
+                listOf(phoneState)
+            }
         }
     }
 
@@ -76,6 +128,12 @@ class PhoneStateSensorManager : SensorManager {
         checkPhoneState(context)
         updateSimSensor(context, 0)
         updateSimSensor(context, 1)
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.Q) {
+            updateSignalStrength(context)
+        }
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.N) {
+            updateDataNetworkType(context)
+        }
     }
 
     @SuppressLint("MissingPermission")
@@ -164,5 +222,50 @@ class PhoneStateSensorManager : SensorManager {
                 attrs
             )
         }
+    }
+
+    @RequiresApi(Build.VERSION_CODES.Q)
+    private fun updateSignalStrength(context: Context) {
+        if (!isEnabled(context, signalStrength)) {
+            return
+        }
+
+        val telephonyManager =
+            context.applicationContext.getSystemService<TelephonyManager>()!!
+
+        val signalQuality = when (telephonyManager.signalStrength?.level) {
+            CellSignalStrength.SIGNAL_STRENGTH_POOR -> "poor"
+            CellSignalStrength.SIGNAL_STRENGTH_GOOD -> "good"
+            CellSignalStrength.SIGNAL_STRENGTH_MODERATE -> "moderate"
+            CellSignalStrength.SIGNAL_STRENGTH_GREAT -> "great"
+            else -> STATE_UNKNOWN
+        }
+        onSensorUpdated(
+            context,
+            signalStrength,
+            telephonyManager.signalStrength?.cellSignalStrengths?.firstOrNull()?.dbm ?: STATE_UNKNOWN,
+            signalStrength.statelessIcon,
+            mapOf(
+                "asu" to telephonyManager.signalStrength?.cellSignalStrengths?.firstOrNull()?.asuLevel,
+                "quality" to signalQuality
+            )
+        )
+    }
+
+    @RequiresApi(Build.VERSION_CODES.N)
+    private fun updateDataNetworkType(context: Context) {
+        if (!isEnabled(context, dataNetworkType)) {
+            return
+        }
+        val telephonyManager =
+            context.applicationContext.getSystemService<TelephonyManager>()!!
+
+        onSensorUpdated(
+            context,
+            dataNetworkType,
+            dataTypeMap.getOrDefault(telephonyManager.dataNetworkType, STATE_UNKNOWN),
+            dataNetworkType.statelessIcon,
+            mapOf("options" to dataTypeMap.values.toList())
+        )
     }
 }
