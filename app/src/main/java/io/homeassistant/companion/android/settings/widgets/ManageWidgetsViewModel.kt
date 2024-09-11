1d2b65bc6b840dbcf708fe6df3503323ccf35fdd
Ivor Smorenburg <ivor@smorenburg.me>
2024-09-11 02:48:20 +0200

    Linting attempt to fic

--- app/src/main/java/io/homeassistant/companion/android/settings/widgets/ManageWidgetsViewModel.kt
+++ app/src/main/java/io/homeassistant/companion/android/settings/widgets/ManageWidgetsViewModel.kt
@@ -16,6 +16,7 @@ import io.homeassistant.companion.android.database.widget.CameraWidgetDao
 import io.homeassistant.companion.android.database.widget.MediaPlayerControlsWidgetDao
 import io.homeassistant.companion.android.database.widget.StaticWidgetDao
 import io.homeassistant.companion.android.database.widget.TemplateWidgetDao
+import io.homeassistant.companion.android.database.widget.graph.GraphWidgetDao
 import javax.inject.Inject
 import kotlinx.coroutines.flow.Flow
 import kotlinx.coroutines.launch
@@ -25,6 +26,7 @@ class ManageWidgetsViewModel @Inject constructor(
     buttonWidgetDao: ButtonWidgetDao,
     cameraWidgetDao: CameraWidgetDao,
     staticWidgetDao: StaticWidgetDao,
+    graphWidgetDao: GraphWidgetDao,
     mediaPlayerControlsWidgetDao: MediaPlayerControlsWidgetDao,
     templateWidgetDao: TemplateWidgetDao,
     application: Application
@@ -39,6 +41,7 @@ class ManageWidgetsViewModel @Inject constructor(
     val buttonWidgetList = buttonWidgetDao.getAllFlow().collectAsState()
     val cameraWidgetList = cameraWidgetDao.getAllFlow().collectAsState()
     val staticWidgetList = staticWidgetDao.getAllFlow().collectAsState()
+    val graphWidgetList = graphWidgetDao.getAllFlow().collectAsState()
     val mediaWidgetList = mediaPlayerControlsWidgetDao.getAllFlow().collectAsState()
     val templateWidgetList = templateWidgetDao.getAllFlow().collectAsState()
     val supportsAddingWidgets: Boolean
