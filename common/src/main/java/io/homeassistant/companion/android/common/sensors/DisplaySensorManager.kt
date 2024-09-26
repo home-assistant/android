eb735696d1561e60156504f87628e7cb4c4254cd
Daniel Shokouhi <dshokouhi@gmail.com>
2024-09-26 14:15:27 -0700

    Remove face up or down sensor for now

--- common/src/main/java/io/homeassistant/companion/android/common/sensors/DisplaySensorManager.kt
+++ common/src/main/java/io/homeassistant/companion/android/common/sensors/DisplaySensorManager.kt
@@ -1,27 +1,18 @@
 package io.homeassistant.companion.android.common.sensors
 
 import android.content.Context
-import android.content.pm.PackageManager.FEATURE_SENSOR_ACCELEROMETER
 import android.content.res.Configuration
-import android.hardware.Sensor
-import android.hardware.SensorEvent
-import android.hardware.SensorEventListener
-import android.hardware.SensorManager.SENSOR_DELAY_NORMAL
 import android.hardware.display.DisplayManager
 import android.provider.Settings
 import android.util.Log
 import android.view.Display
 import android.view.Surface
-import androidx.core.content.getSystemService
 import io.homeassistant.companion.android.common.R as commonR
 import io.homeassistant.companion.android.common.util.STATE_UNKNOWN
 
-class DisplaySensorManager : SensorManager, SensorEventListener {
+class DisplaySensorManager : SensorManager {
     companion object {
         private const val TAG = "DisplaySensors"
-        private var hasAccelerometer = false
-        private var isListenerRegistered = false
-        private var listenerLastRegistered = 0
 
         val screenBrightness = SensorManager.BasicSensor(
             "screen_brightness",
@@ -60,26 +51,13 @@ class DisplaySensorManager : SensorManager, SensorEventListener {
             docsLink = "https://companion.home-assistant.io/docs/core/sensors#screen-rotation-sensor",
             unitOfMeasurement = "°"
         )
-
-        val isFaceDownOrUp = SensorManager.BasicSensor(
-            "is_face_down_or_up",
-            "sensor",
-            commonR.string.sensor_name_is_face_down_or_up,
-            commonR.string.sensor_description_is_face_down_or_up,
-            "mdi:hand-pointing-down",
-            docsLink = "https://companion.home-assistant.io/docs/core/sensors#is-face-down-or-up-sensor"
-        )
     }
+
     override val name: Int
         get() = commonR.string.sensor_name_display_sensors
 
     override suspend fun getAvailableSensors(context: Context): List<SensorManager.BasicSensor> {
-        hasAccelerometer = context.packageManager.hasSystemFeature(FEATURE_SENSOR_ACCELEROMETER)
-        return if (hasAccelerometer) {
-            listOf(screenBrightness, screenOffTimeout, screenOrientation, screenRotation, isFaceDownOrUp)
-        } else {
-            listOf(screenBrightness, screenOffTimeout, screenOrientation, screenRotation)
-        }
+        return listOf(screenBrightness, screenOffTimeout, screenOrientation, screenRotation)
     }
 
     override fun requiredPermissions(sensorId: String): Array<String> {
@@ -90,20 +68,13 @@ class DisplaySensorManager : SensorManager, SensorEventListener {
         return "https://companion.home-assistant.io/docs/core/sensors#display-sensors"
     }
 
-    private lateinit var latestContext: Context
-    private lateinit var mySensorManager: android.hardware.SensorManager
-
     override fun requestSensorUpdate(
         context: Context
     ) {
-        latestContext = context
         updateScreenBrightness(context)
         updateScreenTimeout(context)
         updateScreenOrientation(context)
         updateScreenRotation(context)
-        if (hasAccelerometer) {
-            updateIsFaceDownOrUp(context)
-        }
     }
 
     private fun updateScreenBrightness(context: Context) {
@@ -215,67 +186,6 @@ class DisplaySensorManager : SensorManager, SensorEventListener {
             screenRotation.statelessIcon,
             attrs
         )
-    }
-
-    private fun updateIsFaceDownOrUp(context: Context) {
-        if (!isEnabled(context, isFaceDownOrUp)) {
-            return
-        }
-        val now = System.currentTimeMillis()
-        if (listenerLastRegistered + SensorManager.SENSOR_LISTENER_TIMEOUT < now && isListenerRegistered) {
-            Log.d(TAG, "Re-registering listener as it appears to be stuck")
-            mySensorManager.unregisterListener(this)
-            isListenerRegistered = false
-        }
-        mySensorManager = latestContext.getSystemService()!!
-
-        val accelerometerSensor = mySensorManager.getDefaultSensor(Sensor.TYPE_ACCELEROMETER)
-        if (accelerometerSensor != null && !isListenerRegistered) {
-            mySensorManager.registerListener(
-                this,
-                accelerometerSensor,
-                SENSOR_DELAY_NORMAL
-            )
-            Log.d(TAG, "Accelerometer sensor listener registered")
-            isListenerRegistered = true
-            listenerLastRegistered = now.toInt()
-        }
-    }
-
-    override fun onSensorChanged(event: SensorEvent?) {
-        if (event?.sensor?.type == Sensor.TYPE_ACCELEROMETER) {
-            // Following the example from: https://developer.android.com/reference/android/hardware/SensorEvent#values
-            // When the device is lying flat with the screen down the value is inverted
-            val state = when {
-                event.values[2] < -9 -> "down"
-                event.values[2] > 9 -> "up"
-                else -> STATE_UNKNOWN
-            }
-            val icon = when (state) {
-                "down" -> "mdi:hand-pointing-down"
-                "up" -> "mdi:hand-pointing-up"
-                else -> "mdi:crosshairs-question"
-            }
-            onSensorUpdated(
-                latestContext,
-                isFaceDownOrUp,
-                state,
-                icon,
-                mapOf(
-                    "x" to event.values[0],
-                    "y" to event.values[1],
-                    "z" to event.values[2]
-                )
-            )
-        }
-
-        mySensorManager.unregisterListener(this)
-        Log.d(TAG, "Accelerometer sensor listener unregistered")
-        isListenerRegistered = false
-    }
-
-    override fun onAccuracyChanged(p0: Sensor?, p1: Int) {
-        // No op
     }
 
     private fun getRotationString(rotate: Int): String = when (rotate) {
