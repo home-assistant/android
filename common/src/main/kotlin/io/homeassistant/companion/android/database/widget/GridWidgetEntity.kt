72731f61567a91f9637de72fc07b01e9a44b6f82
mrdanielps <mrdanielps@users.noreply.github.com>
2024-11-02 21:47:58 +0100

    Initial grid widget implementation
    Adds a new widget that allows to configure multiple actions.
    
    Fixes #1193 #4549

---
+++ common/src/main/kotlin/io/homeassistant/companion/android/database/widget/GridWidgetEntity.kt
@@ -0,0 +1,26 @@
+package io.homeassistant.companion.android.database.widget
+
+import androidx.room.ColumnInfo
+import androidx.room.Embedded
+import androidx.room.Entity
+import androidx.room.PrimaryKey
+import androidx.room.Relation
+
+@Entity(tableName = "grid_widgets")
+data class GridWidgetEntity(
+    @PrimaryKey
+    override val id: Int,
+    @ColumnInfo(name = "server_id", defaultValue = "0")
+    override val serverId: Int,
+    @ColumnInfo(name = "label")
+    val label: String?,
+) : WidgetEntity
+
+data class GridWidgetWithItemsEntity(
+    @Embedded val gridWidget: GridWidgetEntity,
+    @Relation(
+        parentColumn = "id",
+        entityColumn = "grid_id"
+    )
+    val items: List<GridWidgetItemEntity>
+)
