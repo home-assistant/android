ec4f78693fc43d4815a522b256dbdba32b18936c
Michiel Cornelissen <michiel.cornelissen@proton.me>
2025-01-03 10:15:07 +0100

    Add database functionality for the thermostat tile.

---
+++ common/src/main/java/io/homeassistant/companion/android/database/wear/ThermostatTileDao.kt
@@ -0,0 +1,23 @@
+package io.homeassistant.companion.android.database.wear
+
+import androidx.room.Dao
+import androidx.room.Insert
+import androidx.room.OnConflictStrategy
+import androidx.room.Query
+import kotlinx.coroutines.flow.Flow
+
+@Dao
+interface ThermostatTileDao {
+
+    @Query("SELECT * FROM thermostat_tiles WHERE id = :id")
+    suspend fun get(id: Int): ThermostatTile
+
+    @Query("SELECT * FROM thermostat_tiles ORDER BY id ASC")
+    fun getAllFlow(): Flow<List<ThermostatTile>>
+
+    @Insert(onConflict = OnConflictStrategy.REPLACE)
+    suspend fun add(tile: ThermostatTile)
+
+    @Query("DELETE FROM thermostat_tiles where id = :id")
+    fun delete(id: Int)
+}
