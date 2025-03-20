3b2aa25f86a7e1ad6cfa10135e3a6d8240b9de29
Timothy <6560631+TimoPtr@users.noreply.github.com>
2025-03-20 10:33:09 +0100

    Add robolectric configuration

--- build-logic/convention/src/main/kotlin/AndroidCommonConventionPlugin.kt
+++ build-logic/convention/src/main/kotlin/AndroidCommonConventionPlugin.kt
@@ -53,7 +53,10 @@ class AndroidCommonConventionPlugin : Plugin<Project> {
                 }
 
                 testOptions {
-                    unitTests.isReturnDefaultValues = true
+                    unitTests {
+                        isReturnDefaultValues = true
+                        isIncludeAndroidResources = true
+                    }
                 }
 
                 tasks.withType<Test> {
