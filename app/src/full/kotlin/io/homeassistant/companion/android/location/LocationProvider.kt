375714143944f2509d683fb00e148eb67781c66d
Timothy <6560631+TimoPtr@users.noreply.github.com>
2025-03-25 15:43:20 +0100

    Implement GeocodeSensor with AOSP Location API for minimal flavor

---
+++ app/src/full/kotlin/io/homeassistant/companion/android/location/LocationProvider.kt
@@ -0,0 +1,19 @@
+package io.homeassistant.companion.android.location
+
+import android.Manifest
+import android.content.Context
+import android.location.Location
+import androidx.annotation.RequiresPermission
+import com.google.android.gms.location.LocationServices
+import kotlinx.coroutines.tasks.await
+import timber.log.Timber
+
+@RequiresPermission(allOf = [Manifest.permission.ACCESS_FINE_LOCATION, Manifest.permission.ACCESS_COARSE_LOCATION])
+suspend fun getLocation(context: Context): Location? {
+    return try {
+        LocationServices.getFusedLocationProviderClient(context).lastLocation.await()
+    } catch (e: Exception) {
+        Timber.e(e, "Failed to get fused location provider client")
+        null
+    }
+}
