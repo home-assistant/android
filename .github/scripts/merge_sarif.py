00a57733fe38892e31d44b62957930ead37138cf
Timothy <6560631+TimoPtr@users.noreply.github.com>
2025-03-26 12:07:34 +0100

    Cleanup sarif for better reporting

--- .github/scripts/merge_sarif.py
+++ .github/scripts/merge_sarif.py
@@ -12,6 +12,18 @@ print(f"Looking for SARIF files in: {sarif_directory}")
 merged_sarif = None
 merged_results = []
 
+# Function to clean the 'uri' field inside 'artifactLocation' blocks from CI paths so that Github
+# can interpret them correctly and create the annotation.
+def clean_artifact_location_uris(results):
+    for result in results:
+        locations = result.get("locations", [])
+        for location in locations:
+            physical_location = location.get("physicalLocation", {})
+            artifact_location = physical_location.get("artifactLocation", {})
+            if "uri" in artifact_location and isinstance(artifact_location["uri"], str):
+                # Remove 'work/android/android/' from the URI
+                artifact_location["uri"] = artifact_location["uri"].replace("work/android/android/", "")
+
 # Iterate over all SARIF files in the directory
 for sarif_file in glob.glob(os.path.join(sarif_directory, "**/*.sarif"), recursive=True):
     print(f"Processing SARIF file: {sarif_file}")
@@ -23,10 +35,13 @@ for sarif_file in glob.glob(os.path.join(sarif_directory, "**/*.sarif"), recursi
         # Merge the `results` array
         merged_results.extend(sarif_data.get("runs", [])[0].get("results", []))
 
+# Clean up 'uri' fields in the 'artifactLocation' blocks
+clean_artifact_location_uris(merged_results)
+
 # Update the merged SARIF structure with the combined results
 if merged_sarif:
     merged_sarif["runs"][0]["results"] = merged_results
-
+    
     # Write the merged SARIF to a new file
     with open(output_file, "w") as file:
         json.dump(merged_sarif, file, indent=4)
