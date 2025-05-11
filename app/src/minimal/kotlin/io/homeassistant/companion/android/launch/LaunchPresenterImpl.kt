f91721a76bd10bffe93468fce2da2ff7d00e9bf3
lone-faerie <lone.faerie@gmail.com>
2025-05-10 22:18:52 -0700

    Add stub FirebaseCloudMessagingProvider for minimal flavor

--- app/src/minimal/kotlin/io/homeassistant/companion/android/launch/LaunchPresenterImpl.kt
+++ app/src/minimal/kotlin/io/homeassistant/companion/android/launch/LaunchPresenterImpl.kt
@@ -3,14 +3,16 @@ package io.homeassistant.companion.android.launch
 import io.homeassistant.companion.android.BuildConfig
 import io.homeassistant.companion.android.common.data.integration.DeviceRegistration
 import io.homeassistant.companion.android.common.data.servers.ServerManager
+import io.homeassistant.companion.android.notifications.PushManager
 import javax.inject.Inject
 import kotlinx.coroutines.launch
 import timber.log.Timber
 
 class LaunchPresenterImpl @Inject constructor(
     view: LaunchView,
-    serverManager: ServerManager
-) : LaunchPresenterBase(view, serverManager) {
+    serverManager: ServerManager,
+    pushManager: PushManager
+) : LaunchPresenterBase(view, serverManager, pushManager) {
     override fun resyncRegistration() {
         if (!serverManager.isRegistered()) return
         serverManager.defaultServers.forEach {
