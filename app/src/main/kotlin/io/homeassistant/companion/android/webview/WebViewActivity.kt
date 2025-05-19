51ebca2f57a85f344a3347797c8d5fbf389e263f
Timothy <6560631+TimoPtr@users.noreply.github.com>
2025-05-19 17:42:00 +0200

    Add new navigate external message

--- app/src/main/kotlin/io/homeassistant/companion/android/webview/WebViewActivity.kt
+++ app/src/main/kotlin/io/homeassistant/companion/android/webview/WebViewActivity.kt
@@ -104,6 +104,7 @@ import io.homeassistant.companion.android.util.isStarted
 import io.homeassistant.companion.android.websocket.WebsocketManager
 import io.homeassistant.companion.android.webview.WebView.ErrorType
 import io.homeassistant.companion.android.webview.externalbus.ExternalBusMessage
+import io.homeassistant.companion.android.webview.externalbus.NavigateTo
 import java.util.concurrent.Executors
 import javax.inject.Inject
 import javax.inject.Named
@@ -1662,14 +1663,19 @@ class WebViewActivity : BaseActivity(), io.homeassistant.companion.android.webvi
         if (presenter.isAlwaysShowFirstViewOnAppStartEnabled() &&
             LifecycleHandler.isAppInBackground()
         ) {
-            // Pattern matches urls which are NOT allowed to show the first view after app is started
-            // This is
-            // /config/* as these are the settings of HA but NOT /config/dashboard. This is just the overview of the HA settings
-            // /hassio/* as these are the addons section of HA settings.
-            if (webView.url?.matches(".*://.*/(config/(?!\\bdashboard\\b)|hassio)/*.*".toRegex()) == false) {
-                Timber.d("Show first view of default dashboard.")
-                webView.evaluateJavascript(
-                    """
+            if (serverManager.getServer(presenter.getActiveServer())?.version?.isAtLeast(2025, 6, 0) == true) {
+                sendExternalBusMessage(
+                    NavigateTo("/config", true)
+                )
+            } else {
+                // Pattern matches urls which are NOT allowed to show the first view after app is started
+                // This is
+                // /config/* as these are the settings of HA but NOT /config/dashboard. This is just the overview of the HA settings
+                // /hassio/* as these are the addons section of HA settings.
+                if (webView.url?.matches(".*://.*/(config/(?!\\bdashboard\\b)|hassio)/*.*".toRegex()) == false) {
+                    Timber.d("Show first view of default dashboard.")
+                    webView.evaluateJavascript(
+                        """
                     var anchor = 'a:nth-child(1)';
                     var defaultPanel = window.localStorage.getItem('defaultPanel')?.replaceAll('"',"");
                     if(defaultPanel) anchor = 'a[href="/' + defaultPanel + '"]';
@@ -1678,10 +1684,11 @@ class WebViewActivity : BaseActivity(), io.homeassistant.companion.android.webvi
                                                                    .shadowRoot.querySelector('paper-listbox > ' + anchor).click();
                     window.scrollTo(0, 0);
                     """,
-                    null
-                )
-            } else {
-                Timber.d("User is in the Home Assistant config. Will not show first view of the default dashboard.")
+                        null
+                    )
+                } else {
+                    Timber.d("User is in the Home Assistant config. Will not show first view of the default dashboard.")
+                }
             }
         }
     }
