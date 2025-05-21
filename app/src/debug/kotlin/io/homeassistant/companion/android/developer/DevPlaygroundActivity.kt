7e549c149f280f8bcfcb502fc21b8bbaa433e92a
Timothy <6560631+TimoPtr@users.noreply.github.com>
2025-05-21 15:57:06 +0200

    Improve DevPlayground for testing SettingsActivity

--- app/src/debug/kotlin/io/homeassistant/companion/android/developer/DevPlaygroundActivity.kt
+++ app/src/debug/kotlin/io/homeassistant/companion/android/developer/DevPlaygroundActivity.kt
@@ -3,10 +3,14 @@ package io.homeassistant.companion.android.developer
 import android.content.Intent
 import android.os.Bundle
 import androidx.activity.compose.setContent
+import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.WindowInsets
+import androidx.compose.foundation.layout.asPaddingValues
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.systemBars
 import androidx.compose.material.Button
 import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
@@ -14,6 +18,8 @@ import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
+import androidx.glance.LocalContext
+import io.homeassistant.companion.android.settings.SettingsActivity
 import io.homeassistant.companion.android.util.compose.HomeAssistantAppTheme
 
 /**
@@ -28,12 +34,11 @@ class DevPlaygroundActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
 
         setContent {
             HomeAssistantAppTheme {
-                DevPlayGroundScreen(onDemoExoPlayerClick = {
-                    startActivity(Intent(this, DemoExoPlayerActivity::class.java))
-                })
+                DevPlayGroundScreen()
             }
         }
     }
@@ -42,9 +47,11 @@ class DevPlaygroundActivity : AppCompatActivity() {
 private class DummyException : Throwable()
 
 @Composable
-private fun DevPlayGroundScreen(onDemoExoPlayerClick: () -> Unit = {}) {
+private fun DevPlayGroundScreen() {
+    val context = LocalContext.current
     Column(
         modifier = Modifier
+            .padding(WindowInsets.systemBars.asPaddingValues())
             .fillMaxWidth(),
         horizontalAlignment = Alignment.CenterHorizontally
     ) {
@@ -54,9 +61,14 @@ private fun DevPlayGroundScreen(onDemoExoPlayerClick: () -> Unit = {}) {
             Text("Crash the app")
         }
         Button(modifier = Modifier.padding(top = 16.dp), onClick = {
-            onDemoExoPlayerClick()
+            context.startActivity(Intent(context, DemoExoPlayerActivity::class.java))
         }) {
             Text("Demo ExoPlayer")
+        }
+        Button(modifier = Modifier.padding(top = 16.dp), onClick = {
+            context.startActivity(SettingsActivity.newInstance(context))
+        }) {
+            Text("Start Settings")
         }
     }
 }
