37c83442ddab400e3ecb0663714320672593e929
Grzegorz Libiszewski <grzegorz.libiszewski7@gmail.com>
2025-05-30 12:40:00 +0200

    feat: edge to edge using views bars

--- common/src/main/kotlin/io/homeassistant/companion/android/common/util/InsetsUtil.kt
+++ common/src/main/kotlin/io/homeassistant/companion/android/common/util/InsetsUtil.kt
@@ -1,6 +1,5 @@
 package io.homeassistant.companion.android.common.util
 
-import android.os.Build
 import android.view.View
 import android.webkit.WebView
 import androidx.core.util.TypedValueCompat.pxToDp
@@ -9,23 +8,26 @@ import androidx.core.view.WindowInsetsCompat
 import androidx.core.view.WindowInsetsCompat.Type.displayCutout
 import androidx.core.view.WindowInsetsCompat.Type.ime
 import androidx.core.view.WindowInsetsCompat.Type.systemBars
-import androidx.core.view.updatePadding
+import androidx.core.view.updateLayoutParams
 
-fun WebView.applyInsets(rootContainer: View, serverHandleInsets: Boolean) {
+fun WebView.applyInsets(
+    rootContainer: View,
+    serverHandleInsets: Boolean,
+    statusBarBackground: View? = null,
+    navigationBarBackground: View? = null,
+) {
     ViewCompat.setOnApplyWindowInsetsListener(rootContainer) { v, windowInsets ->
         val safeInsets = windowInsets.getInsets(
             systemBars() or displayCutout() or ime()
         )
 
-        // If the server doesn't handle the insets we have to manually apply them when we are on API 36
-        // since Edge to Edge is enable by default otherwise the webview is drawn bellow
-        if (!serverHandleInsets && Build.VERSION.SDK_INT >= Build.VERSION_CODES.BAKLAVA) {
-            v.updatePadding(
-                top = safeInsets.top,
-                bottom = safeInsets.bottom,
-                left = safeInsets.left,
-                right = safeInsets.right,
-            )
+        if (!serverHandleInsets) {
+            statusBarBackground?.updateLayoutParams {
+                height = safeInsets.top
+            }
+            navigationBarBackground?.updateLayoutParams {
+                height = safeInsets.bottom
+            }
         } else {
             // From https://medium.com/androiddevelopers/make-webviews-edge-to-edge-a6ef319adfac
             // Convert raw pixels to density independent pixels
