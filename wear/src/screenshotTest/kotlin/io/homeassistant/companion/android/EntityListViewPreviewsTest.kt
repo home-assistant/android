4a90318e0866f4a7631d2ea6fc59bce366f01faa
Joris Pelgröm <joris.pelgrom@gmail.com>
2025-06-08 12:35:18 +0200

    Add @PreviewTest and move preview image

--- wear/src/screenshotTest/kotlin/io/homeassistant/companion/android/EntityListViewPreviewsTest.kt
+++ wear/src/screenshotTest/kotlin/io/homeassistant/companion/android/EntityListViewPreviewsTest.kt
@@ -4,6 +4,7 @@ import androidx.compose.runtime.Composable
 import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.wear.tooling.preview.devices.WearDevices
+import com.android.tools.screenshot.PreviewTest
 import io.homeassistant.companion.android.common.R
 import io.homeassistant.companion.android.home.views.EntityViewList
 import io.homeassistant.companion.android.util.previewEntity1
@@ -11,6 +12,7 @@ import io.homeassistant.companion.android.util.previewEntity2
 
 class EntityListViewPreviewsTest {
 
+    @PreviewTest
     @Preview(device = WearDevices.LARGE_ROUND)
     @Composable
     private fun PreviewEntityListView() {
