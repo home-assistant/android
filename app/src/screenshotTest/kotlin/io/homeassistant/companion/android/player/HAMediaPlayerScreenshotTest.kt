1f8fd4489dceed7cb4252ab783e1e6722b7c052d
Timothy <6560631+TimoPtr@users.noreply.github.com>
2025-06-12 15:44:30 +0200

    Add more screenshot tests

--- app/src/screenshotTest/kotlin/io/homeassistant/companion/android/player/HAMediaPlayerScreenshotTest.kt
+++ app/src/screenshotTest/kotlin/io/homeassistant/companion/android/player/HAMediaPlayerScreenshotTest.kt
@@ -1,8 +1,13 @@
 package io.homeassistant.companion.android.player
 
+import androidx.compose.foundation.layout.size
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
 import androidx.compose.ui.layout.ContentScale
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import io.homeassistant.companion.android.util.compose.media.player.FakePlayer
+import io.homeassistant.companion.android.util.compose.media.player.HAMediaPlayerUI
 import kotlin.time.Duration.Companion.hours
 
 class HAMediaPlayerScreenshotTest {
@@ -29,5 +34,16 @@ class HAMediaPlayerScreenshotTest {
     @Composable
     fun `Media Player is playing`() {
         HAMediaPlayerUI(FakePlayer(muted = true, playing = true), false, ContentScale.Inside)
+    }
+
+    @Preview
+    @Composable
+    fun `Media Player is too small to display bottom controls`() {
+        HAMediaPlayerUI(
+            FakePlayer(muted = true, playing = true),
+            false,
+            ContentScale.Inside,
+            modifier = Modifier.size(100.dp),
+        )
     }
 }
