09fc91cae2a47a5157f0f5c94aca0f53677c256f
Timothy <6560631+TimoPtr@users.noreply.github.com>
2025-07-17 08:27:23 +0200

    Enable edge-to-edge support for BaseActivity (#5387)

--- app/src/main/kotlin/io/homeassistant/companion/android/assist/AssistActivity.kt
+++ app/src/main/kotlin/io/homeassistant/companion/android/assist/AssistActivity.kt
@@ -5,13 +5,10 @@ import android.app.KeyguardManager
 import android.content.Context
 import android.content.Intent
 import android.content.pm.PackageManager
-import android.graphics.Color
 import android.os.Build
 import android.os.Bundle
 import android.view.WindowManager
-import androidx.activity.SystemBarStyle
 import androidx.activity.compose.setContent
-import androidx.activity.enableEdgeToEdge
 import androidx.activity.result.contract.ActivityResultContracts
 import androidx.activity.viewModels
 import androidx.core.content.ContextCompat
@@ -56,13 +53,9 @@ class AssistActivity : BaseActivity() {
 
     private val requestPermission = registerForActivityResult(
         ActivityResultContracts.RequestPermission(),
-        { viewModel.onPermissionResult(it) },
-    )
+    ) { viewModel.onPermissionResult(it) }
 
     override fun onCreate(savedInstanceState: Bundle?) {
-        enableEdgeToEdge(
-            navigationBarStyle = SystemBarStyle.auto(Color.TRANSPARENT, Color.TRANSPARENT),
-        )
         super.onCreate(savedInstanceState)
         updateShowWhenLocked()
 
