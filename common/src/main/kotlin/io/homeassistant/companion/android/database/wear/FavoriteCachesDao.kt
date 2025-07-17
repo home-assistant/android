a6852f147380546cef823936740095449a6283c6
Timothy <6560631+TimoPtr@users.noreply.github.com>
2025-07-17 11:24:07 +0200

    Make FavoriteCachesDao methods suspensible (#5551)

--- common/src/main/kotlin/io/homeassistant/companion/android/database/wear/FavoriteCachesDao.kt
+++ common/src/main/kotlin/io/homeassistant/companion/android/database/wear/FavoriteCachesDao.kt
@@ -9,17 +9,17 @@ import androidx.room.Query
 interface FavoriteCachesDao {
 
     @Query("SELECT * FROM favorite_cache where id = :id")
-    fun get(id: String): FavoriteCaches?
+    suspend fun get(id: String): FavoriteCaches?
 
     @Query("SELECT * FROM favorite_cache ORDER BY id ASC")
-    fun getAll(): List<FavoriteCaches>
+    suspend fun getAll(): List<FavoriteCaches>
 
     @Insert(onConflict = OnConflictStrategy.REPLACE)
-    fun add(cache: FavoriteCaches)
+    suspend fun add(cache: FavoriteCaches)
 
     @Query("DELETE FROM favorite_cache where id = :id")
-    fun delete(id: String)
+    suspend fun delete(id: String)
 
     @Query("DELETE FROM favorite_cache")
-    fun deleteAll()
+    suspend fun deleteAll()
 }
