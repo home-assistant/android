6a39b81128ab203e9e09c603291fe923ce0da783
Timothy <6560631+TimoPtr@users.noreply.github.com>
2025-07-30 18:14:55 +0200

    Support registration in LauncherVM

--- onboarding/src/main/kotlin/io/homeassistant/companion/android/LauncherViewModel.kt
+++ onboarding/src/main/kotlin/io/homeassistant/companion/android/LauncherViewModel.kt
@@ -5,11 +5,13 @@ import androidx.lifecycle.viewModelScope
 import androidx.room.concurrent.AtomicBoolean
 import dagger.hilt.android.lifecycle.HiltViewModel
 import io.homeassistant.companion.android.common.data.authentication.SessionState
+import io.homeassistant.companion.android.common.data.integration.DeviceRegistration
 import io.homeassistant.companion.android.common.data.servers.ServerManager
 import javax.inject.Inject
 import kotlinx.coroutines.flow.MutableSharedFlow
 import kotlinx.coroutines.flow.asSharedFlow
 import kotlinx.coroutines.launch
+import timber.log.Timber
 
 sealed interface LauncherNavigationEvent {
     data object Frontend : LauncherNavigationEvent
@@ -49,16 +51,35 @@ class LauncherViewModel @Inject constructor(private val serverManager: ServerMan
                     serverManager.authenticationRepository()
                         .getSessionState() == SessionState.CONNECTED
                 ) {
-                    // resyncRegistration()
+                    resyncRegistration()
                     _navigationEventsFlow.emit(LauncherNavigationEvent.Frontend)
                 } else {
                     _navigationEventsFlow.emit(LauncherNavigationEvent.Onboarding)
                 }
             } catch (e: IllegalArgumentException) { // Server was just removed, nothing is added
-                // TODO what can throw here this exception resyncRegistration probably
+                // TODO what can throw here this exception
                 _navigationEventsFlow.emit(LauncherNavigationEvent.Onboarding)
             }
             shouldShowSplashScreen.set(false)
+        }
+    }
+
+    private suspend fun resyncRegistration() {
+        serverManager.defaultServers.forEach {
+            try {
+                serverManager.integrationRepository(it.id).updateRegistration(
+                    DeviceRegistration(
+                        // TODO when in :app use ${BuildConfig.VERSION_NAME} (${BuildConfig.VERSION_CODE})
+                        "ONBOARDING_WIP",
+                        null,
+                        null, // TODO when in :app use getMessagingToken()
+                    ),
+                )
+                serverManager.integrationRepository(it.id).getConfig() // Update cached data
+                serverManager.webSocketRepository(it.id).getCurrentUser() // Update cached data
+            } catch (e: Exception) {
+                Timber.e(e, "Issue updating Registration")
+            }
         }
     }
 }
