7e275d5f7e3324b12785f10f0cb01bd45715f54a
Timothy <6560631+TimoPtr@users.noreply.github.com>
2025-08-06 13:40:42 +0200

    Introduce copyWithWidgetId method in WidgetEntity

--- common/src/main/kotlin/io/homeassistant/companion/android/database/widget/WidgetEntity.kt
+++ common/src/main/kotlin/io/homeassistant/companion/android/database/widget/WidgetEntity.kt
@@ -1,6 +1,10 @@
 package io.homeassistant.companion.android.database.widget
 
-interface WidgetEntity {
+import java.io.Serializable
+
+sealed interface WidgetEntity<T : WidgetEntity<T>> : Serializable {
     val id: Int
     val serverId: Int
+
+    fun copyWithWidgetId(appWidgetId: Int): T
 }
