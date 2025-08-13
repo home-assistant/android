9c25baa4580621e2d7aea1939cd78fd3866f0345
Timothy <6560631+TimoPtr@users.noreply.github.com>
2025-08-13 21:30:24 +0200

    Bump threshold for app screenshots

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -34,7 +34,7 @@ android {
     experimentalProperties["android.experimental.enableScreenshotTest"] = true
 
     screenshotTests {
-        imageDifferenceThreshold = 0.00040f // 0.040%
+        imageDifferenceThreshold = 0.00080f // 0.080%
     }
 
     firebaseAppDistribution {
