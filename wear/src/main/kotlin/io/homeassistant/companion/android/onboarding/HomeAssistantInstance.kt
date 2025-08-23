bae94ba3afd36cdcbbb26c92d2956c252de89dfc
yawaaaa <github1369@gmail.com>
2025-08-23 23:29:49 +0800

    Delete wear directory

--- wear/src/main/kotlin/io/homeassistant/companion/android/onboarding/HomeAssistantInstance.kt
+++
@@ -1,5 +0,0 @@
-package io.homeassistant.companion.android.onboarding
-
-import java.net.URL
-
-data class HomeAssistantInstance(val name: String, val url: URL, val version: String)
--- wear/src/main/kotlin/io/homeassistant/companion/android/onboarding/integration/MobileAppIntegrationPresenter.kt
+++
@@ -1,6 +0,0 @@
-package io.homeassistant.companion.android.onboarding.integration
-
-interface MobileAppIntegrationPresenter {
-    fun onRegistrationAttempt(serverId: Int, deviceName: String)
-    fun onFinish()
-}
