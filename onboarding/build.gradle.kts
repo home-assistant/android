0460f5214f548a26ec0dfe7c6efd5833929d0030
Timothy <6560631+TimoPtr@users.noreply.github.com>
2025-08-26 11:40:12 +0200

    FIX instrumentation test setup

--- onboarding/build.gradle.kts
+++ onboarding/build.gradle.kts
@@ -2,10 +2,15 @@ plugins {
     alias(libs.plugins.android.library)
     alias(libs.plugins.homeassistant.android.common)
     alias(libs.plugins.homeassistant.android.compose)
+    alias(libs.plugins.hilt)
 }
 
 android {
     namespace = "io.homeassistant.companion.android.onboarding"
+
+    defaultConfig {
+        testInstrumentationRunner = "io.homeassistant.companion.android.CustomTestRunner"
+    }
 }
 
 dependencies {
@@ -33,4 +38,7 @@ dependencies {
     testImplementation(libs.hilt.android.testing)
 
     androidTestImplementation(libs.leakcanary.android.instrumentation)
+    androidTestImplementation(libs.bundles.androidx.test)
+    androidTestImplementation(libs.hilt.android.testing)
+    kspAndroidTest(libs.hilt.android.compiler)
 }
--- onboarding/src/androidTest/kotlin/io/homeassistant/companion/android/LauncherTest.kt
+++ onboarding/src/androidTest/kotlin/io/homeassistant/companion/android/LauncherTest.kt
@@ -1,15 +1,22 @@
 package io.homeassistant.companion.android
 
 import androidx.compose.ui.test.junit4.createAndroidComposeRule
+import dagger.hilt.android.testing.HiltAndroidRule
+import dagger.hilt.android.testing.HiltAndroidTest
 import leakcanary.DetectLeaksAfterTestSuccess
 import org.junit.Rule
 import org.junit.Test
 
+@HiltAndroidTest
 class LauncherTest {
-    @get:Rule
+
+    @get:Rule(order = 0)
+    var hiltRule = HiltAndroidRule(this)
+
+    @get:Rule(order = 1)
     val composeTestRule = createAndroidComposeRule<Launcher>()
 
-    @get:Rule
+    @get:Rule(order = 2)
     val ruleChain = DetectLeaksAfterTestSuccess()
 
     @Test
