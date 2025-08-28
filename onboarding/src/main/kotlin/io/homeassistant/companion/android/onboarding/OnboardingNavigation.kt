50bcb9038ad116f92d204a8c9f90545f96c69044
Timothy <6560631+TimoPtr@users.noreply.github.com>
2025-08-28 09:42:03 +0200

    Introduce HAApp and navigation

---
+++ onboarding/src/main/kotlin/io/homeassistant/companion/android/onboarding/OnboardingNavigation.kt
@@ -0,0 +1,31 @@
+package io.homeassistant.companion.android.onboarding
+
+import androidx.navigation.NavController
+import androidx.navigation.NavGraphBuilder
+import androidx.navigation.navigation
+import io.homeassistant.companion.android.HAStartDestinationRoute
+import io.homeassistant.companion.android.onboarding.welcome.navigation.WelcomeRoute
+import io.homeassistant.companion.android.onboarding.welcome.navigation.navigateToLearnMore
+import io.homeassistant.companion.android.onboarding.welcome.navigation.welcomeScreen
+import kotlinx.serialization.Serializable
+
+@Serializable
+internal data object OnboardingRoute : HAStartDestinationRoute
+
+/**
+ * Adds the onboarding graph to the [NavGraphBuilder].It is dedicated for onboarding it can be accessed either by using the
+ * route [OnboardingRoute], the start destination of this graph is [WelcomeRoute].
+ *
+ * TODO update docs when the rest of the graph is ready.
+ */
+internal fun NavGraphBuilder.onboarding(
+    navController: NavController,
+    onShowSnackbar: suspend (message: String, action: String?) -> Boolean,
+) {
+    navigation<OnboardingRoute>(startDestination = WelcomeRoute) {
+        welcomeScreen(
+            onConnectClick = {},
+            onLearnMoreClick = navController::navigateToLearnMore,
+        )
+    }
+}
