dad94d07a6dd18c08c7aedcd2bae56099c87ff71
Timothy <6560631+TimoPtr@users.noreply.github.com>
2025-08-28 09:44:08 +0200

    Add unit tests tools

---
+++ testing-unit/src/main/kotlin/io/homeassistant/companion/android/testing/unit/MainDispatcherJUnit.kt
@@ -0,0 +1,38 @@
+package io.homeassistant.companion.android.testing.unit
+
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.ExperimentalCoroutinesApi
+import kotlinx.coroutines.test.StandardTestDispatcher
+import kotlinx.coroutines.test.TestDispatcher
+import kotlinx.coroutines.test.resetMain
+import kotlinx.coroutines.test.setMain
+import org.junit.jupiter.api.extension.AfterEachCallback
+import org.junit.jupiter.api.extension.BeforeEachCallback
+import org.junit.jupiter.api.extension.ExtensionContext
+import org.junit.rules.TestWatcher
+import org.junit.runner.Description
+
+@OptIn(ExperimentalCoroutinesApi::class)
+class MainDispatcherJUnit5Extension(val testDispatcher: TestDispatcher = StandardTestDispatcher()) :
+    AfterEachCallback,
+    BeforeEachCallback {
+
+    override fun beforeEach(context: ExtensionContext?) {
+        Dispatchers.setMain(testDispatcher)
+    }
+
+    override fun afterEach(context: ExtensionContext?) {
+        Dispatchers.resetMain()
+    }
+}
+
+@OptIn(ExperimentalCoroutinesApi::class)
+class MainDispatcherJUnit4Rule(val testDispatcher: TestDispatcher = StandardTestDispatcher()) : TestWatcher() {
+    override fun starting(description: Description?) {
+        Dispatchers.setMain(testDispatcher)
+    }
+
+    override fun finished(description: Description?) {
+        Dispatchers.resetMain()
+    }
+}
