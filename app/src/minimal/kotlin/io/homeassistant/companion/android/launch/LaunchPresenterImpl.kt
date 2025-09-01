f44b133d7355f6c0d2a4b0b4e4e89f65da10f6e4
Timothy <6560631+TimoPtr@users.noreply.github.com>
2025-09-01 14:11:54 +0200

    Revert "Fire and forget resync to avoid blocking UI at startup"
    This reverts commit 2b60ae3735fc97dcbced6e0d269d41c78d793e36.

--- app/src/minimal/kotlin/io/homeassistant/companion/android/launch/LaunchPresenterImpl.kt
+++ app/src/minimal/kotlin/io/homeassistant/companion/android/launch/LaunchPresenterImpl.kt
@@ -5,9 +5,7 @@ import io.homeassistant.companion.android.common.data.integration.DeviceRegistra
 import io.homeassistant.companion.android.common.data.network.NetworkStatusMonitor
 import io.homeassistant.companion.android.common.data.servers.ServerManager
 import javax.inject.Inject
-import kotlinx.coroutines.CoroutineScope
-import kotlinx.coroutines.Dispatchers
-import kotlinx.coroutines.SupervisorJob
+import kotlinx.coroutines.coroutineScope
 import kotlinx.coroutines.launch
 import timber.log.Timber
 
@@ -16,32 +14,11 @@ class LaunchPresenterImpl @Inject constructor(
     serverManager: ServerManager,
     networkStatusMonitor: NetworkStatusMonitor,
 ) : LaunchPresenterBase(view, serverManager, networkStatusMonitor) {
-
-    /**
-     * A dedicated [CoroutineScope] to perform background tasks that should not block the UI.
-     * This is crucial for operations like server resynchronization, especially when dealing with
-     * multiple servers where one might be unresponsive.
-     *
-     * By launching tasks in this scope, we ensure that the UI remains responsive and is displayed
-     * promptly, even if a server connection times out.
-     *
-     * **Note:** Using a dedicated scope like this means that the underlying Activity might not be
-     * destroyed immediately if these background tasks are still running. This is a trade-off
-     * for improved UI responsiveness in scenarios with potential network delays. If we
-     * introduce a new server state like disable we could maybe bring this back in the flow.
-     *
-     * We use a [SupervisorJob] on purpose because it ensures that if one task fails, it won't
-     * impact the others.
-     *
-     * See https://github.com/home-assistant/android/issues/5689#issuecomment-3241605990
-     */
-    private val ioScope: CoroutineScope = CoroutineScope(Dispatchers.IO + SupervisorJob())
-
-    override suspend fun resyncRegistration() {
-        if (!serverManager.isRegistered()) return
+    override suspend fun resyncRegistration() = coroutineScope {
+        if (!serverManager.isRegistered()) return@coroutineScope
 
         serverManager.defaultServers.forEach {
-            ioScope.launch {
+            launch {
                 try {
                     serverManager.integrationRepository(it.id).updateRegistration(
                         DeviceRegistration(
