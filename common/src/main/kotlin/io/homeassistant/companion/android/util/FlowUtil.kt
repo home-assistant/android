84196fb6eb83a0e773560467d04db8fe9b52c8fc
Timothy <6560631+TimoPtr@users.noreply.github.com>
2025-09-02 17:58:11 +0200

    Introduce Flow.delayFirst

--- common/src/main/kotlin/io/homeassistant/companion/android/util/FlowUtil.kt
+++ common/src/main/kotlin/io/homeassistant/companion/android/util/FlowUtil.kt
@@ -1,8 +1,16 @@
 package io.homeassistant.companion.android.util
 
+import androidx.annotation.VisibleForTesting
+import kotlin.time.Clock
+import kotlin.time.Duration
+import kotlin.time.ExperimentalTime
+import kotlin.time.Instant
 import kotlinx.coroutines.delay
 import kotlinx.coroutines.flow.Flow
+import kotlinx.coroutines.flow.buffer
 import kotlinx.coroutines.flow.conflate
+import kotlinx.coroutines.flow.onEach
+import kotlinx.coroutines.flow.onStart
 import kotlinx.coroutines.flow.transform
 
 /**
@@ -18,3 +26,36 @@ fun <T> Flow<T>.throttleLatest(delayMillis: Long): Flow<T> = this
         emit(it)
         delay(delayMillis)
     }
+
+@OptIn(ExperimentalTime::class)
+@VisibleForTesting
+internal fun <T> Flow<T>.delayFirst(delayDuration: Duration, clock: Clock = Clock.System): Flow<T> {
+    lateinit var endDelay: Instant
+
+    return buffer().onStart {
+        endDelay = clock.now() + delayDuration
+    }.onEach {
+        val currentTime = clock.now()
+        if (currentTime < endDelay) {
+            delay(endDelay - currentTime)
+        }
+    }
+}
+
+/**
+ * Delays the emission of the first item from the upstream flow by the specified [delayDuration].
+ * Subsequent items are emitted without delay. This is useful when you want to delay the
+ * initial processing of a flow without delaying the start of the upstream flow itself.
+ * It also adds a buffer to the flow to ensure that the upstream flow can continue emitting items
+ * while the first item is being delayed.
+ *
+ * Unlike using `onStart { delay(duration) }`, which postpones the start of the entire flow,
+ * this operator allows the upstream flow to start immediately, and only delays the downstream
+ *
+ *
+ * @param delayDuration The duration to delay the first item.
+ */
+@OptIn(ExperimentalTime::class)
+fun <T> Flow<T>.delayFirst(delayDuration: Duration): Flow<T> {
+    return delayFirst(delayDuration, Clock.System)
+}
---
+++ common/src/test/kotlin/io/homeassistant/companion/android/util/FlowUtilTest.kt
@@ -0,0 +1,93 @@
+package io.homeassistant.companion.android.util
+
+import app.cash.turbine.test
+import io.homeassistant.companion.android.testing.unit.FakeClock
+import kotlin.time.Duration.Companion.milliseconds
+import kotlin.time.Duration.Companion.seconds
+import kotlin.time.ExperimentalTime
+import kotlinx.coroutines.ExperimentalCoroutinesApi
+import kotlinx.coroutines.channels.Channel
+import kotlinx.coroutines.flow.consumeAsFlow
+import kotlinx.coroutines.flow.flowOf
+import kotlinx.coroutines.flow.onEach
+import kotlinx.coroutines.test.advanceTimeBy
+import kotlinx.coroutines.test.runCurrent
+import kotlinx.coroutines.test.runTest
+import org.junit.jupiter.api.Assertions.assertEquals
+import org.junit.jupiter.api.Test
+
+@OptIn(ExperimentalCoroutinesApi::class)
+class FlowUtilTest {
+    @Test
+    fun `Given event sent before delay when consuming then should return all after delay without delay`() = runTest {
+        flowOf(1, 2, 3)
+            .delayFirst(1.seconds)
+            .test {
+                expectNoEvents()
+                advanceTimeBy(500.milliseconds)
+                expectNoEvents()
+                advanceTimeBy(500.milliseconds)
+                assertEquals(1, awaitItem())
+                assertEquals(2, awaitItem())
+                assertEquals(3, awaitItem())
+                awaitComplete()
+            }
+    }
+
+    @OptIn(ExperimentalTime::class)
+    @Test
+    fun `Given event sent before delay and after delay when consuming then should return at the write time`() = runTest {
+        val channel = Channel<Int>(Channel.UNLIMITED)
+        val fakeClock = FakeClock()
+
+        channel.consumeAsFlow()
+            .delayFirst(1.seconds, fakeClock).test {
+                expectNoEvents()
+                channel.trySend(1)
+                expectNoEvents()
+                advanceTimeBy(1.seconds)
+                runCurrent()
+                assertEquals(1, expectMostRecentItem())
+
+                // We need to modify the clock manually since it is not advancing with advanceTimeBy
+                fakeClock.currentInstant = fakeClock.currentInstant.plus(1.seconds)
+                channel.trySend(2)
+                assertEquals(2, expectMostRecentItem())
+
+                channel.close()
+                awaitComplete()
+            }
+    }
+
+    @Test
+    fun `Given events before delay when consuming then should not block upstream`() = runTest {
+        val channel = Channel<Int>(Channel.UNLIMITED)
+        var currentValueInUpstream: Int? = null
+
+        channel.consumeAsFlow()
+            .onEach { currentValueInUpstream = it }
+            .delayFirst(1.seconds).test {
+                expectNoEvents()
+                channel.trySend(1)
+                assertEquals(1, currentValueInUpstream)
+                channel.trySend(2)
+                assertEquals(2, currentValueInUpstream)
+                expectNoEvents()
+                advanceTimeBy(1.seconds)
+                runCurrent()
+                assertEquals(1, expectMostRecentItem())
+                assertEquals(2, awaitItem())
+                channel.close()
+                awaitComplete()
+            }
+    }
+
+    @Test
+    fun `Given no event when consuming then should complete`() = runTest {
+        flowOf<Int>()
+            .delayFirst(1.seconds)
+            .test {
+                awaitComplete()
+            }
+    }
+}
