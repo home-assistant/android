b142ffc1d87c313674e252ad724c5031663a6c27
Timothy <6560631+TimoPtr@users.noreply.github.com>
2025-09-03 11:28:38 +0200

    Introduce HAThemeForPreview

--- common/src/main/kotlin/io/homeassistant/companion/android/common/compose/theme/HATheme.kt
+++ common/src/main/kotlin/io/homeassistant/companion/android/common/compose/theme/HATheme.kt
@@ -1,9 +1,14 @@
 package io.homeassistant.companion.android.common.compose.theme
 
+import android.annotation.SuppressLint
+import androidx.compose.foundation.background
 import androidx.compose.foundation.isSystemInDarkTheme
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.BoxScope
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.CompositionLocalProvider
+import androidx.compose.ui.Modifier
 
 /**
  * A custom theme built on top of [androidx.compose.material3.MaterialTheme] with Home Assistant colors.
@@ -27,5 +32,17 @@ fun HATheme(darkTheme: Boolean = isSystemInDarkTheme(), content: @Composable () 
                 surfaceContainerLow = LocalHAColorScheme.current.colorSurfaceDefault,
             ),
         )
+    }
+}
+
+/**
+ * Small Wrapper around [HATheme] to handle the background color for screenshots/preview, to test individual screen we
+ * need to set the background color manually.
+ */
+@SuppressLint("ComposeModifierMissing")
+@Composable
+fun HAThemeForPreview(content: @Composable BoxScope.() -> Unit) {
+    HATheme {
+        Box(modifier = Modifier.background(LocalHAColorScheme.current.colorSurfaceDefault), content = content)
     }
 }
