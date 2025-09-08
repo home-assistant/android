7c0614ad9886a4eb75fb5d2e6e9fc5d25b2bfee2
Timothy <6560631+TimoPtr@users.noreply.github.com>
2025-09-08 10:50:58 +0200

    Improve MapAnySerializer to use compiled or builtin serializer

--- common/src/test/kotlin/io/homeassistant/companion/android/common/data/integration/impl/entities/RegisterDeviceRequestTest.kt
+++ common/src/test/kotlin/io/homeassistant/companion/android/common/data/integration/impl/entities/RegisterDeviceRequestTest.kt
@@ -1,6 +1,7 @@
 package io.homeassistant.companion.android.common.data.integration.impl.entities
 
 import io.homeassistant.companion.android.common.util.AppVersion
+import io.homeassistant.companion.android.common.util.MessagingToken
 import io.homeassistant.companion.android.common.util.kotlinJsonMapper
 import io.homeassistant.companion.android.testing.unit.ConsoleLogTree
 import org.junit.jupiter.api.Assertions.assertEquals
@@ -19,7 +20,7 @@ class RegisterDeviceRequestTest {
     @Test
     fun `Given a valid RegisterDeviceRequest when serializing it then it creates a valid JSON`() {
         assertEquals(
-            """{"app_id":"1","app_name":"2","app_version":"3 (3)","device_name":"4","manufacturer":"5","model":"6","os_name":"7","os_version":"8","supports_encryption":true,"app_data":{"1":"2","2":1},"device_id":"3"}""",
+            """{"app_id":"1","app_name":"2","app_version":"3 (3)","device_name":"4","manufacturer":"5","model":"6","os_name":"7","os_version":"8","supports_encryption":true,"app_data":{"1":"2","2":1,"3":"token"},"device_id":"3"}""",
             kotlinJsonMapper.encodeToString(
                 RegisterDeviceRequest(
                     appId = "1",
@@ -31,7 +32,7 @@ class RegisterDeviceRequestTest {
                     osName = "7",
                     osVersion = "8",
                     supportsEncryption = true,
-                    appData = mapOf("1" to "2", "2" to 1),
+                    appData = mapOf("1" to "2", "2" to 1, "3" to MessagingToken("token")),
                     deviceId = "3",
                 ),
             ),
