5d81d2761f69cb5675b0eef8aa2370d987d0211b
Timothy <6560631+TimoPtr@users.noreply.github.com>
2025-09-09 16:11:38 +0200

    Add NameYourDeviceScreen

--- onboarding/src/main/kotlin/io/homeassistant/companion/android/onboarding/OnboardingNavigation.kt
+++ onboarding/src/main/kotlin/io/homeassistant/companion/android/onboarding/OnboardingNavigation.kt
@@ -3,13 +3,17 @@ package io.homeassistant.companion.android.onboarding
 import androidx.navigation.NavController
 import androidx.navigation.NavGraphBuilder
 import androidx.navigation.NavOptions
+import androidx.navigation.navOptions
 import androidx.navigation.navigation
 import io.homeassistant.companion.android.HAStartDestinationRoute
 import io.homeassistant.companion.android.compose.navigateToUri
+import io.homeassistant.companion.android.onboarding.connection.navigation.ConnectionRoute
 import io.homeassistant.companion.android.onboarding.connection.navigation.connectionScreen
 import io.homeassistant.companion.android.onboarding.connection.navigation.navigateToConnection
 import io.homeassistant.companion.android.onboarding.manualserver.navigation.manualServerScreen
 import io.homeassistant.companion.android.onboarding.manualserver.navigation.navigateToManualServer
+import io.homeassistant.companion.android.onboarding.nameyourdevice.navigation.nameYourDeviceScreen
+import io.homeassistant.companion.android.onboarding.nameyourdevice.navigation.navigateToNameYourDevice
 import io.homeassistant.companion.android.onboarding.serverdiscovery.navigation.navigateToServerDiscovery
 import io.homeassistant.companion.android.onboarding.serverdiscovery.navigation.serverDiscoveryScreen
 import io.homeassistant.companion.android.onboarding.welcome.navigation.WelcomeRoute
@@ -64,12 +68,32 @@ internal fun NavGraphBuilder.onboarding(
         )
         connectionScreen(
             onAuthenticated = { url, authCode ->
-                // TODO goes to the name your device screen
+                navController.navigateToNameYourDevice(
+                    url = url,
+                    authCode = authCode,
+                    navOptions {
+                        // We don't want to come back to the connection screen if we navigate to the name your device screen
+                        popUpTo<ConnectionRoute> {
+                            inclusive = true
+                        }
+                    },
+                )
             },
             onShowSnackbar = onShowSnackbar,
             onBackClick = navController::popBackStack,
             onOpenExternalLink = {
                 navController.navigateToUri(it.toString())
+            },
+        )
+        nameYourDeviceScreen(
+            onBackClick = navController::popBackStack,
+            onDeviceNamed = { serverId ->
+                // TODO if external URL or cloud URL available go to Location otherwise go to local first
+            },
+            onShowSnackbar = onShowSnackbar,
+            onHelpClick = {
+                // TODO validate the URL to use
+                navController.navigateToUri("https://www.home-assistant.io/installation/")
             },
         )
     }
