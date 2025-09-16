109c62635784b8b1bf6c65d9d419631350803c7a
Timothy <6560631+TimoPtr@users.noreply.github.com>
2025-09-16 12:18:01 +0200

    Add location screen to navigation and introduce onboardingDone callback

--- onboarding/src/main/kotlin/io/homeassistant/companion/android/onboarding/localfirst/navigation/LocalFirstNavigation.kt
+++ onboarding/src/main/kotlin/io/homeassistant/companion/android/onboarding/localfirst/navigation/LocalFirstNavigation.kt
@@ -4,20 +4,23 @@ import androidx.navigation.NavController
 import androidx.navigation.NavGraphBuilder
 import androidx.navigation.NavOptions
 import androidx.navigation.compose.composable
+import androidx.navigation.toRoute
 import io.homeassistant.companion.android.onboarding.localfirst.LocalFirstScreen
 import kotlinx.serialization.Serializable
 
 @Serializable
-internal data object LocalFirstRoute
+internal data class LocalFirstRoute(val serverId: Int)
 
-internal fun NavController.navigateToLocalFirst(navOptions: NavOptions? = null) {
-    navigate(LocalFirstRoute, navOptions = navOptions)
+internal fun NavController.navigateToLocalFirst(serverId: Int, navOptions: NavOptions? = null) {
+    navigate(LocalFirstRoute(serverId), navOptions = navOptions)
 }
 
-internal fun NavGraphBuilder.localFirstScreen(onNextClick: () -> Unit) {
+internal fun NavGraphBuilder.localFirstScreen(onNextClick: (serverId: Int) -> Unit) {
     composable<LocalFirstRoute> {
         LocalFirstScreen(
-            onNextClick = onNextClick,
+            onNextClick = {
+                onNextClick(it.toRoute<LocalFirstRoute>().serverId)
+            },
         )
     }
 }
