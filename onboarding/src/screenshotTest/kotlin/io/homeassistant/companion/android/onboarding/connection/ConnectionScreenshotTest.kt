adb4005300e8c96d4c3090488537be7b0ff3b651
Timothy <6560631+TimoPtr@users.noreply.github.com>
2025-09-23 09:26:03 +0200

    Add onboarding connection screen (#5759)
    * Copy and adjust HAWebView
    
    * Add connection screen to onboarding
    
    * Open external links into external browser

---
+++ onboarding/src/screenshotTest/kotlin/io/homeassistant/companion/android/onboarding/connection/ConnectionScreenshotTest.kt
@@ -0,0 +1,38 @@
+package io.homeassistant.companion.android.onboarding.connection
+
+import android.webkit.WebViewClient
+import androidx.compose.runtime.Composable
+import com.android.tools.screenshot.PreviewTest
+import io.homeassistant.companion.android.common.compose.theme.HAThemeForPreview
+import io.homeassistant.companion.android.compose.HAPreviews
+
+class ConnectionScreenshotTest {
+
+    @PreviewTest
+    @HAPreviews
+    @Composable
+    fun `ConnectionScreen loading`() {
+        HAThemeForPreview {
+            ConnectionScreen(
+                url = "https://www.example.com",
+                isLoading = true,
+                webViewClient = WebViewClient(),
+                onBackClick = {},
+            )
+        }
+    }
+
+    @PreviewTest
+    @HAPreviews
+    @Composable
+    fun `ConnectionScreen loaded`() {
+        HAThemeForPreview {
+            ConnectionScreen(
+                url = "https://www.example.com",
+                isLoading = false,
+                webViewClient = WebViewClient(),
+                onBackClick = {},
+            )
+        }
+    }
+}
---
+++ onboarding/src/test/kotlin/io/homeassistant/companion/android/onboarding/connection/ConnectionScreenTest.kt
@@ -0,0 +1,90 @@
+package io.homeassistant.companion.android.onboarding.connection
+
+import android.webkit.WebViewClient
+import androidx.compose.ui.test.assertIsDisplayed
+import androidx.compose.ui.test.assertIsNotDisplayed
+import androidx.compose.ui.test.junit4.createAndroidComposeRule
+import androidx.compose.ui.test.onNodeWithContentDescription
+import dagger.hilt.android.testing.HiltAndroidRule
+import dagger.hilt.android.testing.HiltAndroidTest
+import dagger.hilt.android.testing.HiltTestApplication
+import io.homeassistant.companion.android.HiltComponentActivity
+import io.homeassistant.companion.android.onboarding.R
+import io.homeassistant.companion.android.testing.unit.ConsoleLogTree
+import io.homeassistant.companion.android.testing.unit.stringResource
+import org.junit.Before
+import org.junit.Rule
+import org.junit.Test
+import org.junit.jupiter.api.Assertions.assertTrue
+import org.junit.runner.RunWith
+import org.robolectric.RobolectricTestRunner
+import org.robolectric.annotation.Config
+import timber.log.Timber
+
+@RunWith(RobolectricTestRunner::class)
+@Config(application = HiltTestApplication::class)
+@HiltAndroidTest
+class ConnectionScreenTest {
+    @get:Rule(order = 0)
+    val hiltRule = HiltAndroidRule(this)
+
+    @get:Rule(order = 1)
+    val composeTestRule = createAndroidComposeRule<HiltComponentActivity>()
+
+    @Before
+    fun setup() {
+        Timber.plant(ConsoleLogTree)
+        ConsoleLogTree.verbose = true
+    }
+
+    @Test
+    fun `Given ConnectionScreen when isLoading then show loading`() {
+        composeTestRule.apply {
+            setContent {
+                ConnectionScreen(
+                    onBackClick = {},
+                    isLoading = true,
+                    url = "",
+                    webViewClient = WebViewClient(),
+                )
+            }
+            onNodeWithContentDescription(stringResource(R.string.loading_content_description)).assertIsDisplayed()
+        }
+    }
+
+    @Test
+    fun `Given ConnectionScreen when not isLoading then don't show loading`() {
+        composeTestRule.apply {
+            setContent {
+                ConnectionScreen(
+                    onBackClick = {},
+                    isLoading = false,
+                    url = "",
+                    webViewClient = WebViewClient(),
+                )
+            }
+            onNodeWithContentDescription(stringResource(R.string.loading_content_description)).assertIsNotDisplayed()
+        }
+    }
+
+    @Test
+    fun `Given ConnectionScreen when pressing back then triggers onBackClick`() {
+        var backPressed = false
+        composeTestRule.apply {
+            setContent {
+                ConnectionScreen(
+                    onBackClick = {
+                        backPressed = true
+                    },
+                    isLoading = false,
+                    url = "",
+                    webViewClient = WebViewClient(),
+                )
+            }
+
+            activity.onBackPressedDispatcher.onBackPressed()
+
+            assertTrue(backPressed)
+        }
+    }
+}
