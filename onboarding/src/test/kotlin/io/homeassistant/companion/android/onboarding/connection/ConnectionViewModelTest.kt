9e6c5d2917776c1ee63ad9eed7a44c1ecdad57f4
Timothy <6560631+TimoPtr@users.noreply.github.com>
2025-10-16 15:06:54 +0200

    Adjust ConnectionViewModel error handling

--- onboarding/src/test/kotlin/io/homeassistant/companion/android/onboarding/connection/ConnectionViewModelTest.kt
+++ onboarding/src/test/kotlin/io/homeassistant/companion/android/onboarding/connection/ConnectionViewModelTest.kt
@@ -12,6 +12,7 @@ import android.webkit.WebViewClient.ERROR_HOST_LOOKUP
 import android.webkit.WebViewClient.ERROR_PROXY_AUTHENTICATION
 import android.webkit.WebViewClient.ERROR_UNSUPPORTED_AUTH_SCHEME
 import androidx.annotation.StringRes
+import app.cash.turbine.ReceiveTurbine
 import app.cash.turbine.turbineScope
 import io.homeassistant.companion.android.common.R as commonR
 import io.homeassistant.companion.android.common.data.authentication.impl.AuthenticationService
@@ -19,11 +20,11 @@ import io.homeassistant.companion.android.common.data.keychain.KeyChainRepositor
 import io.homeassistant.companion.android.onboarding.R
 import io.homeassistant.companion.android.testing.unit.ConsoleLogTree
 import io.homeassistant.companion.android.testing.unit.MainDispatcherJUnit5Extension
-import io.homeassistant.companion.android.util.TLSWebViewClient
 import io.mockk.every
 import io.mockk.mockk
 import io.mockk.mockkStatic
 import java.net.URL
+import kotlin.reflect.KClass
 import kotlinx.coroutines.ExperimentalCoroutinesApi
 import kotlinx.coroutines.test.advanceUntilIdle
 import kotlinx.coroutines.test.runTest
@@ -60,9 +61,10 @@ class ConnectionViewModelTest {
             val urlFlow = viewModel.urlFlow.testIn(backgroundScope)
             val isLoadingFlow = viewModel.isLoadingFlow.testIn(backgroundScope)
             val navigationEventsFlow = viewModel.navigationEventsFlow.testIn(backgroundScope)
+            val errorFlow = viewModel.errorFlow.testIn(backgroundScope)
 
             // Initial state
-            assertFalse(viewModel.isErrorFlow.value)
+            assertNull(errorFlow.awaitItem())
             assertTrue(isLoadingFlow.awaitItem())
             assertEquals(null, urlFlow.awaitItem())
 
@@ -74,7 +76,7 @@ class ConnectionViewModelTest {
             viewModel.webViewClient.onPageFinished(mockk(), null)
 
             assertFalse(isLoadingFlow.awaitItem())
-            assertFalse(viewModel.isErrorFlow.value)
+            errorFlow.expectNoEvents()
 
             navigationEventsFlow.expectNoEvents() // No authenticated or error events expected
         }
@@ -99,21 +101,24 @@ class ConnectionViewModelTest {
     }
 
     @Test
-    fun `Given a malformed url when buildAuthUrl then navigationEventsFlow emits malformed url error`() = runTest {
+    fun `Given a malformed url when buildAuthUrl then errorFlow emits malformed url error`() = runTest {
         val malformedUrl = "not_a_url"
         val viewModel = ConnectionViewModel(malformedUrl, keyChainRepository)
 
         turbineScope {
             val navigationEventsFlow = viewModel.navigationEventsFlow.testIn(backgroundScope)
             val urlFlow = viewModel.urlFlow.testIn(backgroundScope)
+            val errorFlow = viewModel.errorFlow.testIn(backgroundScope)
+
+            assertNull(errorFlow.awaitItem())
 
             advanceUntilIdle()
             assertNull(urlFlow.awaitItem())
 
-            assertError(navigationEventsFlow.awaitItem(), R.string.connection_screen_malformed_url)
-            assertTrue(viewModel.isErrorFlow.value)
+            errorFlow.awaitConnectionError<ConnectionError.UnreachableError>(R.string.connection_screen_malformed_url, "Expected URL scheme 'http' or 'https' but no scheme was found for not_a_...", IllegalArgumentException::class)
 
             urlFlow.expectNoEvents()
+            navigationEventsFlow.expectNoEvents()
         }
     }
 
@@ -131,6 +136,9 @@ class ConnectionViewModelTest {
 
         turbineScope {
             val navigationEventsFlow = viewModel.navigationEventsFlow.testIn(backgroundScope)
+            val errorFlow = viewModel.errorFlow.testIn(backgroundScope)
+
+            assertNull(errorFlow.awaitItem())
 
             viewModel.webViewClient.isTLSClientAuthNeeded = requireMTLS
 
@@ -147,7 +155,7 @@ class ConnectionViewModelTest {
             assertEquals(authCode, (event as ConnectionNavigationEvent.Authenticated).authCode)
             assertEquals("http://homeassistant.local:8123", event.url)
             assertEquals(requireMTLS, event.requiredMTLS)
-            assertFalse(viewModel.isErrorFlow.value)
+            errorFlow.expectNoEvents()
         }
     }
 
@@ -165,6 +173,9 @@ class ConnectionViewModelTest {
 
         turbineScope {
             val navigationEventsFlow = viewModel.navigationEventsFlow.testIn(backgroundScope)
+            val errorFlow = viewModel.errorFlow.testIn(backgroundScope)
+
+            assertNull(errorFlow.awaitItem())
 
             val result = viewModel.webViewClient.shouldOverrideUrlLoading(
                 null,
@@ -175,7 +186,7 @@ class ConnectionViewModelTest {
 
             assertFalse(result)
             navigationEventsFlow.expectNoEvents()
-            assertFalse(viewModel.isErrorFlow.value)
+            errorFlow.expectNoEvents()
         }
     }
 
@@ -193,6 +204,9 @@ class ConnectionViewModelTest {
 
         turbineScope {
             val navigationEventsFlow = viewModel.navigationEventsFlow.testIn(backgroundScope)
+            val errorFlow = viewModel.errorFlow.testIn(backgroundScope)
+
+            assertNull(errorFlow.awaitItem())
 
             val result = viewModel.webViewClient.shouldOverrideUrlLoading(
                 null,
@@ -207,29 +221,33 @@ class ConnectionViewModelTest {
             assertEquals(callbackUri, (event as ConnectionNavigationEvent.OpenExternalLink).url)
 
             navigationEventsFlow.expectNoEvents()
-            assertFalse(viewModel.isErrorFlow.value)
+            errorFlow.expectNoEvents()
         }
     }
 
     @Test
-    fun `Given SSL errors when onReceivedSslError is invoked then Error event with message`() = runTest {
+    fun `Given SSL errors when onReceivedSslError is invoked then errorFlow emits AuthenticationError with message`() = runTest {
         val viewModel = ConnectionViewModel("http://homeassistant.local:8123", keyChainRepository)
 
         turbineScope {
             val navigationEventsFlow = viewModel.navigationEventsFlow.testIn(backgroundScope)
+            val errorFlow = viewModel.errorFlow.testIn(backgroundScope)
+
+            assertNull(errorFlow.awaitItem())
 
             suspend fun testError(primaryError: Int?, @StringRes messageRes: Int) {
+                val details = "SSL Error: $primaryError".takeIf { primaryError != null }.toString()
                 viewModel.webViewClient.onReceivedSslError(
                     null,
                     null,
                     primaryError?.let {
                         mockk<SslError> {
                             every { this@mockk.primaryError } returns primaryError
+                            every { this@mockk.toString() } returns details
                         }
                     },
                 )
-                assertError(navigationEventsFlow.awaitItem(), messageRes)
-                assertTrue(viewModel.isErrorFlow.value)
+                errorFlow.awaitConnectionError<ConnectionError.AuthenticationError>(messageRes, details, SslError::class)
             }
 
             testError(SslError.SSL_DATE_INVALID, commonR.string.webview_error_SSL_DATE_INVALID)
@@ -239,17 +257,22 @@ class ConnectionViewModelTest {
             testError(SslError.SSL_NOTYETVALID, commonR.string.webview_error_SSL_NOTYETVALID)
             testError(SslError.SSL_UNTRUSTED, commonR.string.webview_error_SSL_UNTRUSTED)
             testError(null, commonR.string.error_ssl)
+
+            navigationEventsFlow.expectNoEvents()
         }
     }
 
     @Test
-    fun `Given HTTP errors when onReceivedHttpError is invoked then Error event with message`() = runTest {
+    fun `Given HTTP errors when onReceivedHttpError is invoked then errorFlow emits appropriate error`() = runTest {
         val rawUrl = "http://homeassistant.local:8123"
         val viewModel = ConnectionViewModel(rawUrl, keyChainRepository)
 
         turbineScope {
             val navigationEventsFlow = viewModel.navigationEventsFlow.testIn(backgroundScope)
+            val errorFlow = viewModel.errorFlow.testIn(backgroundScope)
             val urlFlow = viewModel.urlFlow.testIn(backgroundScope)
+
+            assertNull(errorFlow.awaitItem())
 
             assertEquals(null, urlFlow.awaitItem())
             assertNotNull(urlFlow.awaitItem())
@@ -260,14 +283,13 @@ class ConnectionViewModelTest {
                 }
             }
 
-            val webViewClient = viewModel.webViewClient as TLSWebViewClient
+            val webViewClient = viewModel.webViewClient
 
             // Expired cert
             webViewClient.isTLSClientAuthNeeded = true
             webViewClient.isCertificateChainValid = false
             webViewClient.onReceivedHttpError(null, request, null)
-            assertError(navigationEventsFlow.awaitItem(), commonR.string.tls_cert_expired_message)
-            assertTrue(viewModel.isErrorFlow.value)
+            errorFlow.awaitConnectionError<ConnectionError.AuthenticationError>(commonR.string.tls_cert_expired_message, errorDetails(null, null), WebResourceResponse::class)
 
             // Cert not found
             webViewClient.isTLSClientAuthNeeded = true
@@ -280,8 +302,7 @@ class ConnectionViewModelTest {
                     every { reasonPhrase } returns "reason"
                 },
             )
-            assertError(navigationEventsFlow.awaitItem(), commonR.string.tls_cert_not_found_message)
-            assertTrue(viewModel.isErrorFlow.value)
+            errorFlow.awaitConnectionError<ConnectionError.AuthenticationError>(commonR.string.tls_cert_not_found_message, errorDetails(400, "reason"), WebResourceResponse::class)
 
             // Generic error
             webViewClient.isTLSClientAuthNeeded = false
@@ -294,8 +315,7 @@ class ConnectionViewModelTest {
                     every { reasonPhrase } returns "I'm a teapot"
                 },
             )
-            assertError(navigationEventsFlow.awaitItem(), commonR.string.error_http_generic, 418, "I'm a teapot")
-            assertTrue(viewModel.isErrorFlow.value)
+            errorFlow.awaitConnectionError<ConnectionError.UnknownError>(commonR.string.error_http_generic, errorDetails(418, "I'm a teapot"), WebResourceResponse::class)
 
             // Generic error without reason
             webViewClient.isTLSClientAuthNeeded = false
@@ -312,20 +332,22 @@ class ConnectionViewModelTest {
                     every { reasonPhrase } returns ""
                 },
             )
-            assertError(navigationEventsFlow.awaitItem(), commonR.string.error_http_generic, 418, "No description")
-            assertTrue(viewModel.isErrorFlow.value)
+            errorFlow.awaitConnectionError<ConnectionError.UnknownError>(commonR.string.error_http_generic, errorDetails(418, "No description"), WebResourceResponse::class)
+            navigationEventsFlow.expectNoEvents()
         }
     }
 
     @Test
-    fun `Given received error when onReceivedError is invoked then Error event with message`() = runTest {
+    fun `Given received error when onReceivedError is invoked then errorFlow emits appropriate error`() = runTest {
         val rawUrl = "http://homeassistant.local:8123"
         val viewModel = ConnectionViewModel(rawUrl, keyChainRepository)
 
         turbineScope {
             val navigationEventsFlow = viewModel.navigationEventsFlow.testIn(backgroundScope)
             val urlFlow = viewModel.urlFlow.testIn(backgroundScope)
+            val errorFlow = viewModel.errorFlow.testIn(backgroundScope)
 
+            assertNull(errorFlow.awaitItem())
             assertEquals(null, urlFlow.awaitItem())
             assertNotNull(urlFlow.awaitItem())
 
@@ -335,7 +357,8 @@ class ConnectionViewModelTest {
                 }
             }
 
-            suspend fun testError(errorCode: Int, @StringRes messageRes: Int, description: String? = null) {
+            suspend fun testAuthError(errorCode: Int, @StringRes messageRes: Int) {
+                val description = "Error description"
                 viewModel.webViewClient.onReceivedError(
                     null,
                     request,
@@ -344,20 +367,38 @@ class ConnectionViewModelTest {
                         every { this@mockk.description } returns description
                     },
                 )
-                if (description != null) {
-                    assertError(navigationEventsFlow.awaitItem(), messageRes, errorCode, description)
-                } else {
-                    assertError(navigationEventsFlow.awaitItem(), messageRes)
-                }
-                assertTrue(viewModel.isErrorFlow.value)
+                errorFlow.awaitConnectionError<ConnectionError.AuthenticationError>(messageRes, errorDetails(errorCode, description), WebResourceError::class)
             }
 
-            testError(ERROR_FAILED_SSL_HANDSHAKE, commonR.string.webview_error_FAILED_SSL_HANDSHAKE)
-            testError(ERROR_AUTHENTICATION, commonR.string.webview_error_AUTHENTICATION)
-            testError(ERROR_PROXY_AUTHENTICATION, commonR.string.webview_error_PROXY_AUTHENTICATION)
-            testError(ERROR_UNSUPPORTED_AUTH_SCHEME, commonR.string.webview_error_AUTH_SCHEME)
-            testError(ERROR_HOST_LOOKUP, commonR.string.webview_error_HOST_LOOKUP)
-            testError(-1, commonR.string.error_http_generic, "description")
+            suspend fun testUnreachableError(errorCode: Int, @StringRes messageRes: Int) {
+                val description = "Error description"
+                viewModel.webViewClient.onReceivedError(
+                    null,
+                    request,
+                    mockk<WebResourceError> {
+                        every { this@mockk.errorCode } returns errorCode
+                        every { this@mockk.description } returns description
+                    },
+                )
+                errorFlow.awaitConnectionError<ConnectionError.UnreachableError>(messageRes, errorDetails(errorCode, description), WebResourceError::class)
+            }
+
+            testAuthError(ERROR_FAILED_SSL_HANDSHAKE, commonR.string.webview_error_FAILED_SSL_HANDSHAKE)
+            testAuthError(ERROR_AUTHENTICATION, commonR.string.webview_error_AUTHENTICATION)
+            testAuthError(ERROR_PROXY_AUTHENTICATION, commonR.string.webview_error_PROXY_AUTHENTICATION)
+            testAuthError(ERROR_UNSUPPORTED_AUTH_SCHEME, commonR.string.webview_error_AUTH_SCHEME)
+            testUnreachableError(ERROR_HOST_LOOKUP, commonR.string.webview_error_HOST_LOOKUP)
+
+            // Generic error with description
+            viewModel.webViewClient.onReceivedError(
+                null,
+                request,
+                mockk<WebResourceError> {
+                    every { this@mockk.errorCode } returns -1
+                    every { this@mockk.description } returns "description"
+                },
+            )
+            errorFlow.awaitConnectionError<ConnectionError.UnknownError>(commonR.string.error_http_generic, errorDetails(-1, "description"), WebResourceError::class)
 
             // Generic error without description
             viewModel.webViewClient.onReceivedError(
@@ -372,19 +413,22 @@ class ConnectionViewModelTest {
                     every { this@mockk.description } returns ""
                 },
             )
-            assertError(navigationEventsFlow.awaitItem(), commonR.string.error_http_generic, -1, "No description")
-            assertTrue(viewModel.isErrorFlow.value)
+            errorFlow.awaitConnectionError<ConnectionError.UnknownError>(commonR.string.error_http_generic, errorDetails(-1, "No description"), WebResourceError::class)
+            navigationEventsFlow.expectNoEvents()
         }
     }
 
-    private fun assertError(event: ConnectionNavigationEvent, resId: Int, vararg formatArgs: Any?) {
-        assertTrue(event is ConnectionNavigationEvent.Error)
-        assertEquals(resId, (event as ConnectionNavigationEvent.Error).resId)
-        if (formatArgs.isEmpty()) {
-            assertTrue(event.formatArgs.isEmpty())
-        } else {
-            assertEquals(formatArgs.toList(), event.formatArgs.toList())
-        }
+    private fun errorDetails(code: Int?, description: String?): String {
+        return "Status Code: ${code}\nDescription: $description"
+    }
+
+    private suspend inline fun <reified T : ConnectionError> ReceiveTurbine<ConnectionError?>.awaitConnectionError(messageId: Int, errorDetails: String?, errorClass: KClass<*>) {
+        val error = awaitItem()
+        assertNotNull(error)
+        assertTrue(error is T)
+        assertEquals(messageId, error.message)
+        assertEquals(errorDetails, error.errorDetails)
+        assertEquals(errorClass.toString(), error.rawErrorType)
     }
 
     private fun mockUriParse() {
