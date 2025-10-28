86f3a5f26c8c50e3d96557f4c9568c5ce20b49dd
Timothy <6560631+TimoPtr@users.noreply.github.com>
2025-10-28 16:34:03 +0100

    Introduce ConnectionSecurityLevelFragment

---
+++ app/src/release/kotlin/io/homeassistant/companion/android/settings/ConnectionSecurityLevelFragment.kt
@@ -0,0 +1,14 @@
+package io.homeassistant.companion.android.settings
+
+import androidx.fragment.app.Fragment
+
+/**
+ * Placeholder that doesn't do anything but to allow the app to build in release.
+ */
+class ConnectionSecurityLevelFragment : Fragment() {
+
+    companion object {
+        const val RESULT_KEY = "connection_security_level_result"
+        const val EXTRA_SEVER_ID_KEY = "server_id"
+    }
+}
