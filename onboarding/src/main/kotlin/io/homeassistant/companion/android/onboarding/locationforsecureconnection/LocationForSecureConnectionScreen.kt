11c1c7675ddf6973b0dc7941ac0335724186f63f
Timothy <6560631+TimoPtr@users.noreply.github.com>
2025-11-13 13:22:09 +0100

    Remove WindowsInsets override in LocationForSecureConnectionScreen

--- onboarding/src/main/kotlin/io/homeassistant/companion/android/onboarding/locationforsecureconnection/LocationForSecureConnectionScreen.kt
+++ onboarding/src/main/kotlin/io/homeassistant/companion/android/onboarding/locationforsecureconnection/LocationForSecureConnectionScreen.kt
@@ -7,7 +7,6 @@ import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.ColumnScope
 import androidx.compose.foundation.layout.Spacer
-import androidx.compose.foundation.layout.WindowInsets
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
@@ -93,7 +92,6 @@ internal fun LocationForSecureConnectionScreen(
     Scaffold(
         modifier = modifier,
         topBar = { HATopBar(onBackClick = onBackClick, onHelpClick = onHelpClick) },
-        contentWindowInsets = WindowInsets(0, 0, 0, 0),
     ) { contentPadding ->
         LocationForSecureConnectionContent(
             isStandaloneScreen = isStandaloneScreen,
