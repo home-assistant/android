ae88cfdf35414a9dd8a864f70f69112841fe4c75
Timothy <6560631+TimoPtr@users.noreply.github.com>
2026-02-04 16:32:16 +0100

    Remove unwanted import

--- app/src/test/kotlin/io/homeassistant/companion/android/util/compose/HAAppTest.kt
+++ app/src/test/kotlin/io/homeassistant/companion/android/util/compose/HAAppTest.kt
@@ -31,7 +31,6 @@ import io.homeassistant.companion.android.automotive.navigation.AutomotiveRoute
 import io.homeassistant.companion.android.common.R
 import io.homeassistant.companion.android.common.data.servers.ServerManager
 import io.homeassistant.companion.android.di.ServerManagerModule
-import io.homeassistant.companion.android.frontend.navigation.FrontendActivityRoute
 import io.homeassistant.companion.android.frontend.navigation.FrontendRoute
 import io.homeassistant.companion.android.launch.HAStartDestinationRoute
 import io.homeassistant.companion.android.onboarding.OnboardingRoute
