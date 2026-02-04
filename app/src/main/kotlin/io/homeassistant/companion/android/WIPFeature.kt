8d5db06d1836f0f35b8c7067da42e2ef2fa938b9
Timothy <6560631+TimoPtr@users.noreply.github.com>
2026-02-04 15:58:06 +0100

    Introduce the USE_FRONTEND_V2 WIPFeature

--- app/src/main/kotlin/io/homeassistant/companion/android/WIPFeature.kt
+++ app/src/main/kotlin/io/homeassistant/companion/android/WIPFeature.kt
@@ -1,5 +1,3 @@
-@file:Suppress("ktlint-disable no-empty-file")
-
 package io.homeassistant.companion.android
 
 /**
@@ -13,3 +11,15 @@ package io.homeassistant.companion.android
  *
  * Feature flags should be removed from this file once the feature is fully released and stable.
  */
+object WIPFeature {
+
+    /**
+     * Enables the new frontend screen using compose navigation instead of the WebViewActivity.
+     *
+     * When true, [FrontendRoute] navigates to [FrontendScreen] (Compose).
+     * When false, [FrontendRoute] navigates to [io.homeassistant.companion.android.webview.WebViewActivity] (legacy).
+     *
+     * This flag is only enabled in DEBUG builds during development.
+     */
+    val USE_FRONTEND_V2: Boolean = BuildConfig.DEBUG
+}
