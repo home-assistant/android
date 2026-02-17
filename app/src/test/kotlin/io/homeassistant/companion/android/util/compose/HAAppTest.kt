ed07d93049ed1c4b9111481af4047f8d6a74f741
Timothy <6560631+TimoPtr@users.noreply.github.com>
2026-02-17 11:57:51 +0100

    Use ViewModelFactory for LocationForSecureConnectionViewModel (#6384)

--- app/src/test/kotlin/io/homeassistant/companion/android/util/compose/HAAppTest.kt
+++ app/src/test/kotlin/io/homeassistant/companion/android/util/compose/HAAppTest.kt
@@ -25,9 +25,12 @@ import dagger.hilt.android.testing.BindValue
 import dagger.hilt.android.testing.HiltAndroidRule
 import dagger.hilt.android.testing.HiltAndroidTest
 import dagger.hilt.android.testing.HiltTestApplication
+import dagger.hilt.android.testing.UninstallModules
 import io.homeassistant.companion.android.HiltComponentActivity
 import io.homeassistant.companion.android.automotive.navigation.AutomotiveRoute
 import io.homeassistant.companion.android.common.R
+import io.homeassistant.companion.android.common.data.servers.ServerManager
+import io.homeassistant.companion.android.di.ServerManagerModule
 import io.homeassistant.companion.android.frontend.navigation.FrontendActivityRoute
 import io.homeassistant.companion.android.frontend.navigation.FrontendRoute
 import io.homeassistant.companion.android.launch.HAStartDestinationRoute
@@ -35,7 +38,6 @@ import io.homeassistant.companion.android.onboarding.OnboardingRoute
 import io.homeassistant.companion.android.onboarding.WearOnboardApp
 import io.homeassistant.companion.android.onboarding.WearOnboardingRoute
 import io.homeassistant.companion.android.onboarding.connection.navigation.ConnectionRoute
-import io.homeassistant.companion.android.onboarding.locationforsecureconnection.LocationForSecureConnectionViewModel
 import io.homeassistant.companion.android.onboarding.locationforsecureconnection.navigation.navigateToLocationForSecureConnection
 import io.homeassistant.companion.android.onboarding.nameyourweardevice.navigation.navigateToNameYourWearDevice
 import io.homeassistant.companion.android.onboarding.serverdiscovery.navigation.ServerDiscoveryRoute
@@ -43,7 +45,7 @@ import io.homeassistant.companion.android.onboarding.welcome.navigation.WelcomeR
 import io.homeassistant.companion.android.testing.unit.ConsoleLogRule
 import io.homeassistant.companion.android.testing.unit.stringResource
 import io.homeassistant.companion.android.util.compose.webview.HA_WEBVIEW_TAG
-import io.mockk.coJustRun
+import io.mockk.coEvery
 import io.mockk.every
 import io.mockk.mockk
 import io.mockk.slot
@@ -61,6 +63,7 @@ import org.robolectric.annotation.Config
 
 @RunWith(RobolectricTestRunner::class)
 @Config(application = HiltTestApplication::class)
+@UninstallModules(ServerManagerModule::class)
 @HiltAndroidTest
 class HAAppTest {
 
@@ -73,7 +76,7 @@ class HAAppTest {
     @get:Rule(order = 2)
     val composeTestRule = createAndroidComposeRule<HiltComponentActivity>()
 
-    // Mock the ViewModel to prevent the real allowInsecureConnection() from executing.
+    // Mock the ServerManager to prevent the real allowInsecureConnection() from executing.
     // Without this, the suspend function switches to Dispatchers.IO and the coroutine resumes
     // on a non-main thread, causing navigation to fail with "Method setCurrentState must be
     // called on the main thread" because LifecycleRegistry requires main thread access.
@@ -81,8 +84,8 @@ class HAAppTest {
     // where dispatcher context is not properly preserved across suspend function boundaries.
     @BindValue
     @JvmField
-    val locationForSecureConnectionViewModel = spyk(LocationForSecureConnectionViewModel(0, mockk())).apply {
-        coJustRun { this@apply.allowInsecureConnection(any()) }
+    val serverManager: ServerManager = mockk(relaxed = true) {
+        coEvery { getServer(any<Int>()) } returns null
     }
 
     private lateinit var navController: TestNavHostController
