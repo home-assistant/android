df8bd767c2d42dc1d9ff1090db4018cd9d2e1951
Timothy <6560631+TimoPtr@users.noreply.github.com>
2026-02-26 09:17:57 +0100

    Fix toolchain to adoptium for reproducibility (#6486)

--- settings.gradle.kts
+++ settings.gradle.kts
@@ -20,7 +20,8 @@ pluginManagement {
 
 plugins {
     // So we can't reach the libs.plugins.* aliases from here so we need to declare them the old way...
-    id("org.ajoberstar.reckon.settings").version("2.0.0")
+    id("org.ajoberstar.reckon.settings") version "2.0.0"
+    id("org.gradle.toolchains.foojay-resolver-convention") version "1.0.0"
 }
 
 extensions.configure<org.ajoberstar.reckon.gradle.ReckonExtension>("reckon") {
