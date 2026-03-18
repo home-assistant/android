97c5cc76e9182218e1e77ab3bf602c98d8a73fc3
Timothy <6560631+TimoPtr@users.noreply.github.com>
2026-03-18 16:17:38 +0100

    Adjust tests

--- common/src/test/kotlin/io/homeassistant/companion/android/common/util/LaunchAsyncTest.kt
+++ common/src/test/kotlin/io/homeassistant/companion/android/common/util/LaunchAsyncTest.kt
@@ -6,15 +6,15 @@ import android.content.Intent
 import io.mockk.mockk
 import io.mockk.verify
 import kotlin.time.Duration.Companion.seconds
+import kotlinx.coroutines.CoroutineExceptionHandler
 import kotlinx.coroutines.CoroutineScope
 import kotlinx.coroutines.ExperimentalCoroutinesApi
 import kotlinx.coroutines.SupervisorJob
 import kotlinx.coroutines.TimeoutCancellationException
 import kotlinx.coroutines.delay
-import kotlinx.coroutines.test.StandardTestDispatcher
-import kotlinx.coroutines.test.TestScope
 import kotlinx.coroutines.test.advanceTimeBy
 import kotlinx.coroutines.test.advanceUntilIdle
+import kotlinx.coroutines.test.runTest
 import org.junit.jupiter.api.AfterEach
 import org.junit.jupiter.api.Assertions.assertNotNull
 import org.junit.jupiter.api.Assertions.assertNull
@@ -25,10 +25,6 @@ import org.junit.jupiter.api.fail
 
 @OptIn(ExperimentalCoroutinesApi::class)
 class LaunchAsyncTest {
-
-    private val testDispatcher = StandardTestDispatcher()
-    private val testScope = TestScope(testDispatcher)
-    private val receiverScope = CoroutineScope(SupervisorJob() + testDispatcher)
 
     private lateinit var receiver: BroadcastReceiver
     private lateinit var pendingResult: BroadcastReceiver.PendingResult
@@ -50,35 +46,39 @@ class LaunchAsyncTest {
     }
 
     @Test
-    fun `Given a short task when launchAsync then block executes and finish is called`() {
+    fun `Given a short task when launchAsync then block executes and finish is called`() = runTest {
         var executed = false
 
-        receiver.launchAsync(receiverScope, pendingResult) {
+        receiver.launchAsync(this, pendingResult) {
             executed = true
         }
 
-        testScope.advanceUntilIdle()
+        advanceUntilIdle()
 
         assertTrue(executed, "Block should have executed")
         verify { pendingResult.finish() }
     }
 
     @Test
-    fun `Given a failing block when launchAsync then finish is still called`() {
-        receiver.launchAsync(receiverScope, pendingResult) {
+    fun `Given a failing block when launchAsync then finish is still called`() = runTest {
+        var caughtException: Throwable? = null
+        val scope = CoroutineScope(coroutineContext + SupervisorJob() + CoroutineExceptionHandler { _, throwable -> caughtException = throwable })
+        receiver.launchAsync(scope, pendingResult) {
             throw IllegalStateException("Expected error")
         }
 
-        testScope.advanceUntilIdle()
+        advanceUntilIdle()
 
         verify { pendingResult.finish() }
+        assertNotNull(caughtException)
+        assertTrue(caughtException is IllegalStateException)
     }
 
     @Test
-    fun `Given a block exceeding timeout when launchAsync then block is cancelled and finish is called`() {
+    fun `Given a block exceeding timeout when launchAsync then block is cancelled and finish is called`() = runTest {
         var wasCancelled = false
 
-        receiver.launchAsync(receiverScope, pendingResult) {
+        receiver.launchAsync(this, pendingResult) {
             try {
                 delay(BROADCAST_RECEIVER_TIMEOUT + 1.seconds)
                 fail("Should not reach this part")
@@ -88,8 +88,8 @@ class LaunchAsyncTest {
             }
         }
 
-        testScope.advanceTimeBy(BROADCAST_RECEIVER_TIMEOUT + 1.seconds)
-        testScope.advanceUntilIdle()
+        advanceTimeBy(BROADCAST_RECEIVER_TIMEOUT + 1.seconds)
+        advanceUntilIdle()
 
         assertTrue(wasCancelled, "Block should have been cancelled after timeout")
         assertNotNull(failFastException, "FailFast should have been triggered")
@@ -97,16 +97,16 @@ class LaunchAsyncTest {
     }
 
     @Test
-    fun `Given a block completing just before timeout when launchAsync then block completes and finish is called`() {
+    fun `Given a block completing just before timeout when launchAsync then block completes and finish is called`() = runTest {
         var completed = false
 
-        receiver.launchAsync(receiverScope, pendingResult) {
+        receiver.launchAsync(this, pendingResult) {
             delay(BROADCAST_RECEIVER_TIMEOUT - 1.seconds)
             completed = true
         }
 
-        testScope.advanceTimeBy(BROADCAST_RECEIVER_TIMEOUT)
-        testScope.advanceUntilIdle()
+        advanceTimeBy(BROADCAST_RECEIVER_TIMEOUT)
+        advanceUntilIdle()
 
         assertTrue(completed, "Block should have completed")
         assertNull(failFastException, "FailFast should not have been triggered")
