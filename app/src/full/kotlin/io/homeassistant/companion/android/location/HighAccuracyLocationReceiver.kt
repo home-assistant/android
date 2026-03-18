3ec465162eadc0c5d253a82f796f77a11db181b6
Timothy <6560631+TimoPtr@users.noreply.github.com>
2026-03-18 16:57:07 +0100

    Fix Ktlint issue

--- app/src/full/kotlin/io/homeassistant/companion/android/location/HighAccuracyLocationReceiver.kt
+++ app/src/full/kotlin/io/homeassistant/companion/android/location/HighAccuracyLocationReceiver.kt
@@ -8,7 +8,6 @@ import io.homeassistant.companion.android.sensors.LocationSensorManager
 import kotlinx.coroutines.CoroutineScope
 import kotlinx.coroutines.Dispatchers
 import kotlinx.coroutines.Job
-import kotlinx.coroutines.launch
 
 class HighAccuracyLocationReceiver : BroadcastReceiver() {
     companion object {
