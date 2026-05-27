90948eb41eca10d3861bef90907543b6fcd35a73
vandrham <38225708+vandrham@users.noreply.github.com>
2026-05-27 10:17:15 +0200

    Apply suggestion from @TimoPtr
    Co-authored-by: Timothy <6560631+TimoPtr@users.noreply.github.com>

--- app/src/debug/resources/lib/arm64-v8a/wrap.sh
+++ app/src/debug/resources/lib/arm64-v8a/wrap.sh
@@ -17,4 +17,4 @@ fi
 # Enables HWAddressSanitizer (HWASan) for debug builds on ARM64 devices running Android 14+.
 # HWASan detects memory errors (buffer overflows, use-after-free, double free, stack-use-after-return).
 # See https://developer.android.com/ndk/guides/hwasan
-LD_HWASAN=1 exec "$cmd"
+LD_HWASAN=1 exec $cmd
