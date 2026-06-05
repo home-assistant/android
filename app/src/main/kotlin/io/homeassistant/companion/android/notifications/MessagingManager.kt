ae966fabdac2209468af4e12f57a47605cd2a8ca
Timothy <6560631+TimoPtr@users.noreply.github.com>
2026-06-05 11:18:16 +0200

    Make a context extension to openAppSettings (#6969)
    * Make a context extension to openAppSettings
    
    * Apply suggestion

--- app/src/main/kotlin/io/homeassistant/companion/android/notifications/MessagingManager.kt
+++ app/src/main/kotlin/io/homeassistant/companion/android/notifications/MessagingManager.kt
@@ -68,6 +68,7 @@ import io.homeassistant.companion.android.common.notifications.parseVibrationPat
 import io.homeassistant.companion.android.common.notifications.prepareText
 import io.homeassistant.companion.android.common.util.SdkVersion
 import io.homeassistant.companion.android.common.util.cancelGroupIfNeeded
+import io.homeassistant.companion.android.common.util.createSystemAppSettingsIntent
 import io.homeassistant.companion.android.common.util.getActiveNotification
 import io.homeassistant.companion.android.common.util.isAutomotive
 import io.homeassistant.companion.android.common.util.kotlinJsonMapper
@@ -1796,10 +1797,7 @@ class MessagingManager @Inject constructor(
     }
 
     private fun navigateAppDetails() {
-        val intent = Intent(
-            Settings.ACTION_APPLICATION_DETAILS_SETTINGS,
-            "package:${context.packageName}".toUri(),
-        )
+        val intent = context.createSystemAppSettingsIntent()
         intent.flags = Intent.FLAG_ACTIVITY_NEW_TASK
         context.startActivity(intent)
     }
