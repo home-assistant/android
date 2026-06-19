6ffa26c06fb7ac9b345bd47b567470275dbbb58c
oungsi2000 <oungsi1000@gmail.com>
2026-06-19 18:20:29 +0900

    Remove unused import in ManageTilesView

--- app/src/main/kotlin/io/homeassistant/companion/android/settings/qs/views/ManageTilesView.kt
+++ app/src/main/kotlin/io/homeassistant/companion/android/settings/qs/views/ManageTilesView.kt
@@ -17,7 +17,6 @@ import androidx.compose.material3.SnackbarHost
 import androidx.compose.material3.SnackbarHostState
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
-import androidx.compose.runtime.Immutable
 import androidx.compose.runtime.LaunchedEffect
 import androidx.compose.runtime.Stable
 import androidx.compose.runtime.remember
