112057e92913b88495593e9991e3cfb2e40afe7a
oungsi2000 <oungsi1000@gmail.com>
2026-07-04 00:37:15 +0900

    Remove unused windowInsetsPadding import in ManageTilesScreen
    * Remove unused `windowInsetsPadding` import from `ManageTilesScreen.kt`

--- app/src/main/kotlin/io/homeassistant/companion/android/settings/qs/ui/ManageTilesScreen.kt
+++ app/src/main/kotlin/io/homeassistant/companion/android/settings/qs/ui/ManageTilesScreen.kt
@@ -13,7 +13,6 @@ import androidx.compose.foundation.layout.heightIn
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.size
 import androidx.compose.foundation.layout.widthIn
-import androidx.compose.foundation.layout.windowInsetsPadding
 import androidx.compose.foundation.rememberScrollState
 import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.foundation.verticalScroll
