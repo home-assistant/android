101d8448436f906ca0e55a8df710d23f190227e4
eyal0 <109809+eyal0@users.noreply.github.com>
2026-08-26 15:06:50 -0600

    Fix log message

--- app/src/full/kotlin/io/homeassistant/companion/android/sensors/LocationSensorManager.kt
+++ app/src/full/kotlin/io/homeassistant/companion/android/sensors/LocationSensorManager.kt
@@ -596,9 +596,9 @@ class LocationSensorManager @Inject constructor(
 
             if (!forceHighAccuracyModeOn && !forceHighAccuracyModeOff) {
                 if (!needHighAccuracyMode) {
-                    Timber.d("High accuracy mode disabled, because not in zone $highAccuracyExpZones")
+                    Timber.d("High accuracy mode disabled, because not in high accuracy zone")
                 } else {
-                    Timber.d("High accuracy mode enabled, because in zone $highAccuracyExpZones")
+                    Timber.d("High accuracy mode enabled, because in zone high accuracy zone")
                 }
             }
         }
