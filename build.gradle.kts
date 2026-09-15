bdf91410ee54bf3a2baf698af5c071c5cbac4ea2
Timothy <6560631+TimoPtr@users.noreply.github.com>
2026-09-15 09:51:27 +0200

    Remove Firebase app distributions (#7477)
    Co-authored-by: Joris Pelgröm <jpelgrom@users.noreply.github.com>

--- build.gradle.kts
+++ build.gradle.kts
@@ -12,7 +12,6 @@ plugins {
     alias(libs.plugins.android.library).apply(false)
     alias(libs.plugins.android.lint).apply(false)
     alias(libs.plugins.google.services).apply(false)
-    alias(libs.plugins.firebase.appdistribution).apply(false)
     alias(libs.plugins.hilt).apply(false)
     alias(libs.plugins.kotlin.parcelize).apply(false)
     alias(libs.plugins.ksp).apply(false)
